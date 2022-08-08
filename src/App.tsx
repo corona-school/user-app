b985962bb3cbcb63634e41114526f81d3e84ba26
Christian Roeszies <croeszies@giftgruen.com>
2022-08-08 09:35:18 +0200

    improve auth flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { NativeBaseProvider, View } from 'native-base'
 import Theme from './Theme'
 import Navigator from './Navigator'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import Autoload from './Autoload'
+import useApollo from './hooks/useApollo'
 
 function App() {
+  const { client } = useApollo()
   return (
-    <NativeBaseProvider theme={Theme}>
-      <Navigator></Navigator>
-    </NativeBaseProvider>
+    <ApolloProvider
+      client={client || new ApolloClient({ cache: new InMemoryCache() })}>
+      <NativeBaseProvider theme={Theme}>
+        <Autoload />
+        <Navigator />
+      </NativeBaseProvider>
+    </ApolloProvider>
   )
 }
 
--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import useApollo from './hooks/useApollo'
 import Dashboard from './pages/Dashboard'
 import EditProfile from './pages/EditProfile'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 
 export default function Navigator() {
+  const { token } = useApollo()
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={token ? <Dashboard /> : <Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/login" element={<Login />} />
