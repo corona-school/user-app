e182c6ebfea7f1cbbaf43195808c0c8fed450305
Christian Roeszies <croeszies@giftgruen.com>
2022-08-08 09:35:36 +0200

    work on login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,40 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { gql, useMutation } from '@apollo/client'
+
+import { Button } from 'native-base'
+import useApollo from '../hooks/useApollo'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
-  return <div>Login</div>
+  const [login, { data, error, loading }] = useMutation(gql`
+    mutation login($password: String!, $email: String!) {
+      loginPassword(password: $password, email: $email)
+    }
+  `)
+
+  const { createToken } = useApollo()
+  const navigate = useNavigate()
+
+  const attemptLogin = async () => {
+    createToken()
+    login({
+      variables: {
+        email: 'croeszies@giftgruen.com',
+        password: 'test123'
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (data && data.loginPassword) {
+      navigate('/')
+    }
+  }, [data, navigate])
+
+  return (
+    <div>
+      {error && <div>Falsche Daten! </div>}
+      <Button onPress={attemptLogin}></Button>
+    </div>
+  )
 }
