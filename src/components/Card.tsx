b3956d7d7bc0b2b4e1fbaac25338c13b916aa42a
Christian Roeszies <croeszies@giftgruen.com>
2022-08-12 15:01:06 +0200

    add variant prop

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,12 +4,17 @@ import { ReactNode } from 'react'
 type Props = {
   children: ReactNode | ReactNode[]
   flexibleWidth?: boolean
+  variant?: 'normal' | 'dark'
 }
 
-const Card: React.FC<Props> = ({ children, flexibleWidth = false }) => {
+const Card: React.FC<Props> = ({
+  children,
+  flexibleWidth = false,
+  variant = 'normal'
+}) => {
   return (
     <BaseCard
-      bg="primary.100"
+      bg={variant === 'normal' ? 'primary.100' : 'primary.900'}
       borderRadius={8}
       padding={0}
       w={flexibleWidth ? 'auto' : '180'}>
