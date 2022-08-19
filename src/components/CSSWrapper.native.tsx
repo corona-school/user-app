5d1b8cd49f3d5ea237d318e6cfe2d804cab1ff26
Christian Roeszies <croeszies@giftgruen.com>
2022-08-19 11:28:25 +0200

    split platform specific code

--- src/components/CSSWrapper.native.tsx
+++ src/components/CSSWrapper.native.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 type Props = { className: string; children: ReactNode | ReactNode[] }
 
-const CSSWrapper: React.FC<Props> = ({ className, children }) => {
-  return <div className={className}>{children}</div>
+const CSSWrapper: React.FC<Props> = ({ children }) => {
+  return <>{children}</>
 }
 export default CSSWrapper
