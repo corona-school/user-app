ba2bd6bafeb58905db324a1b5bc0682f31df54af
Christian Roeszies <croeszies@giftgruen.com>
2022-08-19 11:48:37 +0200

    improve editdatarow

--- src/widgets/EditDataRow.tsx
+++ src/widgets/EditDataRow.tsx
@@ -5,19 +5,34 @@ type Props = {
   label: string
   value?: string
   onPress?: () => any
+  isDisabled?: boolean
 }
 
-const EditDataRow: React.FC<Props> = ({ label, value, onPress }) => {
+const EditDataRow: React.FC<Props> = ({
+  label,
+  value,
+  onPress,
+  isDisabled
+}) => {
   const { space } = useTheme()
 
   return (
-    <Pressable onPress={onPress}>
+    <Pressable onPress={onPress} isDisabled={isDisabled}>
       <DataRow>
-        <Text flex="1" fontWeight={600}>
+        <Text
+          flex="1"
+          fontWeight={600}
+          color={isDisabled ? 'gray.500' : 'darkText'}>
           {label}
         </Text>
-        {value && <Text marginRight={space['0.5']}>{value}</Text>}
-        <ChevronRightIcon color="primary.700" />
+        {value && (
+          <Text
+            marginRight={space['0.5']}
+            color={isDisabled ? 'gray.500' : 'darkText'}>
+            {value}
+          </Text>
+        )}
+        <ChevronRightIcon color={isDisabled ? 'gray.500' : 'primary.700'} />
       </DataRow>
     </Pressable>
   )
