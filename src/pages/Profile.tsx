02f75f0676a75459cd1052af5be6f90aa81a77b0
Christian Roeszies <croeszies@giftgruen.com>
2022-08-26 10:00:08 +0200

    fix icon pos

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -56,7 +56,7 @@ const Profile: React.FC<Props> = () => {
               image="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
               size="xl"
             />
-            <Box position="absolute" right="-37px" bottom="-14px">
+            <Box position="absolute" right="-5px" bottom="-5px">
               <Link href="#">
                 <EditIcon
                   fill={colors['lightText']}
