524692f6d12f3a456473906c33d970f4e6fdcd5b
Christian Roeszies <croeszies@giftgruen.com>
2022-09-28 10:28:02 +0200

    rework questionnaire to proxy some functions

--- src/components/Questionnaire.tsx
+++ src/components/Questionnaire.tsx
@@ -13,9 +13,8 @@ import {
   Dispatch,
   SetStateAction,
   useCallback,
-  useEffect,
-  useMemo,
-  useState
+  useContext,
+  useMemo
 } from 'react'
 import { useTranslation } from 'react-i18next'
 import QuestionnaireSelectionView from './questionnaire/QuestionnaireSelectionView'
@@ -40,14 +39,12 @@ export type Answer<T = boolean | number> = {
   [key: string]: T
 }
 
-export type IQuestionnaire = {
-  questions: Question[]
-  onQuestionnaireFinished: (answers: { [key: string]: Answer }) => any
-}
-
 type IQuestionnaireContext = {
   questions: Question[]
+  currentQuestion: Question
+  setQuestions?: Dispatch<SetStateAction<Question[]>>
   currentIndex: number
+  setCurrentIndex?: Dispatch<SetStateAction<number>>
   answers: {
     [key: string]: Answer
   }
@@ -61,134 +58,49 @@ type IQuestionnaireContext = {
 export const QuestionnaireContext = createContext<IQuestionnaireContext>({
   questions: [],
   currentIndex: 0,
-  answers: {}
+  answers: {},
+  currentQuestion: { label: '', question: '', type: 'selection' }
 })
 
+export type IQuestionnaire = {
+  onQuestionnaireFinished: (answers: { [key: string]: Answer }) => any
+  onPressItem?: (data?: any) => any
+  onQuestionChanged?: (question: Question) => any
+  modifySelectionQuestionBeforeRender?: () => any
+  modifyQuestionBeforeNext?: () => any
+}
+
 const Questionnaire: React.FC<IQuestionnaire> = ({
-  questions: _questions = [],
-  onQuestionnaireFinished
+  onQuestionnaireFinished,
+  onPressItem,
+  modifyQuestionBeforeNext
 }) => {
   const { t } = useTranslation()
-  const [currentIndex, setCurrentIndex] = useState<number>(0)
-
-  const [questions, setQuestions] = useState<Question[]>([])
-  const [answers, setAnswers] = useState<{
-    [key: string]: Answer
-  }>({})
+  const { currentIndex, questions, answers, setCurrentIndex, currentQuestion } =
+    useContext(QuestionnaireContext)
 
   const { space } = useTheme()
-
-  useEffect(() => {
-    setQuestions(_questions)
-  }, [_questions])
-
-  const modifySelectionQuestionBeforeRender: (
-    question: SelectionQuestion
-  ) => SelectionQuestion = useCallback(
-    (question: SelectionQuestion) => {
-      if (question.label === 'Klasse') {
-        const answer = answers['Schulform']
-
-        if (!answer) {
-          question.options = new Array(8).fill(0).map((_, i) => ({
-            key: `${i + 5}`,
-            label: t('lernfair.schoolclass', { class: i + 5 })
-          }))
-          return question
-        }
-
-        if (answer['grundschule']) {
-          question.options = new Array(4).fill(0).map((_, i) => ({
-            key: `${i + 1}`,
-            label: t('lernfair.schoolclass', { class: i + 1 })
-          }))
-        } else {
-          question.options = new Array(6).fill(0).map((_, i) => ({
-            key: `${i + 5}`,
-            label: t('lernfair.schoolclass', { class: i + 5 })
-          }))
-        }
-        if (answer['gymnasium']) {
-          question.options = new Array(8).fill(0).map((_, i) => ({
-            key: `${i + 5}`,
-            label: t('lernfair.schoolclass', { class: i + 5 })
-          }))
-        }
-      }
-      return question
-    },
-    [answers, t]
-  )
-
-  const modifyQuestionBeforeRender: (question: Question) => Question =
-    useCallback(
-      (q: Question) => {
-        if (q.type === 'selection') {
-          q = modifySelectionQuestionBeforeRender(q as SelectionQuestion)
-        }
-
-        return q
-      },
-      [modifySelectionQuestionBeforeRender]
-    )
-
-  const currentQuestion = useMemo(() => {
-    const question = { ...questions[currentIndex] }
-    modifyQuestionBeforeRender(question)
-    return question
-  }, [questions, currentIndex, modifyQuestionBeforeRender])
-
-  const modifyQuestionBeforeNext = useCallback(() => {
-    if (currentQuestion.label === 'Sprache') {
-      if (!answers['Sprache']) return
-      const answer = Object.keys(answers['Sprache'])
-
-      if (!answer) return
-      if (!answer.includes('deutsch')) {
-        const q: SelectionQuestion = {
-          type: 'selection',
-          imgRootPath: 'text',
-          options: [
-            { key: '<1', label: 'Weniger als 1 Jahr' },
-            { key: '>1', label: 'Mehr als 1 Jahr' }
-          ],
-          label: 'Deutsch',
-          question: 'Seit wann lernst du Deutsch?'
-        }
-
-        const qs = [...questions]
-        qs.splice(currentIndex + 1, 0, q)
-        setQuestions(qs)
-      }
-    }
-    if (currentQuestion.label === 'Deutsch') {
-      if (Object.keys(answers['Deutsch']).includes('<1')) {
-        const qs = [...questions]
-        qs.splice(currentIndex + 1, 1)
-        setQuestions(qs)
-      }
-    }
-  }, [answers, currentIndex, currentQuestion.label, questions])
 
   const next = useCallback(() => {
     if (currentIndex >= questions.length - 1) {
       onQuestionnaireFinished && onQuestionnaireFinished(answers)
     } else {
-      modifyQuestionBeforeNext()
-      setCurrentIndex(prev => prev + 1)
+      modifyQuestionBeforeNext && modifyQuestionBeforeNext()
+      setCurrentIndex && setCurrentIndex(prev => prev + 1)
     }
   }, [
     answers,
     currentIndex,
     modifyQuestionBeforeNext,
     onQuestionnaireFinished,
-    questions.length
+    questions.length,
+    setCurrentIndex
   ])
 
   const isValidSelectionAnswer: (answer: Answer) => boolean = useCallback(
     (answer: Answer) => {
       const question = currentQuestion as SelectionQuestion
-      const answercount = Object.values(answer || {}).filter(a => a).length
+      const answercount = Object.values(answer || {}).filter(a => !!a).length
       return (
         answercount >= (question.minSelections || 1) &&
         (question.maxSelections ? answercount <= question.maxSelections : true)
@@ -199,11 +111,13 @@ const Questionnaire: React.FC<IQuestionnaire> = ({
 
   const isValidAnswer: boolean = useMemo(() => {
     const currentAnswer = answers[currentQuestion.label]
+    let isValid = false
     if (!currentAnswer) return false
     if (currentQuestion.type === 'selection') {
-      return isValidSelectionAnswer(currentAnswer)
+      isValid = isValidSelectionAnswer(currentAnswer)
     }
-    return false
+
+    return isValid
   }, [
     answers,
     currentQuestion.label,
@@ -217,63 +131,56 @@ const Questionnaire: React.FC<IQuestionnaire> = ({
   }, [answers, currentQuestion.label, next])
 
   const back = useCallback(() => {
-    setCurrentIndex(prev => prev - 1)
-  }, [])
+    setCurrentIndex && setCurrentIndex(prev => prev - 1)
+  }, [setCurrentIndex])
 
   if (questions.length === 0) return <></>
 
   return (
-    <QuestionnaireContext.Provider
-      value={{
-        currentIndex,
-        questions,
-        answers,
-        setAnswers
-      }}>
-      <Flex flex="1" pb={space['1']}>
-        <Box
-          paddingY={space['2']}
-          bgColor="primary.500"
-          justifyContent="center"
-          alignItems="center"
-          borderBottomRadius={8}>
-          <Heading>{currentQuestion.label}</Heading>
-        </Box>
-        <Box paddingX={space['1']} mt={space['1']}>
-          <Progress
-            value={((currentIndex + 1) / questions.length) * 100}
-            h="3.5"
-            _filledTrack={{ backgroundColor: 'primary.900' }}
+    <Flex flex="1" pb={space['1']}>
+      <Box
+        paddingY={space['2']}
+        bgColor="primary.500"
+        justifyContent="center"
+        alignItems="center"
+        borderBottomRadius={8}>
+        <Heading>{currentQuestion.label}</Heading>
+      </Box>
+      <Box paddingX={space['1']} mt={space['1']}>
+        <Progress
+          value={((currentIndex + 1) / questions.length) * 100}
+          h="3.5"
+          _filledTrack={{ backgroundColor: 'primary.900' }}
+        />
+        <Text fontWeight={600} mt={space['0.5']}>
+          {t('questionnaire.step')} {currentIndex + 1} / {questions.length}
+        </Text>
+      </Box>
+      <Flex flex="1" overflowY={'scroll'}>
+        {currentQuestion.type === 'selection' && (
+          <QuestionnaireSelectionView
+            {...(currentQuestion as SelectionQuestion)}
+            prefill={answers[currentQuestion.label]}
+            onPressSelection={onPressItem}
           />
-          <Text fontWeight={600} mt={space['0.5']}>
-            {t('questionnaire.step')} {currentIndex + 1} / {questions.length}
-          </Text>
-        </Box>
-        <Flex flex="1" overflowY={'scroll'}>
-          {currentQuestion.type === 'selection' && (
-            <QuestionnaireSelectionView
-              {...(currentQuestion as SelectionQuestion)}
-              prefill={answers[currentQuestion.label]}
-            />
-          )}
-        </Flex>
-        <VStack paddingX={space['1']} space={space['0.5']}>
-          <Button isDisabled={!isValidAnswer} onPress={next}>
-            {t('questionnaire.btn.next')}
-          </Button>
-
-          <Button onPress={skip} variant={'outline'}>
-            {t('questionnaire.btn.skip')}
-          </Button>
-
-          {currentIndex > 0 && (
-            <Button onPress={back} variant={'link'}>
-              {t('questionnaire.btn.back')}
-            </Button>
-          )}
-        </VStack>
+        )}
       </Flex>
-    </QuestionnaireContext.Provider>
+      <VStack paddingX={space['1']} space={space['0.5']}>
+        <Button isDisabled={!isValidAnswer} onPress={next}>
+          {t('questionnaire.btn.next')}
+        </Button>
+
+        <Button onPress={skip} variant={'outline'}>
+          {t('questionnaire.btn.skip')}
+        </Button>
+
+        {currentIndex > 0 && (
+          <Button onPress={back} variant={'link'}>
+            {t('questionnaire.btn.back')}
+          </Button>
+        )}
+      </VStack>
+    </Flex>
   )
 }
 
