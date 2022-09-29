910f6ecff68d0678d631d3f33c3658438a2d928b
Benjamin Zekavica <bzekavica@giftgruen.com>
2022-09-29 16:51:25 +0200

    COURSE & MATCHING BLOCKER > added

---
+++ src/lang/course/de.ts
@@ -0,0 +1,19 @@
+const course = {
+  blocker: {
+    header: 'Kurs erstellen',
+    title: 'Die Gruppenkurse',
+    firstContent: 'Du benötigts individuelle Unterstützung? Dann ist die 1:1 Lernunterstützung genau richtig. Hier kannst du eine:n neue:n Student:in anfordern, die dich beim Lernen unterstützt.',
+    here: 'hier',
+    secContent: 'Mehr Informationen zur Gruppennachhilfe findest du',
+    thrContent: 'auf unserer Webseite.',
+    contentHeadline: 'Wichtig',
+    content: 'Aktuell bist du für die Gruppenkurse noch nicht freigeschaltet, aber scrolle hier durch die nächsten Prozessschritte, um herauszufinden, wie du das änderst:',
+    cta: {
+      title: 'Onboarding-Tour Gruppenkurs',
+      content: 'Wenn du dir noch nicht sicher bist, ob das Format der Gruppen-Nachhilfe tatsächlich zu dir passt, klick dich durch das Onboarding für den Gruppen-Nachhilfe Bereich und erhalte so eine genauere Idee von der Tätigkeit.',
+      button: 'Tour starten'
+    }
+  }
+}
+
+export default course
