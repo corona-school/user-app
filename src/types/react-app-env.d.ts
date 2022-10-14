7f37e9fe26cded4fa93ab100f4ebf18b28349e25
Christian Roeszies <croeszies@giftgruen.com>
2022-10-14 17:37:31 +0200

    add env variable

--- src/types/react-app-env.d.ts
+++ src/types/react-app-env.d.ts
@@ -8,6 +8,7 @@ declare namespace NodeJS {
     readonly REACT_APP_APOLLO_CLIENT_URI: string
     readonly REACT_APP_MATOMO_URL: string
     readonly REACT_APP_UNSPLASH: string
+    readonly REACT_APP_UPLOAD_URL: string
   }
   interface ProcessEnv extends LernFairEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test'
