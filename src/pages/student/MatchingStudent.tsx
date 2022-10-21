4325e2bc85f5fde7ab2eb9f4813370b124d74987
Christian Roeszies <croeszies@giftgruen.com>
2022-10-21 16:24:22 +0200

    work on matching

--- src/pages/student/MatchingStudent.tsx
+++ src/pages/student/MatchingStudent.tsx
@@ -1,6 +1,15 @@
-import { gql, useQuery } from '@apollo/client'
-import { View, Text, VStack, Heading, Button, useTheme } from 'native-base'
-import { useCallback, useState } from 'react'
+import { gql, useMutation, useQuery } from '@apollo/client'
+import {
+  View,
+  Text,
+  VStack,
+  Heading,
+  Button,
+  useTheme,
+  Modal,
+  useToast
+} from 'native-base'
+import { useCallback, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
 import Tabs from '../../components/Tabs'
@@ -11,107 +20,150 @@ import LearningPartner from '../../widgets/LearningPartner'
 
 type Props = {}
 
+const query = gql`
+  query {
+    me {
+      student {
+        matches {
+          id
+          dissolved
+          pupil {
+            firstname
+
+            subjectsFormatted {
+              name
+            }
+          }
+        }
+        canRequestMatch {
+          allowed
+          reason
+          limit
+        }
+        openMatchRequestCount
+      }
+    }
+  }
+`
+
 const MatchingStudent: React.FC<Props> = () => {
   const { space } = useTheme()
   const navigate = useNavigate()
   const { t } = useTranslation()
+  const toast = useToast()
+
   const [showDissolveModal, setShowDissolveModal] = useState<boolean>()
+  const [focusedMatch, setFocusedMatch] = useState<LFMatch>()
+  const [toastShown, setToastShown] = useState<boolean>()
 
-  const { data, loading, error } = useQuery(gql`
-    query {
-      me {
-        student {
-          matches {
-            id
-            dissolved
-            pupil {
-              firstname
+  const { data, loading, error } = useQuery(query)
 
-              subjectsFormatted {
-                name
-              }
-            }
-          }
-          canRequestMatch {
-            allowed
-            reason
-            limit
-          }
-          openMatchRequestCount
+  const [dissolveMatch, { data: dissolveData, error: dissolveError }] =
+    useMutation(
+      gql`
+        mutation ($matchId: Float!, $dissolveReason: Float!) {
+          matchDissolve(matchId: $matchId, dissolveReason: $dissolveReason)
         }
-      }
-    }
-  `)
+      `,
+      { refetchQueries: [{ query }] }
+    )
 
-  const dissolveMatch = useCallback((match: LFMatch) => {
-    console.log('dissolve match', match)
+  const showDissolveMatchModal = useCallback((match: LFMatch) => {
+    setFocusedMatch(match)
     setShowDissolveModal(true)
   }, [])
 
+  const dissolve = useCallback(() => {
+    setShowDissolveModal(false)
+    dissolveMatch({
+      variables: { matchId: focusedMatch?.id, dissolveReason: 1 }
+    })
+  }, [dissolveMatch, focusedMatch?.id])
+
+  useEffect(() => {
+    if (dissolveData?.matchDissolve && !toastShown) {
+      setToastShown(true)
+      toast.show({ description: 'Das Match wurde aufgelöst' })
+    }
+  }, [dissolveData?.matchDissolve, toast, toastShown])
+
   return (
-    <WithNavigation>
-      <VStack paddingX={space['1']}>
-        <Heading>Match anfordern</Heading>
-        <VStack space={space['0.5']}>
-          <Text>
-            Die 1:1 Lernunterstützung ist eine 1:1 Betreuung für Schüler:innen
-            die individuelle Hilfe benötigen.
-          </Text>
+    <>
+      <WithNavigation>
+        <VStack paddingX={space['1']}>
+          <Heading>Match anfordern</Heading>
+          <VStack space={space['0.5']}>
+            <Text>
+              Die 1:1 Lernunterstützung ist eine 1:1 Betreuung für Schüler:innen
+              die individuelle Hilfe benötigen.
+            </Text>
 
-          <Text mt="1" bold>
-            Wichtig
-          </Text>
-          <Text>
-            Es kann bis zu einer Woche dauern, ehe wir ein Match für dich
-            gefunden haben.
-          </Text>
-          <Button onPress={() => navigate('/request-match')}>
-            Match anfordern
-          </Button>
+            <Text mt="1" bold>
+              Wichtig
+            </Text>
+            <Text>
+              Es kann bis zu einer Woche dauern, ehe wir ein Match für dich
+              gefunden haben.
+            </Text>
+            <Button onPress={() => navigate('/request-match')}>
+              Match anfordern
+            </Button>
+          </VStack>
+
+          <Tabs
+            tabs={[
+              {
+                title: 'Matches',
+                content: (
+                  <VStack>
+                    {(data?.me?.student?.matches.length &&
+                      data?.me?.student?.matches?.map(
+                        (match: LFMatch, index: number) => (
+                          <LearningPartner
+                            key={index}
+                            isDark={true}
+                            name={match?.pupil?.firstname}
+                            subjects={match?.pupil?.subjectsFormatted.map(
+                              (sub: LFSubject) => sub.name
+                            )}
+                            schooltype="Grundschule"
+                            schoolclass={4}
+                            avatar="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                            button={
+                              (!match.dissolved && (
+                                <Button
+                                  variant="outlinelight"
+                                  onPress={() => showDissolveMatchModal(match)}>
+                                  {t('dashboard.helpers.buttons.solveMatch')}
+                                </Button>
+                              )) || (
+                                <Text color="lightText">
+                                  Das Match wurde aufgelöst
+                                </Text>
+                              )
+                            }
+                          />
+                        )
+                      )) || <Text>Du hast keine Matches</Text>}
+                  </VStack>
+                )
+              }
+            ]}
+          />
         </VStack>
-
-        <Tabs
-          tabs={[
-            {
-              title: 'Matches',
-              content: (
-                <VStack>
-                  {(data?.me?.student?.matches.length &&
-                    data?.me?.student?.matches?.map(
-                      (match: LFMatch, index: number) => (
-                        <LearningPartner
-                          key={index}
-                          isDark={true}
-                          name={match?.pupil?.firstname}
-                          subjects={match?.pupil?.subjectsFormatted.map(
-                            (sub: LFSubject) => sub.name
-                          )}
-                          schooltype="Grundschule"
-                          schoolclass={4}
-                          avatar="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                          button={
-                            (!match.dissolved && (
-                              <Button
-                                variant="outlinelight"
-                                onPress={() => dissolveMatch(match)}>
-                                {t('dashboard.helpers.buttons.solveMatch')}
-                              </Button>
-                            )) || (
-                              <Text color="lightText">
-                                Das Match wurde aufgelöst
-                              </Text>
-                            )
-                          }
-                        />
-                      )
-                    )) || <Text>Du hast keine Matches</Text>}
-                </VStack>
-              )
-            }
-          ]}
-        />
-      </VStack>
-    </WithNavigation>
+      </WithNavigation>
+      <Modal isOpen={showDissolveModal}>
+        <Modal.Content>
+          <Modal.Header>Auflösen</Modal.Header>
+          <Modal.CloseButton onPress={() => setShowDissolveModal(false)} />
+          <Modal.Body>Möchtest du das Match wirklich auflösen?</Modal.Body>
+          <Modal.Footer>
+            <Button variant="ghost">Abbrechen</Button>
+            <Button onPress={dissolve}>Match auflösen</Button>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+    </>
   )
 }
 export default MatchingStudent
--- src/pages/student/RequestMatch.tsx
+++ src/pages/student/RequestMatch.tsx
@@ -1,10 +1,11 @@
 import { gql, useMutation, useQuery } from '@apollo/client'
-import { VStack, Modal, Button, useTheme } from 'native-base'
+import { VStack, Modal, Button, useTheme, Heading } from 'native-base'
 import { useCallback, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import NotificationAlert from '../../components/NotificationAlert'
 import ToggleButton from '../../components/ToggleButton'
 import WithNavigation from '../../components/WithNavigation'
+import useModal from '../../hooks/useModal'
 import Utility from '../../Utility'
 import MatchingBlocker from './MatchingBlocker'
 
@@ -16,6 +17,7 @@ type Props = {}
 const RequestMatch: React.FC<Props> = () => {
   const { space } = useTheme()
   const { t } = useTranslation()
+  const { setShow, setContent } = useModal()
   const [currentIndex, setCurrentIndex] = useState<number>(0)
   const [selectedSubjects, setSelectedSubjects] = useState<{
     [key: string]: boolean
@@ -28,7 +30,7 @@ const RequestMatch: React.FC<Props> = () => {
   const [showModal, setShowModal] = useState<boolean>(false)
   const [description, setDescription] = useState<string>('')
 
-  const { data, loading, error } = useQuery(gql`
+  const { data } = useQuery(gql`
     query {
       me {
         student {
@@ -60,25 +62,30 @@ const RequestMatch: React.FC<Props> = () => {
   `)
 
   const requestMatch = useCallback(() => {
-    // console.log('request match', {
-    //   selectedSubjects,
-    //   selectedClasses,
-    //   focusedSubject,
-    //   description
-    // })
     createMatchRequest()
   }, [createMatchRequest])
 
   useEffect(() => {
     if (matchRequest?.data?.studentCreateMatchRequest) {
-      // TODO show success
+      setContent(
+        <>
+          <Heading>Deine Anfrage wurde erstellt!</Heading>
+          <Button
+            onPress={() => {
+              setShow(false)
+            }}>
+            Weiter
+          </Button>
+        </>
+      )
+      setShow(true)
     }
-  }, [matchRequest])
+  }, [matchRequest?.data?.studentCreateMatchRequest, setContent, setShow])
 
   return (
     <>
       <WithNavigation headerTitle={t('')} headerLeft={<NotificationAlert />}>
-        {(data?.me?.student?.canRequestMatch?.allowed && (
+        {(!data?.me?.student?.canRequestMatch?.allowed && (
           <VStack paddingX={space['1']}>
             {currentIndex === 0 && (
               <RequestMatchWizard
@@ -87,7 +94,7 @@ const RequestMatch: React.FC<Props> = () => {
                 setDescription={setDescription}
                 selectedClasses={selectedClasses}
                 // setSelectedClasses={setSelectedClasses}
-                selectedSubjects={selectedClasses}
+                selectedSubjects={selectedSubjects}
                 setSelectedSubjects={setSelectedSubjects}
                 setCurrentIndex={setCurrentIndex}
                 setFocusedSubject={setFocusedSubject}
@@ -110,7 +117,7 @@ const RequestMatch: React.FC<Props> = () => {
           </VStack>
         )) || <MatchingBlocker />}
       </WithNavigation>
-      {console.log(selectedClasses, focusedSubject)}
+
       <Modal isOpen={showModal}>
         <Modal.Content>
           <Modal.Header>
@@ -118,97 +125,55 @@ const RequestMatch: React.FC<Props> = () => {
           </Modal.Header>
           <Modal.Body>
             <VStack>
-              {focusedSubject?.grade?.min < 5 &&
-                focusedSubject?.grade?.max >= 4 && (
+              {[
+                `1. - 4. Klasse`,
+                `5. - 8. Klasse`,
+                `9. - 10. Klasse`,
+                `11. - 13. Klasse`
+              ].map((c, index) => {
+                const i = index + 1
+                const isSelected =
+                  focusedSubject &&
+                  !!selectedClasses[focusedSubject.name] &&
+                  !!selectedClasses[focusedSubject.name][i]
+
+                return (
                   <ToggleButton
-                    label={`1. - 4. Klasse`}
-                    dataKey={'1'}
-                    isActive={
-                      focusedSubject &&
-                      selectedClasses[focusedSubject.name] &&
-                      selectedClasses[focusedSubject.name][1]
-                    }
-                    onPress={() =>
+                    label={c}
+                    dataKey={`${i}`}
+                    isActive={isSelected}
+                    onPress={key => {
                       setSelectedClasses(prev => ({
                         ...prev,
                         [focusedSubject.name]: {
-                          1:
-                            prev[focusedSubject.name] &&
-                            (!prev[focusedSubject.name][1] || false)
+                          ...selectedClasses[focusedSubject.name],
+                          [key]: !isSelected
                         }
                       }))
-                    }
+                    }}
                   />
-                )}
-              {focusedSubject?.grade?.min <= 5 &&
-                focusedSubject?.grade?.max >= 8 && (
-                  <ToggleButton
-                    label={`5. - 8. Klasse`}
-                    dataKey={'2'}
-                    isActive={
-                      focusedSubject &&
-                      selectedClasses[focusedSubject.name] &&
-                      selectedClasses[focusedSubject.name][2]
-                    }
-                    onPress={() =>
-                      setSelectedClasses(prev => ({
-                        ...prev,
-                        [focusedSubject.name]: {
-                          2:
-                            prev[focusedSubject.name] &&
-                            (!prev[focusedSubject.name][2] || false)
-                        }
-                      }))
-                    }
-                  />
-                )}
-              {focusedSubject?.grade?.min <= 9 &&
-                focusedSubject?.grade?.max >= 10 && (
-                  <ToggleButton
-                    label={`9. - 10. Klasse`}
-                    dataKey={'3'}
-                    isActive={
-                      focusedSubject &&
-                      selectedClasses[focusedSubject.name] &&
-                      selectedClasses[focusedSubject.name][3]
-                    }
-                    onPress={() =>
-                      setSelectedClasses(prev => ({
-                        ...prev,
-                        [focusedSubject.name]: {
-                          3:
-                            prev[focusedSubject.name] &&
-                            (!prev[focusedSubject.name][3] || false)
-                        }
-                      }))
-                    }
-                  />
-                )}
-              {focusedSubject?.grade?.min <= 11 && (
-                <ToggleButton
-                  label={`11. - 13. Klasse`}
-                  dataKey={'4'}
-                  isActive={
-                    focusedSubject &&
-                    selectedClasses[focusedSubject.name] &&
-                    selectedClasses[focusedSubject.name][4]
-                  }
-                  onPress={() =>
-                    setSelectedClasses(prev => ({
-                      ...prev,
-                      [focusedSubject.name]: {
-                        4:
-                          prev[focusedSubject.name] &&
-                          (!prev[focusedSubject.name][4] || false)
-                      }
-                    }))
-                  }
-                />
-              )}
+                )
+              })}
             </VStack>
           </Modal.Body>
           <Modal.Footer>
-            <Button onPress={() => setShowModal(false)}>Speichern</Button>
+            <Button
+              onPress={() => {
+                const selectionCount = Object.values(
+                  selectedClasses[focusedSubject.name] || {}
+                ).filter(c => c).length
+                if (selectionCount === 0) {
+                  setSelectedSubjects(prev => {
+                    const p = { ...prev }
+                    p[focusedSubject.name] = false
+                    return p
+                  })
+                }
+
+                setShowModal(false)
+              }}>
+              Speichern
+            </Button>
           </Modal.Footer>
         </Modal.Content>
       </Modal>
