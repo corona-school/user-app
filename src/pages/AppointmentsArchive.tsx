26f2c08da858e631c105294f18e3aee45cd7dad5
Christian Roeszies <croeszies@giftgruen.com>
2022-10-25 00:47:20 +0200

    work on appointmentarchive

--- src/pages/AppointmentsArchive.tsx
+++ src/pages/AppointmentsArchive.tsx
@@ -4,23 +4,132 @@ import {
   useTheme,
   VStack,
   Input,
-  Link,
+  SearchIcon,
   useBreakpointValue,
   Flex,
-  Column
+  Column,
+  Spinner,
+  Box,
+  Row,
+  Button
 } from 'native-base'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import WithNavigation from '../components/WithNavigation'
 import NotificationAlert from '../components/NotificationAlert'
 import AppointmentCard from '../widgets/AppointmentCard'
+import useLernfair from '../hooks/useLernfair'
+import { useMemo, useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
+import { LFLecture, LFSubCourse } from '../types/lernfair/Course'
+import { DateTime } from 'luxon'
 
 type Props = {}
+
+const studentQuery = gql`
+  query {
+    me {
+      student {
+        subcoursesInstructing {
+          id
+          lectures {
+            start
+          }
+          course {
+            name
+            image
+            outline
+            tags {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`
+const pupilQuery = gql`
+  query {
+    me {
+      pupil {
+        subcoursesJoined {
+          id
+          lectures {
+            start
+          }
+          course {
+            name
+            image
+            outline
+            tags {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const AppointmentsArchive: React.FC<Props> = () => {
   const { space, sizes } = useTheme()
   const navigate = useNavigate()
   const { t } = useTranslation()
+  const { userType } = useLernfair()
+
+  const [searchString, setSearchString] = useState<string>('')
+
+  const { loading, data } = useQuery(
+    userType === 'student' ? studentQuery : pupilQuery
+  )
+
+  const searchResults = useMemo(() => {
+    let obj
+    if (userType === 'student') {
+      obj = data?.me?.student?.subcoursesInstructing
+    } else {
+      obj = data?.me?.pupil?.subcoursesJoined
+    }
+
+    return (
+      obj?.filter((sub: LFSubCourse) =>
+        sub.course.name.toLowerCase().includes(searchString.toLowerCase())
+      ) || []
+    )
+  }, [
+    data?.me?.pupil?.subcoursesJoined,
+    data?.me?.student?.subcoursesInstructing,
+    userType,
+    searchString
+  ])
+
+  const sortedSearchResults: { course: LFSubCourse; lecture: LFLecture }[] =
+    useMemo(() => {
+      let obj = null! as LFSubCourse[]
+      if (userType === 'student') {
+        obj = data?.me?.student?.subcoursesInstructing
+      } else {
+        obj = data?.me?.pupil?.subcoursesJoined
+      }
+
+      if (!obj) return []
+
+      const lectures: { course: LFSubCourse; lecture: LFLecture }[] = []
+      for (const sub of obj) {
+        for (const lecture of sub.lectures) {
+          lectures.push({ course: sub, lecture })
+        }
+      }
+
+      return lectures.sort((a, b) => {
+        const _a = DateTime.fromISO(a.lecture.start).toMillis()
+        const _b = DateTime.fromISO(b.lecture.start).toMillis()
+
+        if (_a > _b) return -1
+        else if (_a < _b) return 1
+        else return 0
+      })
+    }, [data?.me?.pupil?.subcoursesJoined, data?.me?.student, userType])
 
   const ContainerWidth = useBreakpointValue({
     base: '100%',
@@ -42,34 +151,63 @@ const AppointmentsArchive: React.FC<Props> = () => {
             <Heading>{t('archive.appointments.title')}</Heading>
             <Text>{t('archive.appointments.content')}</Text>
           </VStack>
-          <VStack paddingY={space['1']}>
+          <Row paddingY={space['1']}>
             <Input
+              flex="1"
               size="lg"
               placeholder={t('matching.group.helper.support.search')}
+              onChangeText={setSearchString}
             />
-          </VStack>
-          <VStack space={space['1']}>
-            <Heading>{t('archive.appointments.sectionHeadline')}</Heading>
-            <Text>{t('archive.appointments.sectionContent')}</Text>
-          </VStack>
-          <VStack>
-            <Flex direction="row" flexWrap="wrap">
-              {new Array(8).fill(0).map(({}, index) => (
-                <Column width={CardGrid} marginRight="15px">
-                  <AppointmentCard
-                    key={index}
-                    variant="horizontal"
-                    description="Lorem Ipsum"
-                    tags={[{ name: 'Mathematik' }, { name: 'Gruppenkurs' }]}
-                    date={new Date().toString()}
-                    countCourse={4}
-                    onPressToCourse={() => alert('YES')}
-                    image="https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                    title="Diskussionen in Mathe!? – Die Kurvendiskussion"
-                  />
-                </Column>
-              ))}
-            </Flex>
+            <Button padding={space['1']}>
+              <SearchIcon />
+            </Button>
+          </Row>
+          <VStack flex="1">
+            {loading && (
+              <Box mt="5">
+                <Spinner />
+              </Box>
+            )}
+            {!loading && (
+              <>
+                {(sortedSearchResults.length > 0 && (
+                  <Flex direction="row" flexWrap="wrap">
+                    {sortedSearchResults.map(
+                      (
+                        {
+                          course: sub,
+                          lecture
+                        }: { course: LFSubCourse; lecture: LFLecture },
+                        index: number
+                      ) => {
+                        return (
+                          <Column
+                            width={CardGrid}
+                            marginRight="15px"
+                            key={index}>
+                            <AppointmentCard
+                              key={index}
+                              variant="horizontal"
+                              description={sub.course.outline}
+                              tags={sub.course.tags}
+                              date={lecture.start}
+                              countCourse={sub.lectures.length}
+                              onPressToCourse={() =>
+                                navigate('/single-course', {
+                                  state: { course: sub.id }
+                                })
+                              }
+                              image={sub.course.image}
+                              title={sub.course.name}
+                            />
+                          </Column>
+                        )
+                      }
+                    )}
+                  </Flex>
+                )) || <Text>Es wurden keine Ergebnisse gefunden.</Text>}
+              </>
+            )}
           </VStack>
         </VStack>
       </VStack>
