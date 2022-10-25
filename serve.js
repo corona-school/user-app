d7f82da7f7db36925708a62ef55758f0698d7694
Jonas Wilms <JonasWilms2000@gmail.com>
2022-10-25 23:04:25 +0200

    Rework Deployment
    Switch from serve to express, serve is not flexible enough.
    
    Enforce HTTPS, we do not want to serve via HTTP.
    
    Fix PWA Routing (always serve index.html in case the path is not found as file)

---
+++ serve.js
@@ -0,0 +1,23 @@
+const Express = require("express");
+const app = Express();
+
+// Enforce HTTPS - The backend will reject requests from HTTP frontends anyways
+app.use((req, res, next) => {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+      }
+      next();
+});
+
+// Aggressively cache assets as js and css files are different for each build anyways
+//  and logos, manifest et. al. also won't change often
+app.use(Express.static(__dirname + '/build', {
+    immutable: true,
+    maxAge: '7 days'
+}));
+
+// Entrypoint of the PWA - Do not cache to be able to invalidate logic changes fast
+app.get((req, res) => res.sendFile(__dirname + '/build/index.html'));
+
+// Serve on the PORT Heroku wishes
+app.listen(process.env.PORT ?? 5000);
