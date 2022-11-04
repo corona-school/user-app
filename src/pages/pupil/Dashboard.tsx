68f37ff839a866764d0578981308d0ff000b5cdf
Christian Roeszies <croeszies@giftgruen.com>
2022-11-04 10:15:32 +0100

    fix subjects

--- src/pages/pupil/Dashboard.tsx
+++ src/pages/pupil/Dashboard.tsx
@@ -45,6 +45,9 @@ const query = gql`
         matches {
           id
           dissolved
+          subjectsFormatted {
+            name
+          }
           student {
             id
             firstname
@@ -345,13 +348,9 @@ const Dashboard: React.FC<Props> = () => {
                             name={`${match.student?.firstname} ${match.student?.lastname}`}
                             variant="dark"
                             tags={
-                              match.subjectsFormatted?.map(s => s.name) || [
-                                'Fehler',
-                                'Backend',
-                                'Permission'
-                              ]
+                              match.subjectsFormatted?.map(s => s.name) || []
                             }
-                            avatar="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                            avatar=""
                             button={
                               (!match.dissolved && (
                                 <Button
