602613759a99636935bda87a8a2ac9161e45cba6
Lomy <salome.wick@typedigital.de>
2022-11-18 16:18:48 +0100

    refactor: `displayTime`, updated imports

--- src/components/Notification/MessageBox.tsx
+++ src/components/Notification/MessageBox.tsx
@@ -1,22 +1,26 @@
 import { Box, HStack, Spacer, Text, VStack } from 'native-base'
-import { getIconForCategory } from '../../helper/notification-helper'
+import { getIcon } from '../../helper/notification-helper'
 import { UserNotification } from '../../types/lernfair/Notification'
-import TimeAgo from './TimeAgo'
+import TimeIndicator from './TimeIndicator'
 
 type Props = {
   userNotification: UserNotification
-  isToast?: boolean
+  displayTime?: boolean
   isRead?: boolean
 }
 
-const MessageBox: React.FC<Props> = ({ userNotification, isToast, isRead }) => {
+const MessageBox: React.FC<Props> = ({
+  userNotification,
+  displayTime,
+  isRead
+}) => {
   const { headline, body, notification, createdAt } = userNotification
 
   return (
     <Box borderRadius={2} bgColor={isRead ? '' : 'primary.100'} mb={2} h="54px">
       <HStack alignItems="center" space={1}>
         <VStack>
-          <Box p={'8px'}>{getIconForCategory(notification.messageType)}</Box>
+          <Box p={'8px'}>{getIcon(notification.messageType)}</Box>
         </VStack>
         <VStack>
           <Text bold fontSize="14px" mt={2}>
@@ -25,7 +29,7 @@ const MessageBox: React.FC<Props> = ({ userNotification, isToast, isRead }) => {
           <Text fontSize="12px">{body.slice(0, 30)}</Text>
         </VStack>
         <Spacer />
-        <Box>{!isToast && <TimeAgo createdAt={createdAt} />}</Box>
+        <Box>{!displayTime && <TimeIndicator createdAt={createdAt} />}</Box>
       </HStack>
     </Box>
   )
