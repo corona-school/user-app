88ca90462f2cca9aa344ad840bc61515ba00e964
Lomy <salome.wick@typedigital.de>
2022-11-29 13:47:43 +0100

    changed notification preference components

--- src/pages/notification/NotficationControlPanel.tsx
+++ src/pages/notification/NotficationControlPanel.tsx
@@ -1,22 +1,34 @@
 import { t } from 'i18next'
-import { Heading, Row, useTheme, View, VStack } from 'native-base'
+import {
+  Heading,
+  Row,
+  useBreakpointValue,
+  useTheme,
+  View,
+  VStack
+} from 'native-base'
 import { useState } from 'react'
 import Tabs, { Tab } from '../../components/Tabs'
 import WithNavigation from '../../components/WithNavigation'
-import SystemNotifications from './SystemNotifications'
+import SystemNotifications from '../../components/notification/preferences/SystemNotifications'
 
 const NotficationControlPanel = () => {
   const [activeTab, setActiveTab] = useState<number>(0)
   const { space } = useTheme()
 
+  const width = useBreakpointValue({
+    base: 600,
+    lg: 1150
+  })
+
   return (
     <>
       <WithNavigation>
-        <View py={3}>
+        <View py={3} width={600}>
           <Row marginBottom={space['2']}>
             <Heading>E-Mail-Benachrichtigungen</Heading>
           </Row>
-          <VStack width={'1150'}>
+          <VStack>
             <Tabs
               onPressTab={(tab: Tab, index: number) => {
                 setActiveTab(index)
