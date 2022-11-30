08005eff6c5f8eea5a7499ca10a20cb645a56923
Lomy <salome.wick@typedigital.de>
2022-11-30 10:37:38 +0100

    pupil and student mutation

--- src/hooks/useLastTimeCheckedNotifications.ts
+++ src/hooks/useLastTimeCheckedNotifications.ts
@@ -1,31 +1,64 @@
 import { gql, useMutation, useQuery } from '@apollo/client'
+import useLernfair from './useLernfair'
 
-const useLastTimeCheckedNotifications = () => {
-  const { data, loading, error } = useQuery(gql`
-    query {
-      me {
-        pupil {
-          lastTimeCheckedNotifications
-        }
+const studentQuery = gql`
+  query {
+    me {
+      student {
+        lastTimeCheckedNotifications
       }
     }
-  `)
+  }
+`
+
+const pupilQuery = gql`
+  query {
+    me {
+      pupil {
+        lastTimeCheckedNotifications
+      }
+    }
+  }
+`
+
+const studentMutation = gql`
+  mutation updateLastTimeCheckedNotifications(
+    $lastTimeCheckedNotifications: DateTime
+  ) {
+    meUpdate(
+      update: {
+        student: { lastTimeCheckedNotifications: $lastTimeCheckedNotifications }
+      }
+    )
+  }
+`
+
+const pupilMutation = gql`
+  mutation updateLastTimeCheckedNotifications(
+    $lastTimeCheckedNotifications: DateTime
+  ) {
+    meUpdate(
+      update: {
+        pupil: { lastTimeCheckedNotifications: $lastTimeCheckedNotifications }
+      }
+    )
+  }
+`
+
+const useLastTimeCheckedNotifications = () => {
+  const { userType } = useLernfair()
+
+  const { data, loading, error } = useQuery(
+    userType === 'student' ? studentQuery : pupilQuery
+  )
 
   const lastTimeChecked = data?.me?.pupil?.lastTimeCheckedNotifications
   const lastTimeCheckedLoading = loading
   const lastTimeCheckedError = error
 
-  const [updateLastTimeCheckedNotifications] = useMutation(gql`
-    mutation updateLastTimeCheckedNotifications(
-      $lastTimeCheckedNotifications: DateTime
-    ) {
-      meUpdate(
-        update: {
-          pupil: { lastTimeCheckedNotifications: $lastTimeCheckedNotifications }
-        }
-      )
-    }
-  `)
+  const [updateLastTimeCheckedNotifications] = useMutation(
+    userType === 'student' ? studentMutation : pupilMutation
+  )
 
   return {
     lastTimeChecked,
