26b1ac7eb7aa01080955b900f50d81108ad2e84d
Christian Roeszies <croeszies@giftgruen.com>
2022-12-01 13:38:01 +0100

    improve dissolvereasons

--- src/lang/matching/de.ts
+++ src/lang/matching/de.ts
@@ -240,6 +240,30 @@ const matching = {
       subtitle: 'Für welches Fach möchtest du deine Hilfe anbieten?',
       hint: 'Solltest du meherere Fächer anbieten wollen, ist eine Mehrfachauswahl möglich.'
     }
+  },
+  dissolveReasons: {
+    student: {
+      1: 'Mein:e Lernpartner:in hat sich nicht zurückgemeldet',
+      2: 'Mein:e Lernpartner:in benötigt keine Unterstützung mehr',
+      3: 'Ich konnte meinem/meiner Lernpartner:in nicht behilflich sein',
+      4: 'Ich habe keine Zeit mehr mein*e Lernpartner:in zu unterstützen',
+      5: 'Wir hatten Schwierigkeiten auf zwischenmenschlicher Ebene',
+      6: 'Wir konnten keine gemeinsamen Termine finden',
+      7: 'Wir hatten technische Schwierigkeiten',
+      8: 'Sonstiges',
+      9: 'Wir hatten sprachliche Schwierigkeiten'
+    },
+    pupil: {
+      1: 'Mein:e Lernpartner:in hat sich nicht zurückgemeldet',
+      2: 'Mein:e Lernpartner:in konnte mir nicht behilflich sein',
+      3: 'Mein:e Lernpartner:in hat keine Zeit mehr mich zu unterstützen',
+      4: 'Ich benötige keine Unterstützung mehr',
+      5: 'Wir hatten Schwierigkeiten auf zwischenmenschlicher Ebene',
+      6: 'Wir konnten keine gemeinsamen Termine finden',
+      7: 'Wir hatten technische Schwierigkeiten',
+      8: 'Sonstiges',
+      9: 'Wir hatten sprachliche Schwierigkeiten'
+    }
   }
 }
 
--- src/modals/DissolveMatchModal.tsx
+++ src/modals/DissolveMatchModal.tsx
@@ -1,5 +1,7 @@
+import { t } from 'i18next'
 import { Button, Modal, Radio, Row, useTheme, VStack } from 'native-base'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
+import useLernfair from '../hooks/useLernfair'
 
 type DissolveModalProps = {
   showDissolveModal: boolean | undefined
@@ -7,14 +9,23 @@ type DissolveModalProps = {
   onPressBack: () => any
 }
 
+// corresponding dissolve reason ids in translation file
+export const studentReasonOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+export const pupilReasonOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 const DissolveMatchModal: React.FC<DissolveModalProps> = ({
   showDissolveModal,
   onPressDissolve,
   onPressBack
 }) => {
   const { space } = useTheme()
-
+  const { userType } = useLernfair()
   const [reason, setReason] = useState<string>('')
+
+  const reasons = useMemo(
+    () => (userType === 'student' ? studentReasonOptions : pupilReasonOptions),
+    [userType]
+  )
 
   return (
     <Modal isOpen={showDissolveModal} onClose={onPressBack}>
@@ -27,11 +38,11 @@ const DissolveMatchModal: React.FC<DissolveModalProps> = ({
             value={reason}
             onChange={setReason}>
             <VStack space={space['1']}>
-              <Radio value={'1'}>
-                Lernpartner:in hat seine/ihre Note erreicht
-              </Radio>
-              <Radio value={'2'}>Termine wurden nicht eingehalten</Radio>
-              <Radio value={'3'}>Sonstiges</Radio>
+              {reasons.map((reason: number) => (
+                <Radio value={`${reason}`}>
+                  {t(`matching.dissolveReasons.${userType}.${reason}`)}
+                </Radio>
+              ))}
             </VStack>
           </Radio.Group>
         </Modal.Body>
@@ -42,7 +53,9 @@ const DissolveMatchModal: React.FC<DissolveModalProps> = ({
               onPress={() => onPressDissolve(reason)}>
               Match auflösen
             </Button>
-            <Button onPress={onPressBack}>Zurück</Button>
+            <Button onPress={onPressBack} variant="ghost">
+              Zurück
+            </Button>
           </Row>
         </Modal.Footer>
       </Modal.Content>
