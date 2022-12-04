f1126c21989809c1cf7730251b2d876b906a081e
Jonas Wilms <JonasWilms2000@gmail.com>
2022-12-04 22:11:57 +0100

    Make GraphQL Schema configurable, change graphql to gql

--- codegen.ts
+++ codegen.ts
@@ -3,11 +3,14 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://corona-school-backend-dev.herokuapp.com/apollo",
+  schema: process.env.GRAPHQL_SCHEMA_URL ?? "https://corona-school-backend-dev.herokuapp.com/apollo",
   documents: "src/**/*.tsx",
   generates: {
     "src/gql/": {
       preset: "client",
+      presetConfig: {
+        gqlTagName: 'gql',
+      },
       plugins: []
     }
   }
