b230e40791e623de8ac65336227863566c582217
Lomy <salome.wick@typedigital.de>
2022-12-09 14:07:59 +0100

    add marketing message type

--- src/types/lernfair/Notification.ts
+++ src/types/lernfair/Notification.ts
@@ -10,22 +10,6 @@ export type Notification = {
   messageType: string
 }
 
-// TODO delete NotiType
-export enum NotiType {
-  SURVEY = 'survey',
-  TRANSACTION = 'transaction',
-  TYP1 = 'Type 1',
-  TYP2 = 'Type 2',
-  TYP3 = 'Type 3',
-  TYP4 = 'Type 4',
-  TYP5 = 'Type 5',
-  TYP6 = 'Type 6',
-  TYP7 = 'Type 7',
-  TYP8 = 'Type 8',
-  TYP9 = 'Type 9',
-  TYP10 = 'Type 10'
-}
-
 export enum MessageType {
   MESSAGE = 'message',
   MATCH = 'match',
@@ -34,4 +18,15 @@ export enum MessageType {
   SURVEY = 'survey',
   NEWS = 'news',
   CHAT = 'chat'
+}
+
+export enum MarketingMessageType {
+  NEWSLETTER = 'newsletter',
+  TRAINING = 'training',
+  EVENTS = 'events',
+  NEWSOFFER = 'newsoffer',
+  REQUEST = 'request',
+  LEARNOFFER = 'learnoffer',
+  ALTERNATIVEOFFER = 'alternativeoffer',
+  FEEDBACK = 'feedback'
 }
