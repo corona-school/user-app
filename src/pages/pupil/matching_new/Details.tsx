d5f0619b949308878d003a80866a261ab26559b7
Christian <croeszies@giftgruen.com>
2022-12-12 08:42:15 +0100

    add modal

--- src/pages/pupil/matching_new/Details.tsx
+++ src/pages/pupil/matching_new/Details.tsx
@@ -1,34 +1,75 @@
+import { gql, useMutation } from '@apollo/client'
 import {
-  View,
   Text,
   VStack,
   Heading,
   Button,
   useTheme,
-  TextArea
+  TextArea,
+  useToast
 } from 'native-base'
 import { useCallback, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import useModal from '../../../hooks/useModal'
 import { LFSubject } from '../../../types/lernfair/Subject'
 import { RequestMatchContext } from './Matching'
 
 type Props = {}
 
 const Details: React.FC<Props> = () => {
+  const { setShow, setContent, setVariant } = useModal()
   const { space } = useTheme()
+  const toast = useToast()
   const { matching, setMatching } = useContext(RequestMatchContext)
+  const navigate = useNavigate()
 
-  const requestMatch = useCallback(() => {
+  const [update, _update] = useMutation(gql`
+    mutation update($subjects: [SubjectInput!]) {
+      meUpdate(update: { pupil: { subjects: $subjects } })
+    }
+  `)
+
+  const showModal = useCallback(() => {
+    setVariant('dark')
+
+    setContent(
+      <VStack>
+        <Heading fontSize={'2xl'}>
+          Geschafft, du bist auf der Warteliste!
+        </Heading>
+
+        <Button>Zu den Gruppenkursen</Button>
+        <Button
+          variant={'outline'}
+          onPress={() => {
+            navigate('/start')
+            setShow(false)
+          }}>
+          Fertig
+        </Button>
+      </VStack>
+    )
+    setShow(true)
+  }, [navigate, setContent, setShow, setVariant])
+
+  const requestMatch = useCallback(async () => {
     const subs: LFSubject[] = []
     for (const sub of matching.subjects) {
       const data = { name: sub.label } as { name: string; mandatory?: boolean }
-
       if (matching.priority === sub) {
         data.mandatory = true
       }
-
       subs.push(data)
     }
-  }, [matching.priority, matching.subjects])
+
+    const res = await update({ variables: { subjects: subs } })
+
+    if (res.data && !res.errors) {
+      showModal()
+    } else {
+      toast.show({ description: 'Es ist ein Fehler aufgetreten' })
+    }
+  }, [matching.priority, matching.subjects, showModal, toast, update])
 
   return (
     <VStack paddingX={space['1']} space={space['0.5']}>
@@ -53,7 +94,9 @@ const Details: React.FC<Props> = () => {
         value={matching.message}
         onChangeText={text => setMatching(prev => ({ ...prev, message: text }))}
       />
-      <Button onPress={requestMatch}>Weiter</Button>
+      <Button isDisabled={_update.loading} onPress={requestMatch}>
+        Weiter
+      </Button>
     </VStack>
   )
 }
