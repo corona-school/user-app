846ad58536c4beb7a671a58b8644d7c9777fd7f8
Christian <croeszies@giftgruen.com>
2022-12-14 16:34:57 +0100

    remove text

--- src/pages/pupil/Matching.tsx
+++ src/pages/pupil/Matching.tsx
@@ -212,11 +212,6 @@ const Matching: React.FC<Props> = () => {
                   title: t('matching.request.check.tabs.tab2'),
                   content: (
                     <VStack space={space['1']}>
-                      <Text marginBottom={space['1']}>
-                        {t('matching.request.check.openedRequests')}
-                        {'  '}
-                        {data?.me?.pupil?.openMatchRequestCount}
-                      </Text>
                       <VStack space={space['0.5']}>
                         <Flex direction="row" flexWrap="wrap">
                           {(data?.me?.pupil?.openMatchRequestCount &&
