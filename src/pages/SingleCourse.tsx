dd40800406c08c3e3f858a879e4dcab4bbf3b175
Christian <croeszies@giftgruen.com>
2022-12-15 10:14:56 +0100

    fix

--- src/pages/SingleCourse.tsx
+++ src/pages/SingleCourse.tsx
@@ -237,7 +237,7 @@ const SingleCourse: React.FC = () => {
           title: $subject
           body: $message
           participantIDs: $participants
-          fileIDs: ""
+          fileIDs: []
         )
       }
     `
