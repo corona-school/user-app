6041034fd2a1320b6d9d2f22ef2bdca679c5c827
Jonas Wilms <JonasWilms2000@gmail.com>
2022-12-17 12:18:55 +0100

    Remove ESLint, add Prettier with Precommit Hook

--- src/User.tsx
+++ src/User.tsx
@@ -1,53 +1,48 @@
-import { useLocation, Navigate } from "react-router-dom"
-import CenterLoadingSpinner from "./components/CenterLoadingSpinner"
-import useApollo from "./hooks/useApollo"
-import VerifyEmailModal from "./modals/VerifyEmailModal"
+import { useLocation, Navigate } from 'react-router-dom'
+import CenterLoadingSpinner from './components/CenterLoadingSpinner'
+import useApollo from './hooks/useApollo'
+import VerifyEmailModal from './modals/VerifyEmailModal'
 
 export const RequireAuth = ({ children }: { children: JSX.Element }) => {
-    const location = useLocation()
-    
-    const { sessionState, user } = useApollo()
-  
-    if (sessionState === 'logged-out')
-      return <Navigate to="/welcome" state={{ from: location }} replace />
-  
-    
-    if (sessionState === "unknown" || !user)
-      return <CenterLoadingSpinner />
+  const location = useLocation()
 
-    if (sessionState === 'logged-in') {
-      if (user && !(user.pupil ?? user.student)!.verifiedAt)
-        return <VerifyEmailModal email={user.email} />
-  
-      return children
-    }
-  
+  const { sessionState, user } = useApollo()
+
+  if (sessionState === 'logged-out')
     return <Navigate to="/welcome" state={{ from: location }} replace />
-  }
-  
-  export const SwitchUserType = ({
-    pupilComponent,
-    studentComponent
-  }: {
-    pupilComponent?: JSX.Element
-    studentComponent?: JSX.Element
-  }) => {
-    const location = useLocation()
-    const { sessionState, user } = useApollo()
-  
-    if (sessionState === "logged-out")
-      return <Navigate to="/welcome" state={{ from: location }} replace />
-  
-    if (sessionState === "unknown" || !user) 
-      return <CenterLoadingSpinner />
-  
 
-    if (user!.student) {
-      if (studentComponent) return studentComponent
-      else return <Navigate to="/dashboard" state={{ from: location }} replace />
-    } else {
-      if (pupilComponent) return pupilComponent
-      else return <Navigate to="/dashboard" state={{ from: location }} replace />
-    }
+  if (sessionState === 'unknown' || !user) return <CenterLoadingSpinner />
+
+  if (sessionState === 'logged-in') {
+    if (user && !(user.pupil ?? user.student)!.verifiedAt)
+      return <VerifyEmailModal email={user.email} />
+
+    return children
   }
-  
+
+  return <Navigate to="/welcome" state={{ from: location }} replace />
+}
+
+export const SwitchUserType = ({
+  pupilComponent,
+  studentComponent
+}: {
+  pupilComponent?: JSX.Element
+  studentComponent?: JSX.Element
+}) => {
+  const location = useLocation()
+  const { sessionState, user } = useApollo()
+
+  if (sessionState === 'logged-out')
+    return <Navigate to="/welcome" state={{ from: location }} replace />
+
+  if (sessionState === 'unknown' || !user) return <CenterLoadingSpinner />
+
+  if (user!.student) {
+    if (studentComponent) return studentComponent
+    else return <Navigate to="/dashboard" state={{ from: location }} replace />
+  } else {
+    if (pupilComponent) return pupilComponent
+    else return <Navigate to="/dashboard" state={{ from: location }} replace />
+  }
+}
