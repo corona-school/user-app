bf4176cdcf8d08fb714e63fafd4cb58b174caad8
Leo310 <leodev310@gmail.com>
2022-12-19 12:09:47 +0100

    Add: more conditionals and callbacks (still not completely finished)

--- src/widgets/ImportantInformation.tsx
+++ src/widgets/ImportantInformation.tsx
@@ -4,6 +4,7 @@ import Card from '../components/Card';
 import BooksIcon from '../assets/icons/lernfair/lf-books.svg';
 import { useMutation, useQuery } from '@apollo/client';
 import { gql } from '../gql';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
     variant?: 'normal' | 'dark';
@@ -12,6 +13,7 @@ type Props = {
 const ImportantInformation: React.FC<Props> = ({ variant }) => {
     const { space } = useTheme();
     const { t } = useTranslation();
+    const navigate = useNavigate();
     const textColor = variant === 'dark' ? 'lightText' : 'darkText';
 
     const { data } = useQuery(
@@ -25,6 +27,10 @@ const ImportantInformation: React.FC<Props> = ({ variant }) => {
             student {
               createdAt
               verifiedAt
+			  matches {
+				dissolved
+				createdAt
+			  }
               firstMatchRequest
               openMatchRequestCount
               certificateOfConduct {
@@ -42,10 +48,15 @@ const ImportantInformation: React.FC<Props> = ({ variant }) => {
             pupil {
               createdAt
               verifiedAt
+			  matches {
+				dissolved
+				createdAt
+			  }
               firstMatchRequest
 			  openMatchRequestCount
               tutoringInterestConfirmation {
                 id
+				token
                 status
               }
             }
@@ -58,24 +69,32 @@ const ImportantInformation: React.FC<Props> = ({ variant }) => {
         gql(`mutation SendVerificationMail($email: String!) { tokenRequest(email:$email action: "user-verify-email" redirectTo: "/dashboard") }`),
         { variables: { email: data?.me?.email || 'email never empty' } }
     );
-    const [confirmInterest] = useMutation(gql(`mutation Confirm($token: String!){ tutoringInterestConfirm(token:$token) }`), { variables: { token: 'TODO' } });
-    const [refuseInterest] = useMutation(gql(`mutation Refuse($token: String!){ tutoringInterestRefuse(token:$token) }`), { variables: { token: 'TODO' } });
+    const [confirmInterest] = useMutation(gql(`mutation Confirm($token: String!){ tutoringInterestConfirm(token:$token) }`), {
+        variables: { token: data?.me?.pupil?.tutoringInterestConfirmation?.token || 'token never empty' },
+    });
+    const [refuseInterest] = useMutation(gql(`mutation Refuse($token: String!){ tutoringInterestRefuse(token:$token) }`), {
+        variables: { token: data?.me?.pupil?.tutoringInterestConfirmation?.token || 'token never empty' },
+    });
 
-    let infos = [];
+    let infos: { label: string; btnfn: (() => void)[] }[] = [];
     if (!data?.me?.student?.verifiedAt) infos.push({ label: 'verifizierung', btnfn: [sendVerificationMail] });
-    if (!data?.me?.student?.firstMatchRequest && !data?.me?.pupil) infos.push({ label: 'kennenlernen', btnfn: [] });
-    if (!data?.me?.pupil?.firstMatchRequest && !data?.me?.student) infos.push({ label: 'willkommen', btnfn: [] });
+    if (data?.me?.student?.canRequestMatch?.reason === 'not-screened' || data?.me?.student?.canCreateCourse?.reason === 'not-screened')
+        infos.push({ label: 'kennenlernen', btnfn: [] });
+    if (!data?.me?.pupil?.firstMatchRequest && !data?.me?.student)
+        infos.push({ label: 'willkommen', btnfn: [() => navigate('/group'), () => navigate('/matching')] });
     if (data?.me?.pupil?.openMatchRequestCount && data?.me?.pupil?.openMatchRequestCount > 0) infos.push({ label: 'statusSchüler', btnfn: [] });
     if (data?.me?.student?.openMatchRequestCount && data?.me?.student?.openMatchRequestCount > 0)
         infos.push({ label: 'statusStudent', btnfn: [() => (window.location.href = 'mailto:support@lern-fair.de')] });
-    if (!data?.me?.secrets?.some((secret: any) => secret.type === 'PASSWORD')) infos.push({ label: 'passwort', btnfn: [] });
+    if (!data?.me?.secrets?.some((secret: any) => secret.type === 'PASSWORD')) infos.push({ label: 'passwort', btnfn: [() => navigate('/reset-password')] });
     if (data?.me?.pupil?.tutoringInterestConfirmation?.status && data?.me?.pupil?.tutoringInterestConfirmation?.status === 'pending')
         infos.push({ label: 'interestconformation', btnfn: [confirmInterest, refuseInterest] });
-    if (!data?.me?.student) infos.push({ label: 'kontaktSchüler', btnfn: [] });
-    if (!data?.me?.student) infos.push({ label: 'angeforderteBescheinigung', btnfn: [] });
-    if (!data?.me?.pupil) infos.push({ label: 'kontaktStudent', btnfn: [] });
+    // if (data?.me?.pupil?.matches.some((match: any) => !match.dissolved && match.createdAt > new Date(Date.now() - 14 * 24 * 60 * 60 * 1000)))
+    infos.push({ label: 'kontaktSchüler', btnfn: [() => (window.location.href = 'mailto:support@lern-fair.de')] });
+    if (data?.me?.student?.matches.some((match: any) => !match.dissolved && match.createdAt > new Date(Date.now() - 14 * 24 * 60 * 60 * 1000)))
+        infos.push({ label: 'kontaktStudent', btnfn: [() => (window.location.href = 'mailto:support@lern-fair.de')] });
     if (!data?.me?.pupil && !data?.me?.student?.certificateOfConduct?.id)
         infos.push({ label: 'zeugnis', btnfn: [() => (window.location.href = 'mailto:fz@lern-fair.de')] });
+    // if (!data?.me?.student) infos.push({ label: 'angeforderteBescheinigung', btnfn: [] });
 
     const importantInformationCards = infos.map((config, index) => {
         const buttontexts: String[] = t('helperwizard.' + config.label + '.buttons', { returnObjects: true });
