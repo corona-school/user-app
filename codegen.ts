ba5efb83c92194f9b9d26b6d2fb9a8f359ac420b
Jonas Wilms <jonaswilms2000@gmail.com>
2022-12-27 11:23:50 +0100

    Fetch GraphQL Schema from the Backend URL configured in .env (#83)

--- codegen.ts
+++ codegen.ts
@@ -1,9 +1,30 @@
-
 import type { CodegenConfig } from '@graphql-codegen/cli';
+
+// Required by ./config
+process.env.NODE_ENV = 'production';
+const getClientEnvironment = require("./config/env");
+
+// Usually take the explicit env variable - This is set inside CI and other well configured environments
+let schema = process.env.GRAPHQL_SCHEMA_URL;
+
+// If that isn't set, try the one Webpack also builds into the App, this should match to the development setup
+if (!schema) {
+  const backend = (getClientEnvironment("").raw as any)?.REACT_APP_BACKEND_URL;
+  if (backend) {
+    schema = `${backend}/apollo`;
+  }
+}
+
+if (!schema) {
+  // In case nobody configured it, fall back to default staging backend
+  schema = "https://corona-school-backend-dev.herokuapp.com/apollo";
+}
+
+console.info(`Loading GraphQL Schema from '${schema}'`);
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: process.env.GRAPHQL_SCHEMA_URL ?? "https://corona-school-backend-dev.herokuapp.com/apollo",
+  schema,
   documents: "src/**/*.tsx",
   generates: {
     "src/gql/": {
