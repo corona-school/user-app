b38224901fd2685bb64d8e53bde0bb84efefeb31
Jonas Wilms <JonasWilms2000@gmail.com>
2022-12-27 19:33:33 +0100

    Also run codegen for .ts files

--- config/graphql-codegen.ts
+++ config/graphql-codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: 'src/gql/schema.json',
-  documents: "src/**/*.tsx",
+  documents: "src/**/*.(tsx|ts)",
   generates: {
     "src/gql/": {
       preset: "client",
