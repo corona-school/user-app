3c04ffcde9ba4251e79bbd93e13f2ffd078dcabe
Lomy <salome.wick@typedigital.de>
2022-12-28 11:24:11 +0100

    show button cases, rename capacity

--- src/widgets/PromoteButton.tsx
+++ src/widgets/PromoteButton.tsx
@@ -1,4 +1,5 @@
-import { gql, useMutation, useQuery } from '@apollo/client';
+import { gql } from './../gql';
+import { useMutation, useQuery } from '@apollo/client';
 import { Button, Tooltip, useBreakpointValue, useTheme, useToast } from 'native-base';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -15,16 +16,16 @@ query Subcourse($subcourseId: Int!) {
     courseId
     maxParticipants
     participantsCount
-    courseCapacity
+    capacity
     published
     publishedAt
     alreadyPromoted
   }
 }, `);
 
-const promoteMutation = gql(`
-    mutation PromoteSubcourse($subcourseId: Float!) {
-        promoteSubcourse(subcourseId: $subcourseId)
+const mutation = gql(`
+    mutation subcoursePromote($subcourseId: Float!) {
+        subcoursePromote(subcourseId: $subcourseId)
     }
 `);
 
@@ -33,14 +34,13 @@ const PromoteButton: React.FC<Props> = ({ subcourseId }) => {
     const { sizes } = useTheme();
     const toast = useToast();
     const { t } = useTranslation();
-
     const { data, loading, refetch } = useQuery(promoteQuery, { variables: { subcourseId: subcourseId } });
-
-    const [promote] = useMutation(promoteMutation, { variables: { subcourseId: subcourseId } });
+    const [promote] = useMutation(mutation, { variables: { subcourseId: subcourseId } });
 
     const alreadyPromoted = data?.subcourse?.alreadyPromoted;
-    const courseCapacity = data?.subcourse?.courseCapacity;
+    const capacity = data?.subcourse?.capacity ?? 0;
     const publishedAt = data?.subcourse?.publishedAt;
+    const published = data?.subcourse?.published;
     const { daysDiff } = getTimeDifference(publishedAt);
 
     const ButtonContainer = useBreakpointValue({
@@ -48,24 +48,32 @@ const PromoteButton: React.FC<Props> = ({ subcourseId }) => {
         lg: sizes['desktopbuttonWidth'],
     });
 
-    async function doPromote() {
+    const doPromote = async () => {
         await promote();
         toast.show({ description: 'Kurs beworben' });
         refetch();
-    }
+    };
+
+    const isPublishedThreeDaysAgo = (publishDate: string): boolean => {
+        const { daysDiff } = getTimeDifference(publishDate);
+        if (publishDate === null || daysDiff > 3) {
+            return true;
+        }
+        return false;
+    };
 
     useEffect(() => {
-        if (alreadyPromoted == false && courseCapacity < 0.75 && (publishedAt === null || daysDiff > 3)) {
+        if (!alreadyPromoted && capacity < 0.75 && isPublishedThreeDaysAgo(publishedAt)) {
             setIsPromotionButtonDisabled(false);
         } else {
             setIsPromotionButtonDisabled(true);
         }
-    }, [alreadyPromoted, courseCapacity, daysDiff]);
+    }, [alreadyPromoted, capacity, publishedAt, daysDiff]);
 
     return (
         <div>
-            {!loading && (
-                <Tooltip label={t('single.buttonPromote.tooltip')} p={3}>
+            {!loading && published && isPublishedThreeDaysAgo(publishedAt) && (
+                <Tooltip label={t('single.buttonPromote.tooltip')} p={3} placement="bottom" hasArrow>
                     <Button width={ButtonContainer} isDisabled={isPromotionButtonDisabled} onPress={doPromote}>
                         {t('single.buttonPromote.button')}
                     </Button>
