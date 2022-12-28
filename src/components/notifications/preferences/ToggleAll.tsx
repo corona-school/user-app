d68878e070848a4b485ce92f6f95f18ee8d1a4c5
Jewgenij Steinhart <4200365+jeffersondarcy@users.noreply.github.com>
2022-12-28 11:09:01 +0100

    toggle buttons responsive

--- src/components/notifications/preferences/ToggleAll.tsx
+++ src/components/notifications/preferences/ToggleAll.tsx
@@ -1,24 +1,21 @@
-import { Button, Box, HStack, VStack, useBreakpointValue } from 'native-base';
+import { Button, Box, useBreakpointValue, Stack } from 'native-base';
 import { useTranslation } from 'react-i18next';
 import { NotificationCategories } from '../../../helper/notification-preferences';
-import { useContext, useEffect, useRef } from 'react';
+import { FC, useContext, useEffect, useRef } from 'react';
 import { NotificationPreferencesContext } from '../../../pages/notification/NotficationControlPanel';
 import { getAllPreferencesInCategorySetToValue } from '../../../helper/notification-helper';
+import { useLayoutHelper } from '../../../hooks/useLayoutHelper';
 
 type PrefProps = {
     notificationCategories: NotificationCategories;
 };
 
-export const ToggleAll: React.FC<PrefProps> = ({ notificationCategories }) => {
+export const ToggleAll: FC<PrefProps> = ({ notificationCategories }) => {
     const { userPreferences, channels, updateUserPreferences } = useContext(NotificationPreferencesContext);
+    const { isMobile } = useLayoutHelper();
     const allEnabled = useRef(false);
     const allDisabled = useRef(false);
     const { t } = useTranslation();
-
-    const maxW = useBreakpointValue({
-        base: 200,
-        lg: '100%',
-    });
 
     const width = useBreakpointValue({
         base: 340,
@@ -30,7 +27,7 @@ export const ToggleAll: React.FC<PrefProps> = ({ notificationCategories }) => {
         return Object.keys(notificationCategories).every((category: string) =>
             channels.every((channel: string) => {
                 if (!userPreferences.hasOwnProperty(category) || !userPreferences[category].hasOwnProperty(channel)) return true;
-                console.log(category, channel, userPreferences[category][channel]);
+
                 return isEnabled ? userPreferences[category][channel] : !userPreferences[category][channel];
             })
         );
@@ -62,14 +59,14 @@ export const ToggleAll: React.FC<PrefProps> = ({ notificationCategories }) => {
         channels &&
         notificationCategories && (
             <Box borderBottomWidth={1} borderBottomColor={'gray.100'} py={3} width={width}>
-                <HStack alignItems="center" space={1}>
+                <Stack direction={isMobile ? 'column' : 'row'} alignItems="center" space={1}>
                     <Button onPress={enableAll} disabled={allEnabled.current}>
                         {t('notification.controlPanel.preference.enableAll')}
                     </Button>
                     <Button onPress={disableAll} variant="ghost" disabled={allDisabled.current}>
                         {t('notification.controlPanel.preference.disableAll')}
                     </Button>
-                </HStack>
+                </Stack>
             </Box>
         )
     );
