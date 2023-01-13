fc2dd8cbed0ee076036c9f38c84954cccbee67c1
Lomy <salome.wick@typedigital.de>
2023-01-13 15:18:49 +0100

    add translation

---
+++ src/lang/appointments/de.ts
@@ -0,0 +1,19 @@
+const appointments = {
+    singleAppointment: {
+        group: 'Gruppentermin mit {{instructor}}',
+        single: 'Einzeltermin mit {{instructor}}',
+        without: 'Termin mit {{instructor}}',
+        lecture: 'Lektion {{appointmentCount}}: {{lecture}}',
+        title: 'Kurs: {{courseTitle}}',
+        time: '{{start}} - {{end}} Uhr ({{duration}})',
+        repeateDate: '{{appointmentCount}}. von insgesamt {{appointmentsTotal}} Terminen',
+        participants: '{{participantsTotal}} Teilnehmer:innen',
+        videochatButton: 'Jetzt Videochat beitreten',
+        descriptionTitle: 'Kursbeschreibung: {{courseTitle}}',
+        deleteAppointmentButton: 'Termin löschen',
+        cancelAppointmentButton: 'Termin absagen',
+        adjustAppointmentButton: 'Termin bearbeiten',
+    },
+};
+
+export default appointments;
--- src/lang/de.ts
+++ src/lang/de.ts
@@ -15,6 +15,7 @@ import matching from './matching/de';
 import course from './course/de';
 import archive from './archive/de';
 import notification from './notification/de';
+import appointments from './appointments/de';
 
 const de = {
     ...shared,
@@ -34,5 +35,6 @@ const de = {
     course,
     archive,
     notification,
+    appointments,
 };
 export default de;
