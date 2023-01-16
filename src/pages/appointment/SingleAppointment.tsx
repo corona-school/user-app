bdac19981dec1e00a00ad684c35c004f070de3ab
Lomy <salome.wick@typedigital.de>
2023-01-16 11:52:33 +0100

    add dummy to page

--- src/pages/appointment/SingleAppointment.tsx
+++ src/pages/appointment/SingleAppointment.tsx
@@ -1,7 +1,28 @@
+import AppointmentDetail from '../../components/appointment/AppointmentDetail';
 import WithNavigation from '../../components/WithNavigation';
 
 const SingleAppointment = () => {
-    return <WithNavigation showBack>Test</WithNavigation>;
+    return (
+        <WithNavigation showBack>
+            <AppointmentDetail
+                id={1}
+                appointmentType={'GROUP'}
+                instructors={['Anna Maier', 'Klaus Kleber', 'Hans Huber']}
+                participants={['Max Müller', 'Mara Muster', 'Paula Peter', 'Mia Maus', 'Sarah Sonntag']}
+                appointmentTitle={'Lektion: Grundlagen td'}
+                courseTitle={'Test von td'}
+                description={
+                    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+                }
+                startDate={'2023-01-20T15:00Z'}
+                duration={60}
+                appointmentsCount={2}
+                appointmentsTotal={5}
+                userType={'pupil'}
+                meetingLink={'https://www.lern-fair.de/'}
+            />
+        </WithNavigation>
+    );
 };
 
 export default SingleAppointment;
