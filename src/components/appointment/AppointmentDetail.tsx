634eba245ef6372b5ef312fcf0ed845e98f8f21e
Lomy <salome.wick@typedigital.de>
2023-01-16 11:29:40 +0100

    get data dynamicaly

--- src/components/appointment/AppointmentDetail.tsx
+++ src/components/appointment/AppointmentDetail.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Divider, Heading, HStack, Spacer, Stack, Text, useBreakpointValue, VStack } from 'native-base';
+import { Avatar, Box, Button, Divider, Heading, HStack, Spacer, Stack, Text, useBreakpointValue, useTheme, useToast, VStack } from 'native-base';
 import { useTranslation } from 'react-i18next';
 import { useLayoutHelper } from '../../hooks/useLayoutHelper';
 import InformationBadge from '../notifications/preferences/InformationBadge';
@@ -6,101 +6,173 @@ import DateIcon from '../../assets/icons/lernfair/appointments/appointment_date.
 import TimeIcon from '../../assets/icons/lernfair/appointments/appointment_time.svg';
 import PersonIcon from '../../assets/icons/lernfair/appointments/appointment_person.svg';
 import RepeatIcon from '../../assets/icons/lernfair/appointments/appointment_repeat.svg';
+import StudentAvatar from '../../assets/icons/lernfair/avatar_student.svg';
+import PupilAvatar from '../../assets/icons/lernfair/avatar_pupil.svg';
+import { getAppointmentDateTime } from '../../helper/appointment-helper';
+import { useCallback, useState } from 'react';
 
 type AppointmentDetailProps = {
     id: number;
     instructors: string[];
     participants: string[];
     appointmentTitle: string;
-    title: string;
-    description: string;
     startDate: string;
     duration: number;
+    courseTitle?: string;
+    description?: string;
+    appointmentType?: string;
     appointmentsCount?: number;
+    appointmentsTotal?: number;
+    userType?: string;
+    meetingLink?: string;
 };
 const AppointmentDetail: React.FC<AppointmentDetailProps> = ({
     id,
     instructors,
     participants,
     appointmentTitle,
-    title,
-    description,
     startDate,
     duration,
+    courseTitle,
+    description,
+    appointmentType,
     appointmentsCount,
+    appointmentsTotal,
+    userType,
+    meetingLink,
 }) => {
     const { isMobile } = useLayoutHelper();
     const { t } = useTranslation();
+    const toast = useToast();
+    const { sizes } = useTheme();
+    const [canceled, setCanceled] = useState<boolean>(false);
 
     const buttonWidth = useBreakpointValue({
         base: 'full',
         lg: '300',
     });
+
+    const containerWidth = useBreakpointValue({
+        base: 'full',
+        lg: sizes['containerWidth'],
+    });
+
+    const containerMargin = useBreakpointValue({
+        base: 'auto',
+        lg: 5,
+    });
+
+    const cancelAppointment = useCallback(() => {
+        toast.show({ description: 'Termin wurde abgesagt', placement: 'top' });
+        setCanceled(true);
+        // TODO mutation: declinedBy.push(participant)
+    }, []);
+
+    const { date, startTime, endTime } = getAppointmentDateTime(startDate, duration);
+
     return (
-        <Box>
+        <Box marginX={containerMargin} maxW={containerWidth}>
+            {/* Avatare  */}
             <HStack py={5}>
-                <Avatar.Group max={6}>
-                    <Avatar />
-                    <Avatar />
-                    <Avatar />
-                    <Avatar />
-                    <Avatar />
-                    <Avatar />
-                    <Avatar />
-                    <Avatar />
+                <Avatar.Group _avatar={{ size: 'md' }} space={-2} max={5}>
+                    {instructors
+                        ?.map((i) => <Avatar key={i}>{<StudentAvatar style={{ margin: '-1' }} />}</Avatar>)
+                        .concat(participants?.map((i) => <Avatar key={i}>{<PupilAvatar style={{ margin: '-20' }} />}</Avatar>) ?? [])}
                 </Avatar.Group>
             </HStack>
+
+            {/* Header  */}
             <VStack space={2}>
-                <Text color="primary.600">{t('appointments.singleAppointment.group')}</Text>
-                <Heading fontSize="3xl" color="primary.900">
-                    Lektion 3: Algebra
+                <Text color="primary.600" fontWeight="normal">
+                    {t(
+                        appointmentType === 'GROUP'
+                            ? 'appointments.appointmentDetail.group'
+                            : appointmentType === 'ONE_TO_ONE'
+                            ? 'appointments.appointmentDetail.oneToOne'
+                            : 'appointments.appointmentDetail.without',
+                        {
+                            instructor: instructors?.join(', '),
+                        }
+                    )}
+                </Text>
+                <Heading fontSize="3xl" fontWeight="normal" color="primary.900">
+                    {t('appointments.appointmentDetail.appointmentTitle', { appointmentTitle: appointmentTitle })}
                 </Heading>
-                <Text color="primary.600">Kurs: Grundlagen Tutorium</Text>
+                {appointmentType === 'GROUP' && (
+                    <Text color="primary.600" fontWeight="normal">
+                        {t('appointments.appointmentDetail.courseTitle', { courseTitle: courseTitle })}
+                    </Text>
+                )}
             </VStack>
-            <Divider my={3} />
-            <Stack direction={isMobile ? 'column' : 'row'} space={isMobile ? 4 : 7}>
+            <Divider thickness="0.25" my={5} />
+
+            {/* Meta Details  */}
+            <Stack direction={isMobile ? 'column' : 'row'} space={isMobile ? 5 : 7}>
                 <HStack space={2} alignItems="center">
                     <DateIcon />
-                    <Text>02. Januar 2023</Text>
+                    <Text fontWeight="normal">{date}</Text>
                 </HStack>
                 <HStack space={2} alignItems="center">
                     <TimeIcon />
-                    <Text>10:00 - 11:00 Uhr (60 Minuten)</Text>
+                    <Text fontWeight="normal">{t('appointments.appointmentDetail.time', { start: startTime, end: endTime, duration: duration })}</Text>
                 </HStack>
                 <HStack space={2} alignItems="center">
                     <RepeatIcon />
-                    <Text>3. von insgesamt 5 Terminen</Text>
+                    <Text fontWeight="normal">
+                        {t('appointments.appointmentDetail.repeatDate', { appointmentCount: appointmentsCount, appointmentsTotal: appointmentsTotal })}
+                    </Text>
                 </HStack>
                 <HStack space={2} alignItems="center">
                     <PersonIcon />
-                    <Text> 4 Teilnehmer:innen</Text>
+                    <Text fontWeight="normal">
+                        {t('appointments.appointmentDetail.participants', { participantsTotal: participants.length + instructors.length })}
+                    </Text>
                     <InformationBadge />
                 </HStack>
             </Stack>
             <Spacer py={3} />
-            <Button width={buttonWidth}>Jetzt Videochat beitreten</Button>
-            <Divider my={3} />
-            <VStack p={3}>
-                <Text color="primary.900" mb="2">
-                    Kursbeschreibung: Grundlagen
-                </Text>
-                <Text color="primary.600">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-                    diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-                </Text>
-            </VStack>
-            <Divider my={3} />
-            <Stack direction={isMobile ? 'column' : 'row'} space={3}>
-                <Button _text={{ color: 'white' }} bgColor="amber.700" width={buttonWidth}>
-                    Termin löschen
-                </Button>
-                {/* <Button _text={{ color: 'white' }} bgColor="amber.700" width={buttonWidth}>
-                    Termin absagen
-                </Button> */}
+            <Button width={buttonWidth} isDisabled={meetingLink ? false : true} onPress={() => window.open(meetingLink, '_blank')}>
+                {t('appointments.appointmentDetail.videochatButton')}
+            </Button>
 
-                <Button variant="outline" width={buttonWidth}>
-                    Termin bearbeiten
-                </Button>
+            {/* Description */}
+            <Divider thickness="0.25" my={5} />
+            {description && (
+                <>
+                    <VStack p={3}>
+                        <Text color="primary.900" mb="2">
+                            {t(
+                                appointmentType === 'GROUP'
+                                    ? 'appointments.appointmentDetail.courseDescriptionHeader'
+                                    : 'appointments.appointmentDetail.desciptionHeader',
+                                { courseTitle: courseTitle }
+                            )}
+                        </Text>
+                        <Text color="primary.600" fontWeight="normal">
+                            {description}
+                        </Text>
+                    </VStack>
+                    <Divider thickness="0.25" my={5} />
+                </>
+            )}
+
+            {/* Button Section */}
+            <Stack direction={isMobile ? 'column' : 'row'} space={3}>
+                {userType === 'student' && (
+                    <>
+                        <Button _text={{ color: 'white' }} bgColor="amber.700" width={buttonWidth}>
+                            {t('appointments.appointmentDetail.deleteButton')}
+                        </Button>
+                        <Button variant="outline" width={buttonWidth}>
+                            {t('appointments.appointmentDetail.adjustButton')}
+                        </Button>
+                    </>
+                )}
+                {userType === 'pupil' && (
+                    <Button _text={{ color: 'white' }} bgColor="amber.700" width={buttonWidth} onPress={cancelAppointment} isDisabled={canceled}>
+                        {t('appointments.appointmentDetail.cancelButton')}
+                    </Button>
+                )}
             </Stack>
         </Box>
     );
