b50743dda665c478be90c05f3ecf2787f3f40239
Lomy <salome.wick@typedigital.de>
2023-01-16 11:03:48 +0100

    add i18n to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,7 @@ module.exports = {
         // Thus we enforce a unified usage here:
         '../src/**/*.stories.mdx',
     ],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-react-i18next'],
     framework: '@storybook/react',
     core: {
         builder: '@storybook/builder-webpack5',
