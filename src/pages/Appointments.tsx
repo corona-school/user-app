1ba9938cab2f14c29e9419425675e0c95e3c5b49
Lomy <salome.wick@typedigital.de>
2023-01-26 10:31:57 +0100

    conect add button with page

--- src/pages/Appointments.tsx
+++ src/pages/Appointments.tsx
@@ -1,6 +1,7 @@
 import { useBreakpointValue, useTheme, VStack } from 'native-base';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import AsNavigationItem from '../components/AsNavigationItem';
 import NotificationAlert from '../components/notifications/NotificationAlert';
 import WithNavigation from '../components/WithNavigation';
@@ -9,6 +10,7 @@ import FloatingActionButton from '../widgets/FloatingActionButton';
 import Hello from '../widgets/Hello';
 
 const Appointments: React.FC = () => {
+    const navigate = useNavigate();
     const { space, sizes } = useTheme();
     const ContainerWidth = useBreakpointValue({
         base: '100%',
@@ -24,7 +26,7 @@ const Appointments: React.FC = () => {
     return (
         <AsNavigationItem path="appointments">
             <WithNavigation headerContent={<Hello />} headerTitle={t('appointment.title')} headerLeft={<NotificationAlert />}>
-                <FloatingActionButton handlePress={() => console.log('Add new appointment')} place={fabPlace} />
+                <FloatingActionButton handlePress={() => navigate('/create-appointment')} place={fabPlace} />
                 <VStack maxWidth={ContainerWidth} marginBottom={space['1']}>
                     <AppointmentList />
                 </VStack>
