ffe02b808836c9a33a9ca4bbc2c2261249e1ccce
Jonas Wilms <unknown@example.org>
2023-02-05 18:18:17 +0100

    Remove subject settings page

--- src/NavigatorLazy.tsx
+++ src/NavigatorLazy.tsx
@@ -4,7 +4,6 @@ import ProfilePupil from './pages/pupil/ProfilePupil';
 
 import Settings from './pages/Settings';
 import OnboardingTourList from './pages/OnboardingTourList';
-import ChangeSettingSubject from './pages/change-setting/ChangeSettingSubject';
 import HelpCenter from './pages/Helpcenter';
 import ChangeSettingSchoolType from './pages/change-setting/ChangeSettingSchoolType';
 import ChangeSettingState from './pages/change-setting/ChangeSettingState';
@@ -108,7 +107,6 @@ export default function NavigatorLazy() {
                     </RequireAuth>
                 }
             >
-                <Route path="subjects" element={<ChangeSettingSubject />} />
                 <Route path="school-type" element={<ChangeSettingSchoolType />} />
                 <Route path="state" element={<ChangeSettingState />} />
                 <Route path="language" element={<ChangeSettingLanguage />} />
