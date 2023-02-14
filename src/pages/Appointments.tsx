465b12492a88fe311653d886750034a0c7dd0c0a
Lomy <salome.wick@typedigital.de>
2023-02-14 07:31:03 +0100

    add myappointments query, courseAppointments query

--- src/pages/Appointments.tsx
+++ src/pages/Appointments.tsx
@@ -9,9 +9,27 @@ import AddAppointmentButton from '../widgets/AddAppointmentButton';
 import Hello from '../widgets/Hello';
 import { useUserType } from '../hooks/useApollo';
 import AppointmentList from '../widgets/appointment/AppointmentList';
+import { gql } from '../gql';
+import { useQuery } from '@apollo/client';
 
+// TODO get my appointments and pass data to AppointmentList
+const myAppointmentsQuery = gql(`
+    query myAppointments{
+        me{
+            appointments(take: 100){
+                id
+                title
+                description
+                start
+                duration
+                subcourseId
+        }
+        }
+    }
+`);
 const Appointments: React.FC = () => {
     const userType = useUserType();
+    // const {data, loading} = useQuery(myAppointmentsQuery)
 
     const navigate = useNavigate();
     const { space, sizes } = useTheme();
--- src/pages/create-appointment/AppointmentsInsight.tsx
+++ src/pages/create-appointment/AppointmentsInsight.tsx
@@ -1,17 +1,38 @@
 import { Box, Button, Stack, useBreakpointValue } from 'native-base';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useQuery } from '@apollo/client';
 import { useLayoutHelper } from '../../hooks/useLayoutHelper';
 import AppointmentList from '../../widgets/appointment/AppointmentList';
+import { gql } from '../../gql';
 
 type Props = {
     next: () => void;
     back: () => void;
 };
 
+// TODO add query to get appointments for course
+const query = gql(`
+    query courseLectures($subcourseId: Int!) {
+        subcourse(subcourseId: $subcourseId) {
+            lectures {
+                id
+                title
+                description
+                start
+                duration
+                appointmentType
+            }
+        }
+    }
+`);
+
 const AppointmentsInsight: React.FC<Props> = ({ next, back }) => {
     const { t } = useTranslation();
     const { isMobile } = useLayoutHelper();
+
+    // TODO get data
+    // const {data, loading} = useQuery(query, {variables: {courseId}})
 
     const maxHeight = useBreakpointValue({
         base: 400,
