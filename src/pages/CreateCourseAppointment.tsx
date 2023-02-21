45dca33f21066e0a51eb53d1ec119eea3f3669ae
Lomy <salome.wick@typedigital.de>
2023-02-21 13:11:22 +0100

    Add create appointment provider

--- src/pages/CreateCourseAppointment.tsx
+++ src/pages/CreateCourseAppointment.tsx
@@ -3,20 +3,22 @@ import { useNavigate } from 'react-router-dom';
 import AsNavigationItem from '../components/AsNavigationItem';
 import NotificationAlert from '../components/notifications/NotificationAlert';
 import WithNavigation from '../components/WithNavigation';
+import { CreateAppointmentProvider } from '../context/AppointmentContext';
 import AppointmentCreation from './create-appointment/AppointmentCreation';
 
 const CreateCourseAppointment = () => {
     const navigate = useNavigate();
 
-    // TODO go back to /create-course and setCurrentIndex to 1
     return (
-        <AsNavigationItem path="create-course-appointment">
-            <WithNavigation headerTitle="Termin erstellen" showBack headerLeft={<NotificationAlert />}>
-                <Box mx="4">
-                    <AppointmentCreation back={() => navigate('/create-course', { state: { currentStep: 1 } })} />
-                </Box>
-            </WithNavigation>
-        </AsNavigationItem>
+        <CreateAppointmentProvider>
+            <AsNavigationItem path="create-course-appointment">
+                <WithNavigation headerTitle="Termin erstellen" showBack headerLeft={<NotificationAlert />}>
+                    <Box mx="4">
+                        <AppointmentCreation back={() => navigate('/create-course', { state: { currentStep: 1 } })} />
+                    </Box>
+                </WithNavigation>
+            </AsNavigationItem>
+        </CreateAppointmentProvider>
     );
 };
 
