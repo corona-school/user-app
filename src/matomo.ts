2faaaaeb268dad637926b3459db9d478877a24dc
Leo310 <leodev310@gmail.com>
2023-03-12 13:31:24 +0100

    Update: siteID from 3 to 6

--- src/matomo.ts
+++ src/matomo.ts
@@ -1,18 +1,15 @@
-import { createInstance } from '@jonkoops/matomo-tracker-react'
-import { DISABLE_MATOMO } from './config'
+import { createInstance } from '@jonkoops/matomo-tracker-react';
+import { DISABLE_MATOMO } from './config';
 
 export default createInstance({
-  urlBase: process.env.REACT_APP_MATOMO_URL,
-  siteId: 3,
-  disabled:
-    process.env.NODE_ENV === 'development' ||
-    process.env.NODE_ENV === 'test' ||
-    DISABLE_MATOMO === 'true',
+    urlBase: process.env.REACT_APP_MATOMO_URL,
+    siteId: 6,
+    disabled: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test' || DISABLE_MATOMO === 'true',
     configurations: {
-      "setCookieDomain": ["*.lern-fair.de"],
-      "setDomains": ["*.lern-fair.de","*.app.lern-fair.de"],
-      "enableCrossDomainLinking": [],
-      "setExcludedQueryParams": ["token", "secret_token", "legacyToken", "redirectTo"],
-      "disableCookies": [],
+        setCookieDomain: ['*.lern-fair.de'],
+        setDomains: ['*.lern-fair.de', '*.app.lern-fair.de'],
+        enableCrossDomainLinking: [],
+        setExcludedQueryParams: ['token', 'secret_token', 'legacyToken', 'redirectTo'],
+        disableCookies: [],
     },
-})
+});
