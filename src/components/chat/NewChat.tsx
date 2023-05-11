3b0e51729ef5e5734b968aa890b4a38e81687e1f
Lomy <salome.wick@typedigital.de>
2023-05-11 09:57:29 +0200

    add chat context and useChat hook

--- src/components/chat/NewChat.tsx
+++ src/components/chat/NewChat.tsx
@@ -1,5 +1,8 @@
 const NewChat = () => {
     // TODO list all conversations, which a user can start, depending on contact options
+    // TODO add query to get my contacts
+    // TODO add mutation to create a convo
+
     return <div>NewChat</div>;
 };
 
--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
+import Talk from 'talkjs';
+import useApollo from '../hooks/useApollo';
 
-const ChatContext = () => {
-    // TODO create session
-    // * getOrCreateConversation
-    return <div>ChatContext</div>;
+type IChatContext = {
+    session: Talk.Session | null;
+    talkLoaded: boolean;
+};
+const ChatContext = createContext<IChatContext>({
+    session: null,
+    talkLoaded: false,
+});
+
+export const LFChatProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [session, setSession] = useState<Talk.Session | null>(null);
+    const [talkLoaded, markTalkLoaded] = useState<boolean>(false);
+    // TODO get userID from logged in user
+    // * const userType = useApollo();
+    // TODO add query for the current user
+    // TODO get signature from BE
+    // TODO add query to get has unread messages
+
+    const me = {
+        id: '123',
+        name: 'Lomy',
+        role: 'student',
+        email: 'salome.wick@typedigital.de',
+    };
+
+    useEffect(() => {
+        Talk.ready.then(() => markTalkLoaded(true));
+
+        if (talkLoaded) {
+            const currentUser = new Talk.User(me);
+
+            const session = new Talk.Session({
+                appId: 't5NarFaG',
+                me: currentUser,
+                // signature:
+            });
+            setSession(session);
+            return () => session.destroy();
+        }
+    }, [talkLoaded]);
+
+    return <ChatContext.Provider value={{ session, talkLoaded }}>{children}</ChatContext.Provider>;
 };
 
-export default ChatContext;
+export const useChat = () => {
+    const { session, talkLoaded } = useContext(ChatContext);
+    return { session, talkLoaded };
+};
