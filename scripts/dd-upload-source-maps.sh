3a44e62bd9f78a88e10283c7751303367fc9b961
Daniel Henkel <daniel@henkel.tech>
2023-06-18 19:11:23 +0200

    test smth

---
+++ scripts/dd-upload-source-maps.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+set -eo pipefail
+
+appVersion=${HEROKU_SLUG_COMMIT:-latest}
+env=${DD_ENV:-dev}
+serviceName=${SERVICE_NAME:-user-app}
+
+
+# RUM should not be active in our review apps
+if [ "$env" == "dev" ]; then
+	echo "Not uploading source maps in dev"
+	# exit 0
+fi
+
+pathPrefix="http://localhost:3000"
+case "$env" in
+    "dev")
+				pathPrefix="https://user-app-include-dd-rum-kbporw.herokuapp.com"
+        ;;
+    "staging")
+				pathPrefix="https://lernfair-user-app-dev.herokuapp.com"
+        ;;
+    "production")
+				pathPrefix="https://app.lern-fair.de"
+        ;;
+esac
+
+echo "Running upload: env: $env, serviceName: $serviceName, appVersion: $appVersion, pathPrefix: $pathPrefix"
+./node_modules/.bin/datadog-ci sourcemaps upload ./build --service $serviceName --release-version $appVersion --minified-path-prefix $pathPrefix --repository-url https://github.com/corona-school/user-app --disable-git
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './I18n';
+import { datadogRum } from '@datadog/browser-rum';
+import { APP_VERSION, DD_APP_ID, DD_CLIENT_TOKEN, DD_ENV } from './config';
+
 const root = document.getElementById('root');
 
-console.log('LernFair Web App Version', `${process.env.REACT_APP_VERSION}`);
+console.log('LernFair Web App Version', APP_VERSION);
+
+datadogRum.init({
+    applicationId: DD_APP_ID,
+    clientToken: DD_CLIENT_TOKEN,
+    site: 'datadoghq.eu',
+    service: 'user-app',
+    env: DD_ENV,
+    // Specify a version number to identify the deployed version of your application in Datadog
+    version: APP_VERSION,
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 100,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    traceSampleRate: 100,
+    defaultPrivacyLevel: 'mask',
+    // TODO: Add proper tracing urls
+    allowedTracingUrls: [() => true],
+    useCrossSiteSessionCookie: false,
+    useSecureSessionCookie: false,
+    allowFallbackToLocalStorage: true,
+});
+
+datadogRum.startSessionReplayRecording();
 
 ReactDOM.render(
     <React.StrictMode>
