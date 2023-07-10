f6508d7c4304cc8f32974bdd8eb67ef36b6fe322
Fabian5150 <fabian.strauch.04@web.de>
2023-07-10 16:56:28 +0200

    remove number of lessons from course tiles

--- src/widgets/AppointmentCard.tsx
+++ src/widgets/AppointmentCard.tsx
@@ -244,17 +244,10 @@ const AppointmentCard: React.FC<Props> = ({
                                     <>
                                         <Row paddingTop="4px" space={1}>
                                             <Text color={textColor}>
-                                                {countCourse && countCourse > 1
-                                                    ? t('single.card.dateLectures', {
-                                                          date: date.toFormat('dd.MM.yyyy'),
-                                                          time: date.toFormat('HH:mm'),
-                                                          count: countCourse,
-                                                      })
-                                                    : t('single.card.oneLecture', {
-                                                          date: date.toFormat('dd.MM.yyyy'),
-                                                          time: date.toFormat('HH:mm'),
-                                                          count: countCourse,
-                                                      })}
+                                                {t('single.card.dateLectures', {
+                                                    date: date.toFormat('dd.MM.yyyy'),
+                                                    time: date.toFormat('HH:mm'),
+                                                })}
                                             </Text>
                                         </Row>
                                     </>
