2e8b788f220d6686cdb4398e869d522a6a7882eb
Jonas <jonas@wilms.ninja>
2023-09-17 14:33:10 +0200

    fix: Use span instead of div for DD Privacy Container

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -118,9 +118,9 @@ const Chat: React.FC = () => {
             >
                 {!isConverstationSelected && <FloatingActionButton handlePress={handleNewChatPress} place={'bottom-right'} icon={<LFAddChatIcon />} />}
 
-                <div className="dd-privacy-hidden">
+                <span className="dd-privacy-hidden">
                     <Box h="90%" pl={isMobile ? 2 : 0} pb={isMobile ? 5 : 0} pr={paddingRight} w={chatWidth} ref={inboxRef} />
-                </div>
+                </span>
                 <Modal isOpen={isContactModalOpen} onClose={onClose}>
                     <ChatContactsModal onClose={onClose} setChatId={(id: string) => setSelectedChatId(id)} />
                 </Modal>
