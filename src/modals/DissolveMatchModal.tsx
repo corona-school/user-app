83d6d2c586dfc972d3675df1bf6930af9f61e7cf
Leo310 <leodev310@gmail.com>
2023-10-15 14:31:34 +0200

    feat(dissolve-match-modal): showing warning modal for first 14 days

--- src/modals/DissolveMatchModal.tsx
+++ src/modals/DissolveMatchModal.tsx
@@ -10,6 +10,7 @@ import UserType from '../pages/registration/UserType';
 
 type DissolveModalProps = {
     showDissolveModal: boolean | undefined;
+    alsoShowWarningModal?: boolean | undefined;
     onPressDissolve: (dissolveReason: string) => any;
     onPressBack: () => any;
 };
@@ -19,8 +20,9 @@ type DissolveModalProps = {
 export const studentReasonOptions = new Array(9).fill(0);
 export const pupilReasonOptions = new Array(9).fill(0);
 
-const DissolveMatchModal: React.FC<DissolveModalProps> = ({ showDissolveModal, onPressDissolve, onPressBack }) => {
+const DissolveMatchModal: React.FC<DissolveModalProps> = ({ showDissolveModal, alsoShowWarningModal, onPressDissolve, onPressBack }) => {
     const [currentIndex, setCurrentIndex] = useState<number>(0);
+    const [showedWarning, setShowedWarning] = useState<boolean>(false);
     const { t } = useTranslation();
     const navigate = useNavigate();
     const { space } = useTheme();
@@ -35,28 +37,45 @@ const DissolveMatchModal: React.FC<DissolveModalProps> = ({ showDissolveModal, o
                 <Modal.CloseButton />
                 {currentIndex === 0 && (
                     <>
-                        <Modal.Header>{t('matching.dissolve.modal.title')}</Modal.Header>
-                        <Modal.Body>
-                            <Radio.Group name="dissolve-reason" value={reason} onChange={setReason}>
-                                <VStack space={space['1']}>
-                                    {reasons.map((_: number, index: number) => (
-                                        <Radio key={index} value={`${index + 1}`}>
-                                            {t(`matching.dissolveReasons.${userType}.${index + 1}` as unknown as TemplateStringsArray)}
-                                        </Radio>
-                                    ))}
-                                </VStack>
-                            </Radio.Group>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Row space={space['1']}>
-                                <Button isDisabled={!reason} onPress={() => onPressDissolve(reason)}>
-                                    {t('matching.dissolve.modal.btn')}
-                                </Button>
-                                <Button onPress={onPressBack} variant="ghost">
-                                    {t('back')}
-                                </Button>
-                            </Row>
-                        </Modal.Footer>
+                        {alsoShowWarningModal && !showedWarning ? (
+                            <>
+                                <Modal.Header>{t('matching.dissolve.warningModal.title')}</Modal.Header>
+                                <Modal.Body>{t('matching.dissolve.warningModal.body')}</Modal.Body>
+                                <Modal.Footer>
+                                    <Row space={space['1']}>
+                                        <Button onPress={onPressBack} variant="ghost">
+                                            {t('back')}
+                                        </Button>
+                                        <Button onPress={() => setShowedWarning(true)}>{t('matching.dissolve.warningModal.btn')}</Button>
+                                    </Row>
+                                </Modal.Footer>
+                            </>
+                        ) : (
+                            <>
+                                <Modal.Header>{t('matching.dissolve.modal.title')}</Modal.Header>
+                                <Modal.Body>
+                                    <Radio.Group name="dissolve-reason" value={reason} onChange={setReason}>
+                                        <VStack space={space['1']}>
+                                            {reasons.map((_: number, index: number) => (
+                                                <Radio key={index} value={`${index + 1}`}>
+                                                    {t(`matching.dissolveReasons.${userType}.${index + 1}` as unknown as TemplateStringsArray)}
+                                                </Radio>
+                                            ))}
+                                        </VStack>
+                                    </Radio.Group>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Row space={space['1']}>
+                                        <Button isDisabled={!reason} onPress={() => onPressDissolve(reason)}>
+                                            {t('matching.dissolve.modal.btn')}
+                                        </Button>
+                                        <Button onPress={onPressBack} variant="ghost">
+                                            {t('back')}
+                                        </Button>
+                                    </Row>
+                                </Modal.Footer>
+                            </>
+                        )}
                     </>
                 )}
                 {/* TODO: move dissolve logic in this modal.*/}
