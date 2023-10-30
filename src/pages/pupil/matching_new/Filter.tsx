a9562d9836c3f31f232472d8c2917c3895d6a1c2
Fabian5150 <fabian.strauch.04@web.de>
2023-10-30 17:47:53 +0100

    alternative offers 2 diffrent pages

--- src/pages/pupil/matching_new/Filter.tsx
+++ src/pages/pupil/matching_new/Filter.tsx
@@ -71,26 +71,54 @@ const Filter: React.FC = () => {
                 <VStack space={space['0.5']}>
                     <Heading fontSize="2xl">Alternative Angebote</Heading>
 
-                    <Text>Leider können wir dich derzeit nicht mit einem:r Lernpartner:in versorgen.</Text>
-
-                    <Heading fontSize="md" mt={space['1']}>
-                        Kennst du schon unsere Gruppen-Kurse?
-                    </Heading>
-
                     <Text>
-                        Vielleicht ist hier etwas Passendes für dich dabei. Es lohnt sich öfter vorbeizuschauen, denn wir laden regelmäßig neue Kurse hoch.
-                        Alternativ kannst du dich auf edu-cloud.org umsehen. Dort findest du viele andere digitale Plattformen, die dich beim Lernen
-                        unterstützen können.
+                        Leider können wir dich bei Lern-Fair in diesem Fall nicht unterstützen. Unsere Hilfe wird von freiwilligen Helfer:innen angeboten.
+                        Deshalb haben wir nur begrenzte Plätze und möchten vor allem denjenigen Schüler:innen helfen, die folgende Kriterien erfüllen:
                     </Text>
 
-                    <Text>Bei Fragen kannst du dich gerne jederzeit bei uns melden.</Text>
+                    <Text>● Du brauchst Hilfe beim Lernen.</Text>
+                    <Text>● Du hast Nachteile beim Lernen.</Text>
+                    <Text>● Deine Familie kann dir nicht beim Lernen helfen.</Text>
+                    <Text>● Deine Familie kann keine Nachhilfe für dich bezahlen.</Text>
 
-                    <NextPrevButtons
-                        altPrevText="Zur Edu-Cloud"
-                        altNextText="Zu den Gruppen-Kursen"
-                        onPressPrev={() => window.open('https://edu-cloud.org', '_blank')}
-                        onPressNext={() => navigate('/group')}
-                    />
+                    {isFit === 'no' ? (
+                        <>
+                            <Heading fontSize="md" mt={space['1']}>
+                                Kennst du schon Edu-Cloud?
+                            </Heading>
+
+                            <Text>
+                                Dort findest du viele Angebote, die für dich interessant sein können. Bei Fragen kannst du dich gerne jederzeit bei uns melden.
+                            </Text>
+
+                            <NextPrevButtons
+                                altPrevText="Zur Edu-Cloud"
+                                altNextText="Zu den Gruppen-Kursen"
+                                onPressPrev={() => window.open('https://edu-cloud.org', '_blank')}
+                                onPressNext={() => navigate('/group')}
+                            />
+                        </>
+                    ) : (
+                        <>
+                            <Heading fontSize="md" mt={space['1']}>
+                                Kennst du schon unsere Hausaufgabenhilfe?
+                            </Heading>
+
+                            <Text>
+                                Du hast eine Frage zu deinen Hausaufgaben? Zwischen 16:00 und 17:00 Uhr sind wir vom Lern-Fair Team auf Zoom und können dir bei
+                                deinen Hausaufgaben helfen.
+                            </Text>
+
+                            <Text>Bei Fragen kannst du dich gerne jederzeit bei uns melden.</Text>
+
+                            <NextPrevButtons
+                                altPrevText="Zu den Gruppen-Kursen"
+                                altNextText="Zur Hausaufgabenhilfe"
+                                onPressPrev={() => navigate('/group')}
+                                onPressNext={() => window.open('https://www.lern-fair.de/hausaufgabenhilfe-anmeldung', '_blank')}
+                            />
+                        </>
+                    )}
                 </VStack>
             )}
         </VStack>
