888a1b2ec334fd552caba214d12ded47d14650f5
Jonas <jonas@wilms.ninja>
2023-11-04 23:24:11 +0100

    Replace translation files with single JSON file for easier automated translation

--- src/lang/welcome/de.ts
+++
@@ -1,9 +0,0 @@
-const welcome = {
-    title: 'Herzlich Willkommen bei Lern-Fair',
-    subtitle: 'Hast du bereits einen Account?\nOder bist du neu bei uns und möchtest dich registrieren?',
-    deactivationAlert: 'Dein Account wurde deaktiviert',
-    needHelp: 'Brauchst du Hilfe?',
-    contactSupport: 'Kontaktiere uns',
-};
-
-export default welcome;
