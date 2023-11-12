1e3907abf8456e76b89ba83668c9c99f4e164ad7
Jonas <jonas@wilms.ninja>
2023-11-12 15:06:42 +0100

    Fix styling of TextInputWithSuggestions

--- src/components/TextInputWithSuggestions.tsx
+++ src/components/TextInputWithSuggestions.tsx
@@ -2,11 +2,11 @@ import { useTheme, VStack, HStack, Text } from 'native-base';
 import { TextInput, Pressable } from 'react-native';
 
 export function TextInputWithSuggestions({ value, setValue, suggestions }: { value: string; setValue: (it: string) => void; suggestions: string[] }) {
-    const { space } = useTheme();
+    const { space, colors } = useTheme();
 
     return (
         <VStack>
-            <TextInput value={value} onChangeText={setValue} />
+            <TextInput style={{ backgroundColor: colors.primary['100'], fontSize: '15px', padding: space['2'] }} value={value} onChangeText={setValue} />
             <HStack paddingTop={space['1']} space={space['1']} display="flex" flexWrap="wrap">
                 {suggestions.map((it) => (
                     <Pressable onPress={() => setValue(it)}>
