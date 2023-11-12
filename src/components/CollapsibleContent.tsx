5c4ba80327ce02340652e349eb33bd5f93793e89
Jonas <jonas@wilms.ninja>
2023-11-12 17:15:17 +0100

    First translations

--- src/components/CollapsibleContent.tsx
+++ src/components/CollapsibleContent.tsx
@@ -1,5 +1,6 @@
 import { ChevronDownIcon, ChevronUpIcon, Heading, Pressable, Row, useTheme, VStack } from 'native-base';
 import { ReactNode, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
     isOpen?: boolean;
@@ -11,6 +12,7 @@ type Props = {
 
 const CollapsibleContent: React.FC<Props> = ({ children, isOpen, textColor, header, onPressHeader }) => {
     const { space, colors } = useTheme();
+    const { t } = useTranslation();
 
     const iconColor = useMemo(() => (isOpen ? 'lightText' : 'primary.900'), [isOpen]);
 
@@ -20,7 +22,7 @@ const CollapsibleContent: React.FC<Props> = ({ children, isOpen, textColor, head
                 <Pressable onPress={onPressHeader}>
                     <Row alignItems="center" borderBottomWidth="1" borderColor={colors['primary']['500']}>
                         <Heading fontSize="md" pt={space['1']} pb={space['0.5']} flex="1" color={textColor}>
-                            Weitere Details
+                            {t('further_details')}
                         </Heading>
                         {isOpen ? <ChevronUpIcon color={iconColor} /> : <ChevronDownIcon color={iconColor} />}
                     </Row>
