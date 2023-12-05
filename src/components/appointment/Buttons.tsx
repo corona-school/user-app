59f416dacd896dfe0181593bf38c99f5106fe1f8
Fabian5150 <fabian.strauch.04@web.de>
2023-12-05 18:49:02 +0100

    Add DisablebleButton to appointment/Buttons and refactor

--- src/components/appointment/Buttons.tsx
+++ src/components/appointment/Buttons.tsx
@@ -1,19 +1,17 @@
-import { Button, Stack, Tooltip, useBreakpointValue } from 'native-base';
+import { Button, Stack, useBreakpointValue } from 'native-base';
 import { useTranslation } from 'react-i18next';
 import useApollo from '../../hooks/useApollo';
 import { useLayoutHelper } from '../../hooks/useLayoutHelper';
-import AlertMessage from '../../widgets/AlertMessage';
+import DisablebleButton from '../DisablebleButton';
 
 type AvatarsProps = {
     onPress: () => void;
     onEditPress: () => void;
     canceled: boolean;
-    declined: boolean;
-    canEdit: boolean;
     isOver: boolean;
     isLast: boolean;
 };
-const Buttons: React.FC<AvatarsProps> = ({ onPress, onEditPress, canceled, declined, canEdit, isOver, isLast }) => {
+const Buttons: React.FC<AvatarsProps> = ({ onPress, onEditPress, canceled, isOver, isLast }) => {
     const { isMobile } = useLayoutHelper();
     const { t } = useTranslation();
     const { user } = useApollo();
@@ -25,30 +23,51 @@ const Buttons: React.FC<AvatarsProps> = ({ onPress, onEditPress, canceled, decli
 
     return (
         <>
-            {isOver && <AlertMessage content={t('appointment.detail.editBlockedWarning')} />}
             <Stack direction={isMobile ? 'column' : 'row'} space={3}>
                 {user?.student && (
                     <>
-                        <Tooltip label={t('appointment.detail.deleteTooltip')} isDisabled={!isLast}>
-                            <Button
-                                _text={{ color: 'white' }}
-                                onPress={onPress}
-                                bgColor="danger.100"
-                                width={buttonWidth}
-                                isDisabled={canEdit || isOver || isLast}
-                            >
-                                {t('appointment.detail.deleteButton')}
-                            </Button>
-                        </Tooltip>
-                        <Button variant="outline" width={buttonWidth} onPress={onEditPress} isDisabled={canEdit || isOver}>
+                        <DisablebleButton
+                            isDisabled={isOver || isLast}
+                            reasonDisabled={
+                                isOver ? t('appointment.detail.reasonDisabled.deleteBtn.isOver') : t('appointment.detail.reasonDisabled.deleteBtn.isLast')
+                            }
+                            buttonProps={{
+                                _text: { color: 'white' },
+                                onPress: onPress,
+                                bgColor: 'danger.100',
+                                width: buttonWidth,
+                            }}
+                        >
+                            {t('appointment.detail.deleteButton')}
+                        </DisablebleButton>
+                        <DisablebleButton
+                            isDisabled={isOver}
+                            reasonDisabled={t('appointment.detail.reasonDisabled.editBtn.isOver')}
+                            buttonProps={{
+                                variant: 'outline',
+                                width: buttonWidth,
+                                onPress: onEditPress,
+                            }}
+                        >
                             {t('appointment.detail.editButton')}
-                        </Button>
+                        </DisablebleButton>
                     </>
                 )}
                 {user?.pupil && (
-                    <Button _text={{ color: 'white' }} bgColor="danger.100" width={buttonWidth} onPress={onPress} isDisabled={canceled || declined || isOver}>
+                    <DisablebleButton
+                        isDisabled={canceled || isOver}
+                        reasonDisabled={
+                            isOver ? t('appointment.detail.reasonDisabled.cancelBtn.isOver') : t('appointment.detail.reasonDisabled.cancelBtn.isCancelled')
+                        }
+                        buttonProps={{
+                            _text: { color: 'white' },
+                            onPress: onPress,
+                            bgColor: 'danger.100',
+                            width: buttonWidth,
+                        }}
+                    >
                         {t('appointment.detail.cancelButton')}
-                    </Button>
+                    </DisablebleButton>
                 )}
             </Stack>
         </>
