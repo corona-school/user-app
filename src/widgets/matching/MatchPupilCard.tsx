96771cb8aa76c3feb7f7d509738c1fc53d787d08
realmayus <realmayus@gmail.com>
2024-01-04 11:45:07 +0100

    Implement support for multiple dissolve reasons

--- src/widgets/matching/MatchPupilCard.tsx
+++ src/widgets/matching/MatchPupilCard.tsx
@@ -82,8 +82,12 @@ export function MatchPupilCard({ match }: { match: MatchWithPupil }) {
                             </Text>
                         )}
                         {match!.dissolvedBy && <Text>{t('screening.dissolved_by', { dissolver: labelForDissolver(match!.dissolvedBy) })}</Text>}
-                        {match!.dissolveReasonEnum && (
-                            <Text>{t('screening.dissolve_reason', { reason: reasonForDissolver(match!.dissolvedBy!, match!.dissolveReasonEnum) })}</Text>
+                        {match!.dissolveReasons?.length !== 0 && (
+                            <>
+                                {match!.dissolveReasons.map((reason) => (
+                                    <Text>{t('screening.dissolve_reason', { reason: reasonForDissolver(match!.dissolvedBy!, reason) })}</Text>
+                                ))}
+                            </>
                         )}
                     </VStack>
                 </HStack>
