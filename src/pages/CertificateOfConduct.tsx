129325d8cf149506b9f7380fa6489aaf5ee929dd
LomyW <salome.wick@typedigital.de>
2024-01-12 11:38:42 +0100

    add download permission

--- src/pages/CertificateOfConduct.tsx
+++ src/pages/CertificateOfConduct.tsx
@@ -1,13 +1,14 @@
-import { Box, Button, Circle, HStack, Heading, Link, Stack, Text, Tooltip } from 'native-base';
+import { Box, Button, Circle, HStack, Heading, Stack, Text, Tooltip } from 'native-base';
 import WithNavigation from '../components/WithNavigation';
 import HelpNavigation from '../components/HelpNavigation';
 import NotificationAlert from '../components/notifications/NotificationAlert';
 import { gql } from '../gql';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import { DateTime } from 'luxon';
 import AlertMessage from '../widgets/AlertMessage';
 import { Trans, useTranslation } from 'react-i18next';
 import useApollo from '../hooks/useApollo';
+import { BACKEND_URL } from '../config';
 
 export const COC_DATE_QUERY = gql(`
 query GetCocDate {
@@ -20,11 +21,17 @@ query GetCocDate {
 `);
 
 const CertificateOfConduct = () => {
+    const { t } = useTranslation();
+    const { user } = useApollo();
     const { data } = useQuery(COC_DATE_QUERY);
     const cocDate = data?.me?.student?.certificateOfConductDeactivationDate;
 
-    const { t } = useTranslation();
-    const { user } = useApollo();
+    const [downloadRemissionRequest] = useMutation(gql(`mutation DownloadRemissionRequest { studentGetRemissionRequestAsPDF }`));
+
+    async function openRemissionRequest() {
+        const { data } = await downloadRemissionRequest();
+        window.open(BACKEND_URL + data!.studentGetRemissionRequestAsPDF, '_blank');
+    }
 
     return (
         <>
@@ -43,7 +50,6 @@ const CertificateOfConduct = () => {
                         <Text>{t('certificateOfConduct.description')}</Text>
                         <HStack space={2}>
                             <Text>
-                                {' '}
                                 <Trans>{t('certificateOfConduct.deadline', { cocDate: DateTime.fromISO(cocDate).toFormat('dd.MM.yyyy') })}</Trans>
                             </Text>
                             <Text></Text>
@@ -57,7 +63,9 @@ const CertificateOfConduct = () => {
                     </Stack>
                     ´
                     <HStack space={4} mb="10">
-                        <Button variant="outline">{t('certificateOfConduct.download')}</Button>
+                        <Button variant="outline" onPress={openRemissionRequest}>
+                            {t('certificateOfConduct.download')}
+                        </Button>
                         <Button
                             onPress={() =>
                                 (window.location.href = `mailto:fz@lern-fair.de?subject=F%C3%BChrungszeugnis%3A%20${user?.firstname}%20${user?.lastname}&body=Liebes%20Lern-Fair%20Team%2C%0D%0A%0D%0Aanbei%20finet%20ihr%20das%20erweiterte%20F%C3%BChrungszeugnis%2C%20welches%20ich%20gem%C3%A4%C3%9F%20der%20Anforderungen%20beantragt%20habe.%20Ich%20bedanke%20mich%20f%C3%BCr%20die%20Unterst%C3%BCtzung%20und%20freue%20mich%2C%20wenn%20ihr%20den%20Erhalt%20kurz%20best%C3%A4tigen%20k%C3%B6nnt.%0D%0A%0D%0ALiebe%20Gr%C3%BC%C3%9Fe%2C%0D%0A${user?.firstname}%0D%0A${user?.email}%0D%0A%0D%0A`)
