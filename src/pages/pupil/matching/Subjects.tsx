84eadcc0c8cef562690067205c405dbdffd8e9b4
Fabian5150 <fabian.strauch.04@web.de>
2024-01-14 13:05:46 +0100

    combine two useEffects into one

--- src/pages/pupil/matching/Subjects.tsx
+++ src/pages/pupil/matching/Subjects.tsx
@@ -16,11 +16,8 @@ const Subjects: React.FC = () => {
 
     useEffect(() => {
         setSkippedSubjectPriority(isDAZ || matchRequest.subjects.length === 1);
-    }, [matchRequest.subjects.length, isDAZ]);
-
-    useEffect(() => {
         matchRequest.subjects.forEach((subj) => setSubjectPriority(subj.name, skippedSubjectPriority));
-    }, [setSubjectPriority, skippedSubjectPriority]);
+    }, [matchRequest.subjects.length, isDAZ, setSkippedSubjectPriority, setSubjectPriority, skippedSubjectPriority]);
 
     return (
         <VStack paddingX={space['1']} space={space['0.5']}>
