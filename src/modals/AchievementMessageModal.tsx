7cc5369f32f50efc42ff0872b08540737b50f6f8
LomyW <115979086+LomyW@users.noreply.github.com>
2024-01-17 15:38:51 +0100

    feat: open modal new achievement message (#433)
    # Description
    If a user clicks on a new achievement message the actual achievement
    modal should be pop up. The navigateTo in the DB (`mesageTranslation`)
    has to be `achievement/{{achievement.id}}`
    - Closes https://github.com/corona-school/project-user/issues/1063
    
    ## 👀 What was done?
    - [x] added an "intermediate" component to query achievement by ID and
    open correct modal
    - [x] add navigate on click on the modal button "Zu den Erfolgen"
    
    ## ✅ Checklist before requesting a review
    - [x] I have performed a self-review of my code

---
+++ src/modals/AchievementMessageModal.tsx
@@ -0,0 +1,60 @@
+import { useQuery } from '@apollo/client';
+import { gql } from '../gql';
+import AchievementModal from '../components/achievements/modals/AchievementModal';
+
+const ACHIEVMENT_BY_ID_QUERY = gql(`
+query achievementById($achievementId: Float!) {
+    me {
+      achievement(id: $achievementId) {
+        subtitle
+        name
+        description
+        achievementState
+        achievementType
+        actionType
+        isNewAchievement
+        steps {
+          name
+          isActive
+        }
+        maxSteps
+        currentStep
+        progressDescription
+        image
+        alternativeText
+        actionName
+        actionRedirectLink
+      }
+    }
+  }`);
+
+const AchievementMessageModal: React.FC<{ achievementId: number; isOpenModal: boolean; onClose: () => void }> = ({ achievementId, isOpenModal, onClose }) => {
+    const { data } = useQuery(ACHIEVMENT_BY_ID_QUERY, { variables: { achievementId } });
+    const achievement = data?.me.achievement;
+
+    if (achievement) {
+        return (
+            <>
+                <AchievementModal
+                    title={achievement?.subtitle}
+                    name={achievement.name}
+                    description={achievement.description}
+                    achievementState={achievement.achievementState}
+                    achievementType={achievement.achievementType}
+                    showModal={isOpenModal}
+                    alternativeText={achievement.alternativeText}
+                    currentStep={achievement.currentStep}
+                    image={achievement.image}
+                    isNewAchievement
+                    maxSteps={achievement.maxSteps}
+                    progressDescription={achievement.progressDescription ?? ''}
+                    steps={achievement.steps ?? []}
+                    onClose={onClose}
+                />
+            </>
+        );
+    }
+    return <></>;
+};
+
+export default AchievementMessageModal;
