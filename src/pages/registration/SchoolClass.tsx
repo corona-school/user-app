144680839804c50eaf93896db296be2b4a5cc267
Jonas <jonas@wilms.ninja>
2024-02-10 00:53:05 +0100

    refactor: Add GradeSelector

--- src/pages/registration/SchoolClass.tsx
+++ src/pages/registration/SchoolClass.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { RegistrationContext } from '../Registration';
 import IconTagList from '../../widgets/IconTagList';
 import { useNavigate } from 'react-router-dom';
+import { GradeSelector } from '../../components/GradeSelector';
 
 const SchoolClass: React.FC = () => {
     const { schoolClass, setSchoolClass, setCurrentIndex } = useContext(RegistrationContext);
@@ -14,20 +15,7 @@ const SchoolClass: React.FC = () => {
     return (
         <VStack flex="1" marginTop={space['1']}>
             <Heading>{t(`registration.steps.2.subtitle`)}</Heading>
-            <Row flexWrap="wrap" w="100%" mt={space['1']} marginBottom={space['1']}>
-                {new Array(13).fill(0).map((_, i) => (
-                    <Column mb={space['0.5']} mr={space['0.5']}>
-                        <IconTagList
-                            initial={schoolClass === i + 1}
-                            textIcon={`${i + 1}`}
-                            text={t('lernfair.schoolclass', {
-                                class: i + 1,
-                            })}
-                            onPress={() => setSchoolClass(i + 1)}
-                        />
-                    </Column>
-                ))}
-            </Row>
+            <GradeSelector grade={schoolClass} setGrade={setSchoolClass} />
             <Box alignItems="center" marginTop={space['2']}>
                 <Row space={space['1']} justifyContent="center">
                     <Column width="100%">
