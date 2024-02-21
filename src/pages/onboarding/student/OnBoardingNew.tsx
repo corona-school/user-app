de02d7383dfbe0ac6f30ad368cb31d138a99e207
Fabian5150 <fabian.strauch.04@web.de>
2024-02-21 12:15:07 +0100

    simplify video height to only 1 effet + 1 state

--- src/pages/onboarding/student/OnBoardingNew.tsx
+++ src/pages/onboarding/student/OnBoardingNew.tsx
@@ -7,23 +7,13 @@ type Props = {};
 const OnboardingNew: React.FC<Props> = () => {
     const { space, sizes } = useTheme();
 
-    const [heightHeader, setHeight] = useState(0);
-    const [heightFooter, setHeightF] = useState(0);
-
+    const [videoHeight, setVideoHeight] = useState(480);
     const headerRef = useRef<HTMLElement>(null);
     const footerRef = useRef<HTMLElement>(null);
 
     useEffect(() => {
-        if (headerRef.current !== null) {
-            setHeight(headerRef.current.clientHeight);
-        }
-    }, [headerRef.current?.clientHeight]);
-
-    useEffect(() => {
-        if (footerRef.current !== null) {
-            setHeightF(footerRef.current.clientHeight);
-        }
-    }, [footerRef.current?.clientHeight]);
+        setVideoHeight(window.innerHeight - space['1'] * 8 - (headerRef.current?.clientHeight ?? 0) - (footerRef.current?.clientHeight ?? 0));
+    }, [headerRef.current?.clientHeight, footerRef.current?.clientHeight, window.innerHeight]);
 
     /* // TBD: Adding Tracking for this new Page
     const { trackPageView } = useMatomo();
@@ -37,28 +27,6 @@ const OnboardingNew: React.FC<Props> = () => {
     const contentWidth = useBreakpointValue({
         base: '300px',
         lg: '570px',
-    });
-
-    const videoSize = useBreakpointValue({
-        /* base: {
-            width: 576,
-            height: 324,
-        }, */
-        lg: {
-            //*1.5
-            width: 864,
-            height: 486,
-        },
-        /* xl: {
-            //*1.7
-            width: 979.2,
-            height: 550.8,
-        }, */
-        /* '2xl': {
-            //*2
-            width: 1152,
-            height: 648,
-        }, */
     });
 
     //TEMPORARY
@@ -96,10 +64,9 @@ const OnboardingNew: React.FC<Props> = () => {
                     alignSelf="center"
                     p={space['1']}
                     my={space['1']}
-                    height={window.innerHeight - heightHeader - heightFooter - space['1'] * 8 /* videoSize.height */}
-                    width={'864px' /* videoSize.width - space['1'] * 6.2 */} //please don't ask me why 6.2, in theory it should be 2...
+                    height={videoHeight}
+                    width={'864px'}
                     maxWidth={window.innerWidth - space['1'] * 8}
-                    maxHeight={window.innerHeight - heightHeader - heightFooter - space['1'] * 8}
                 >
                     <iframe
                         width="100%"
