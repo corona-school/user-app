179c22c49007dfcae6228cdfd4f3cb0282f60b17
fabian5150 <fabian.strauch.04@web.de>
2024-02-24 16:39:37 +0100

    privacy banner for youtube video

--- src/pages/onboarding/student/OnBoardingNew.tsx
+++ src/pages/onboarding/student/OnBoardingNew.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useRef, useState } from 'react';
 import { useMatomo } from '@jonkoops/matomo-tracker-react';
-import { Container, Flex, useTheme, Box, Text, Row, Heading, View, useBreakpointValue, Link } from 'native-base';
+import { Container, Flex, useTheme, Box, Text, Row, Heading, View, useBreakpointValue, Link, Button, Card, ScrollView } from 'native-base';
 import { useTranslation } from 'react-i18next';
 import { ConfirmModal } from '../../../modals/ConfirmModal';
 
 type Props = {};
 
 //TBD: How should this page be skipped / finished and to what page should they lead the user afterwards?
+//TBD: Add proper text to youtube privacy banner
 const OnboardingNew: React.FC<Props> = () => {
     const { space } = useTheme();
 
@@ -17,9 +18,9 @@ const OnboardingNew: React.FC<Props> = () => {
     const paddingVidBox = space['1'];
 
     const [videoHeight, setVideoHeight] = useState(400);
-    const [isVertival, setIsVertical] = useState(window.innerHeight > window.innerWidth);
+    const [isVertical, setIsVertical] = useState(window.innerHeight > window.innerWidth);
     const [showCalendlyModal, setShowCalendlyModal] = useState(false);
-    const [showYotubeModal, setShowYotubeModal] = useState(false);
+    const [agreedYoutubePrivacy, setAgreedYoutubePrivacy] = useState(false);
 
     const headerRef = useRef<HTMLElement>(null);
     const footerRef = useRef<HTMLElement>(null);
@@ -47,73 +48,92 @@ const OnboardingNew: React.FC<Props> = () => {
     });
 
     return (
-        <Container backgroundColor="primary.100" maxWidth="100%" height="100%" overflowY="scroll" alignItems="stretch">
-            <Flex justifyContent="space-between" height="100%">
-                {/* HEADER */}
-                <Box width="100%" ref={headerRef}>
-                    <View
-                        paddingX={space['1']}
-                        paddingBottom={space['1']}
-                        color="lightText"
-                        alignItems="center"
-                        borderBottomRadius="15px"
-                        backgroundColor="primary.800"
+        <ScrollView>
+            <Container backgroundColor="primary.100" maxWidth="100%" overflowY="scroll" alignItems="stretch">
+                <Flex justifyContent="space-between" height="100%">
+                    {/* HEADER */}
+                    <Box width="100%" ref={headerRef}>
+                        <View
+                            paddingX={space['1']}
+                            paddingBottom={space['1']}
+                            color="lightText"
+                            alignItems="center"
+                            borderBottomRadius="15px"
+                            backgroundColor="primary.800"
+                        >
+                            <Row flexDirection="column">
+                                <Heading fontSize="xl" color="lightText" textAlign="center" paddingY={space['1']} maxWidth={contentWidth}>
+                                    {t('onboarding.student.header')}
+                                </Heading>
+                                <Text color="lightText" textAlign="center" maxWidth={contentWidth} paddingBottom={space['1']}>
+                                    {t('onboarding.student.text')}
+                                </Text>
+                            </Row>
+                        </View>
+                    </Box>
+                    {/* VIDEO */}
+                    <Box
+                        borderRadius="md"
+                        bg="primary.400"
+                        alignSelf="center"
+                        p={paddingVidBox}
+                        my={space['1']}
+                        height={videoHeight}
+                        width={(videoHeight - 8 * paddingVidBox) * (isVertical ? 9 / 16 : 16 / 9) + 8 * paddingVidBox}
+                        maxWidth={window.innerWidth - paddingVidBox * 8}
+                        minHeight={isVertical ? 400 : 260}
+                        minWidth={isVertical ? 260 : 400}
                     >
-                        <Row flexDirection="column">
-                            <Heading fontSize="xl" color="lightText" textAlign="center" paddingY={space['1']} maxWidth={contentWidth}>
-                                {t('onboarding.student.header')}
-                            </Heading>
-                            <Text color="lightText" textAlign="center" maxWidth={contentWidth} paddingBottom={space['1']}>
-                                {t('onboarding.student.text')}
-                            </Text>
-                        </Row>
-                    </View>
-                </Box>
-                {/* VIDEO */}
-                <Box
-                    borderRadius="md"
-                    bg="primary.400"
-                    alignSelf="center"
-                    p={paddingVidBox}
-                    my={space['1']}
-                    height={videoHeight}
-                    width={(videoHeight - 8 * paddingVidBox) * (isVertival ? 9 / 16 : 16 / 9) + 8 * paddingVidBox}
-                    maxWidth={window.innerWidth - paddingVidBox * 8}
+                        {!agreedYoutubePrivacy ? (
+                            <Flex justifyContent="center" alignItems="center" height="100%">
+                                <Card p={space['2']} bg="primary.100" alignSelf="center" maxHeight="100%" maxWidth="100%">
+                                    <Text textAlign="center">
+                                        {
+                                            'Youtube klaut deine Daten! Möchtest du das wirklich? Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n '
+                                        }
+                                    </Text>
+                                    <Button onPress={() => setAgreedYoutubePrivacy(true)} minWidth="100px" minHeight="40px">
+                                        Ja, ich will!
+                                    </Button>
+                                </Card>
+                            </Flex>
+                        ) : (
+                            <iframe
+                                width="100%"
+                                height="100%"
+                                src={`https://www.youtube.com/embed/${isVertical ? videoMobile : video}`}
+                                allow="encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                                title="Onboarding Video"
+                                style={{
+                                    border: 'none',
+                                }}
+                            />
+                        )}
+                    </Box>
+                    {/* FOOTER */}
+                    <Box backgroundColor="primary.700" ref={footerRef}>
+                        <Text color="lightText" fontWeight="700" textAlign="center" py={space['1']} onPress={() => setShowCalendlyModal(true)}>
+                            {t('onboarding.student.button')}
+                        </Text>
+                    </Box>
+                </Flex>
+                <ConfirmModal
+                    text=""
+                    isOpen={showCalendlyModal}
+                    onConfirmed={() => window.open('https://calendly.com/lern-fair/huh-kennenlernen')}
+                    onClose={() => setShowCalendlyModal(false)}
+                    danger={false}
                 >
-                    <iframe
-                        width="100%"
-                        height="100%"
-                        src={`https://www.youtube.com/embed/${isVertival ? videoMobile : video}`} /* TBD: Datenschutzbanner für YouTube*/
-                        allow="encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Onboarding Video"
-                        style={{
-                            border: 'none',
-                        }}
-                    />
-                </Box>
-                {/* FOOTER */}
-                <Box backgroundColor="primary.700" ref={footerRef}>
-                    <Text color="lightText" fontWeight="700" textAlign="center" py={space['1']} onPress={() => setShowCalendlyModal(true)}>
-                        {t('onboarding.student.button')}
+                    <Text textAlign="center">
+                        {
+                            'Mit dem folgenden Link wirst du zu unserem in den USA sitzenden Dienstleister Calendly weitergeleitet.\nWeitere Informationen zur Datenverarbeitung bei Calendly findest du '
+                        }
+                        <Link onPress={() => window.open('https://calendly.com/privacy')}>hier</Link>.{'\n\nMöchtest du fortfahren?'}
                     </Text>
-                </Box>
-            </Flex>
-            <ConfirmModal
-                text=""
-                isOpen={showCalendlyModal}
-                onConfirmed={() => window.open('https://calendly.com/lern-fair/huh-kennenlernen')}
-                onClose={() => setShowCalendlyModal(false)}
-                danger={false}
-            >
-                <Text textAlign="center">
-                    {
-                        'Mit dem folgenden Link wirst du zu unserem in den USA sitzenden Dienstleister Calendly weitergeleitet.\nWeitere Informationen zur Datenverarbeitung bei Calendly findest du '
-                    }
-                    <Link onPress={() => window.open('https://calendly.com/privacy')}>hier</Link>.{'\n\nMöchtest du fortfahren?'}
-                </Text>
-            </ConfirmModal>
-        </Container>
+                </ConfirmModal>
+            </Container>
+        </ScrollView>
     );
 };
 
