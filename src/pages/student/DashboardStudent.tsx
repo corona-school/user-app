3759c1c3b3865c0e56df3ef666c884426620df4b
John Angel <161815068+JeangelLF@users.noreply.github.com>
2024-03-12 13:31:37 +0100

    Make support offers more present (#495)
    ## What was done
    
    - Created a SupportSection component as described in
    https://github.com/corona-school/project-user/issues/960
    - Added needed translation strings
    - Added needed icons _(I grabbed them directly from the landing page)_
    
    **Notes:**
    - I didn't use the same tab ids provided in the issue, as some of those
    weren't working _(I also grabbed them from the landing page)_
    - Initially tried to use `IconTagList` to build this but that component
    is quite strict in how it works/looks. It's probably worth adjusting it
    or create another one more re-usable/flexible, but definitely for a
    separate task.

--- src/pages/student/DashboardStudent.tsx
+++ src/pages/student/DashboardStudent.tsx
@@ -25,6 +25,7 @@ import HelpNavigation from '../../components/HelpNavigation';
 import NextAppointmentCard from '../../widgets/NextAppointmentCard';
 import { Lecture } from '../../gql/graphql';
 import useApollo from '../../hooks/useApollo';
+import SupportCategories from '../../widgets/SupportCategories';
 
 type Props = {};
 
@@ -351,6 +352,9 @@ const DashboardStudent: React.FC<Props> = () => {
                                     )}
                                 </VStack>
                             )}
+                            <VStack marginBottom={space['1.5']}>
+                                <SupportCategories />
+                            </VStack>
                             <VStack marginBottom={space['1.5']}>
                                 <Heading marginBottom={space['1']}>{t('dashboard.helpers.headlines.recommend')}</Heading>
                                 <CTACard
