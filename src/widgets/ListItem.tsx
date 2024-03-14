475774d32b980fb07b5bddd6f29eb139859476c2
John Angel <john.angel@lern-fair.de>
2024-03-14 10:18:03 +0100

    Rename EditDataRow -> ListItem

--- src/widgets/ListItem.tsx
+++ src/widgets/ListItem.tsx
@@ -2,12 +2,11 @@ import { Row, Text, ChevronRightIcon, useTheme, Pressable } from 'native-base';
 
 type Props = {
     label: string;
-    value?: string;
     onPress?: () => any;
     isDisabled?: boolean;
 };
 
-const EditDataRow: React.FC<Props> = ({ label, value, onPress, isDisabled }) => {
+const EditDataRow: React.FC<Props> = ({ label, onPress, isDisabled }) => {
     const { space } = useTheme();
     const textColor = isDisabled ? 'gray.300' : 'darkText';
     return (
@@ -16,11 +15,6 @@ const EditDataRow: React.FC<Props> = ({ label, value, onPress, isDisabled }) => 
                 <Text flex="1" fontWeight={600} color={textColor}>
                     {label}
                 </Text>
-                {value && (
-                    <Text marginRight={space['0.5']} color={textColor}>
-                        {value}
-                    </Text>
-                )}
                 <ChevronRightIcon color={textColor} />
             </Row>
         </Pressable>
