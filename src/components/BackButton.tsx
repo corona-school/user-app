a0db03fa3a03c9c2e49f7bebead81989d8c573e3
John Angel <john.angel@lern-fair.de>
2024-04-19 14:01:59 +0200

    fix: Use new hook to customize behavior of back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,6 +1,7 @@
 import { ArrowBackIcon } from 'native-base';
 import { TouchableOpacity } from 'react-native';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigationStack } from '../hooks/useNavigationStack';
 
 type Props = {
     onPress?: () => any;
@@ -9,15 +10,25 @@ type Props = {
 
 const BackButton: React.FC<Props> = ({ onPress, previousFallbackRoute: fallback }) => {
     const navigate = useNavigate();
+    const { navigationStack } = useNavigationStack();
     const location = useLocation();
 
     const handleOnBack = () => {
-        if (onPress) onPress();
-        else if (location.key === 'default' && fallback) {
-            navigate(fallback, { replace: true });
-        } else {
-            navigate(-1);
+        if (onPress) {
+            onPress();
+            return;
         }
+        const isDefaultRoute = location.key === 'default';
+
+        // -1 is the current one
+        const previousRoute = navigationStack[navigationStack.length - 2]?.pathname;
+        const isPreviousRouteValid = previousRoute && ![location.pathname, '/login', '/welcome', '/registration'].includes(previousRoute);
+        if ((isDefaultRoute || !isPreviousRouteValid) && !!fallback) {
+            navigate(fallback);
+            return;
+        }
+
+        navigate(-1);
     };
     return (
         <TouchableOpacity onPress={handleOnBack}>
