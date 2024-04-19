aa5506b49729533bdc4686f181f9a14e1a28728f
John Angel <john.angel@lern-fair.de>
2024-04-19 11:32:31 +0200

    feature: Add previousFallbackRoute prop to the BackButton

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,15 +1,26 @@
 import { ArrowBackIcon } from 'native-base';
 import { TouchableOpacity } from 'react-native';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 type Props = {
     onPress?: () => any;
+    previousFallbackRoute?: string;
 };
 
-const BackButton: React.FC<Props> = ({ onPress }) => {
+const BackButton: React.FC<Props> = ({ onPress, previousFallbackRoute: fallback }) => {
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleOnBack = () => {
+        if (onPress) onPress();
+        else if (location.key === 'default' && fallback) {
+            navigate(fallback, { replace: true });
+        } else {
+            navigate(-1);
+        }
+    };
     return (
-        <TouchableOpacity onPress={onPress || (() => navigate(-1))}>
+        <TouchableOpacity onPress={handleOnBack}>
             <ArrowBackIcon size="xl" color="lightText" />
         </TouchableOpacity>
     );
