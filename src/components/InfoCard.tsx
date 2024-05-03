d8d082cbb10a4851b3e986a5eb8016f022be250d
John Angel <john.angel@lern-fair.de>
2024-05-03 12:49:06 +0200

    fix: Weird condition

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -39,8 +39,7 @@ export function InfoCard({ title, message, children, icon, background, noMargin 
                 <Heading color="white" paddingBottom="10px">
                     {title}
                 </Heading>
-                {!children && message && <Text color="white">{message}</Text>}
-                {children && children}
+                {children ? children : <Text color="white">{message}</Text>}
             </VStack>
         </HStack>
     );
