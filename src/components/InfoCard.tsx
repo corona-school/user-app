f7f445580ff52f4fec8c359ec36c989c0ea991d0
John Angel <john.angel@lern-fair.de>
2024-05-03 11:34:52 +0200

    feat: Add children prop to InfoCard component

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -2,20 +2,18 @@ import { Text } from 'native-base';
 import { Heading, HStack, useTheme, VStack } from 'native-base';
 import { ColorType } from 'native-base/lib/typescript/components/types';
 import { IconLoader } from './IconLoader';
+import React from 'react';
 
-export function InfoCard({
-    title,
-    message,
-    icon,
-    background,
-    noMargin,
-}: {
+interface InfoCardProps {
     title: string;
-    message: string;
+    message?: string;
+    children?: React.ReactNode;
     background?: ColorType;
     icon: 'loki' | 'yes' | 'no';
     noMargin?: boolean;
-}) {
+}
+
+export function InfoCard({ title, message, children, icon, background, noMargin }: InfoCardProps) {
     const { space } = useTheme();
 
     const iconPath = {
@@ -41,7 +39,8 @@ export function InfoCard({
                 <Heading color="white" paddingBottom="10px">
                     {title}
                 </Heading>
-                <Text color="white">{message}</Text>
+                {!children && message && <Text color="white">{message}</Text>}
+                {children && children}
             </VStack>
         </HStack>
     );
