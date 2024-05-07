0b0805bd6925353cbcda791ac6f38ba2ec0955c3
John Angel <john.angel@lern-fair.de>
2024-05-07 14:08:49 +0200

    feat: Add button and text stories

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from './Button';
+
+type Story = StoryObj<typeof Button>;
+const meta: Meta<typeof Button> = {
+    title: 'Button',
+    component: Button,
+    argTypes: {
+        variant: {
+            options: ['secondary', 'outlinelight', 'solid', 'outline', 'link', 'ghost', 'subtle', 'unstyled'],
+            control: 'radio',
+        },
+        isDisabled: {
+            control: 'boolean',
+        },
+        isLoading: {
+            control: 'boolean',
+        },
+        label: {
+            control: 'text',
+        },
+    },
+};
+
+export const Base: Story = {
+    render: (props) => (
+        <Button {...props} variant={props.variant || 'solid'} width={200}>
+            {props.label || 'Button'}
+        </Button>
+    ),
+};
+
+export default meta;
---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Text from './Text';
+import { VStack } from 'native-base';
+
+type Story = StoryObj<typeof Text>;
+const meta: Meta<typeof Text> = {
+    title: 'Text',
+    component: Text,
+    argTypes: {
+        children: {
+            control: 'text',
+        },
+        fontWeight: {
+            control: 'radio',
+            options: ['light', 'bold', 'black', 'hairline', 'thin', 'normal', 'medium', 'semibold', 'extrabold', 'extraBlack'],
+        },
+    },
+};
+
+export const Base: Story = {
+    args: {
+        fontWeight: 'normal',
+    },
+    render: (props) => (
+        <VStack space={1}>
+            <Text fontSize="xs" {...props}>
+                {props.children || 'Text'} {'(xs)'}
+            </Text>
+            <Text fontSize="sm" {...props}>
+                {props.children || 'Text'} {'(sm)'}
+            </Text>
+            <Text fontSize="md" {...props}>
+                {props.children || 'Text'} {'(md)'}
+            </Text>
+            <Text fontSize="lg" {...props}>
+                {props.children || 'Text'} {'(lg)'}
+            </Text>
+            <Text fontSize="xl" {...props}>
+                {props.children || 'Text'} {'(xl)'}
+            </Text>
+            <Text fontSize="2xl" {...props}>
+                {props.children || 'Text'} {'(2xl)'}
+            </Text>{' '}
+            <Text fontSize="3xl" {...props}>
+                {props.children || 'Text'} {'(3xl)'}
+            </Text>
+            <Text fontSize="4xl" {...props}>
+                {props.children || 'Text'} {'(4xl)'}
+            </Text>
+            <Text fontSize="5xl" {...props}>
+                {props.children || 'Text'} {'(5xl)'}
+            </Text>
+            <Text fontSize="6xl" {...props}>
+                {props.children || 'Text'} {'(6xl)'}
+            </Text>
+            <Text fontSize="7xl" {...props}>
+                {props.children || 'Text'} {'(7xl)'}
+            </Text>
+            <Text fontSize="8xl" {...props}>
+                {props.children || 'Text'} {'(8xl)'}
+            </Text>
+            <Text fontSize="9xl" {...props}>
+                {props.children || 'Text'} {'(9xl)'}
+            </Text>
+        </VStack>
+    ),
+};
+
+export default meta;
