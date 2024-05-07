7a3e57c904e9d948b080e9ab47cbaaa3c08d87bc
John Angel <john.angel@lern-fair.de>
2024-05-07 17:36:07 +0200

    feat: Add Input story

---
+++ src/components/Input.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Input from './Input';
+import { FormControl } from 'native-base';
+
+type Story = StoryObj<typeof Input>;
+const meta: Meta<typeof Input> = {
+    title: 'Atoms/Input',
+    component: Input,
+    argTypes: {
+        placeholder: { control: 'text' },
+        isDisabled: { control: 'boolean' },
+    },
+};
+
+export const Base: Story = {
+    name: 'Input',
+    render: (props) => (
+        <FormControl>
+            <FormControl.Label>Input label</FormControl.Label>
+            <Input {...props} width={200} />
+            <FormControl.HelperText>Helper text</FormControl.HelperText>
+        </FormControl>
+    ),
+};
+
+export default meta;
