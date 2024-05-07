90dbbe96e2e708c89bab69d2f05b162d1b326d6d
John Angel <john.angel@lern-fair.de>
2024-05-07 13:52:02 +0200

    feat: Theme

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -7,7 +7,6 @@ const theme = create({
     brandImage: 'https://assets-global.website-files.com/63906379538296472e0ae3d2/63a1e5e3d91c5d153f5906ef_Lern-fair_logo-horizontal.svg',
     brandUrl: 'https://www.lern-fair.de/',
     colorPrimary: '#00494f',
-    buttonBg: '#00494f',
     appBorderColor: '#edf4f3',
     textColor: '#00334ccc',
     colorSecondary: '#00494f',
@@ -17,7 +16,9 @@ const theme = create({
     barHoverColor: '#00334ccc',
     barBg: '#ffffff',
 
-    inputBorder: '#f4f4f4'
+    inputBorder: '#f4f4f4',
+
+    buttonBg: '#EDF4F3',
 });
 
 addons.setConfig({
