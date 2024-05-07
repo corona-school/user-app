cb3b6ee62082e98ae918911634bc8601ab5ef069
John Angel <john.angel@lern-fair.de>
2024-05-07 13:39:43 +0200

    feat: Storybook theme

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Preview } from '@storybook/react';
 import { NativeBaseProvider } from 'native-base';
 import Theme from '../src/Theme';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,29 +8,31 @@ import { I18nextProvider } from 'react-i18next';
 import { MockedProvider } from '@apollo/client/testing';
 import './style.css';
 
-export const parameters = {
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
+const preview: Preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
+        },
+        i18n,
+        locale: 'de-DE',
+        locales: {
+            de_DE: 'Deutsch',
         },
     },
-    i18n,
-    locale: 'de-DE',
-    locales: {
-        de_DE: 'Deutsch',
-    },
+    decorators: [
+        (Page: () => React.ReactElement) => (
+            <I18nextProvider i18n={i18n}>
+                <BrowserRouter>
+                    <NativeBaseProvider theme={Theme}>
+                        <MockedProvider mocks={[]}>{Page()}</MockedProvider>
+                    </NativeBaseProvider>
+                </BrowserRouter>
+            </I18nextProvider>
+        ),
+    ],
 };
 
-// NOTE: Decorators are only applied to <Story> tags inside MDX!
-export const decorators = [
-    (Page: () => React.ReactElement) => (
-        <I18nextProvider i18n={i18n}>
-            <BrowserRouter>
-                <NativeBaseProvider theme={Theme}>
-                    <MockedProvider mocks={[]}>{Page()}</MockedProvider>
-                </NativeBaseProvider>
-            </BrowserRouter>
-        </I18nextProvider>
-    ),
-];
+export default preview;
