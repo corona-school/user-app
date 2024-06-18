8a3b31ea2be2976de035fe104f7a91e351d9da43
John Angel <john.angel@lern-fair.de>
2024-06-18 16:31:38 +0200

    feat: Add hook to handle app installation

---
+++ src/hooks/useInstallation.ts
@@ -0,0 +1,71 @@
+import { useMatomo } from '@jonkoops/matomo-tracker-react';
+import { useEffect, useRef, useState } from 'react';
+import { BeforeInstallPromptEvent } from '../types/window';
+
+const PromotionType = {
+    native: 'native' as const,
+    iPhone: 'iPhone' as const,
+    iPad: 'iPad' as const,
+    none: 'none' as const,
+};
+
+const useInstallation = () => {
+    const { trackEvent } = useMatomo();
+    const deferredPromptRef = useRef<BeforeInstallPromptEvent | null>(null);
+    const [promotionType, setPromotionType] = useState<keyof typeof PromotionType>(PromotionType.none);
+
+    const isIphone = () => {
+        const userAgent = window?.navigator?.userAgent?.toLowerCase();
+        return /iphone|ipod/.test(userAgent);
+    };
+
+    const isIpad = () => {
+        const userAgent = window?.navigator?.userAgent?.toLowerCase();
+        const isMac = RegExp(/Macintosh/i).test(userAgent);
+        const iPad = /ipad/.test(userAgent);
+        return iPad || (isMac && navigator.maxTouchPoints && navigator.maxTouchPoints > 2);
+    };
+
+    const isInStandaloneMode = () => 'standalone' in window.navigator && (window.navigator as any)?.standalone;
+
+    useEffect(() => {
+        if (isIphone() && !isInStandaloneMode()) {
+            setPromotionType(PromotionType.iPhone);
+            return;
+        }
+        if (isIpad() && !isInStandaloneMode()) {
+            setPromotionType(PromotionType.iPad);
+            return;
+        }
+
+        const handleBeforeInstallPrompt = (e: Event) => {
+            e.preventDefault();
+            deferredPromptRef.current = e as BeforeInstallPromptEvent;
+            setPromotionType(PromotionType.native);
+        };
+
+        window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
+        return () => {
+            window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+        };
+    }, []);
+
+    const promote = async () => {
+        if (deferredPromptRef.current) {
+            deferredPromptRef.current.prompt();
+            const choiceResult = await deferredPromptRef.current.userChoice;
+            trackEvent({
+                category: 'pwa',
+                action: 'click-event',
+                name: choiceResult.outcome === 'accepted' ? 'App-Installation abgeschlossen' : 'App-Installation abgebrochen',
+            });
+            deferredPromptRef.current = null;
+            setPromotionType(PromotionType.none);
+        }
+    };
+
+    return { shouldPromote: promotionType !== PromotionType.none, promotionType, promote };
+};
+
+export default useInstallation;
