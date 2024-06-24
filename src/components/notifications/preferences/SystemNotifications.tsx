c32048f76228fcf800a79389a48a77f2d1d27112
John Angel <john.angel@lern-fair.de>
2024-06-24 12:33:43 +0200

    feat: Hide Push UI behind a feature flag

--- src/components/notifications/preferences/SystemNotifications.tsx
+++ src/components/notifications/preferences/SystemNotifications.tsx
@@ -4,8 +4,9 @@ import { useTranslation } from 'react-i18next';
 import { systemNotificationCategories } from '../../../helper/notification-preferences';
 import { useWebPush } from '../../../lib/WebPush';
 import InformationBadge from './InformationBadge';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import InformationModal from '../../../modals/InformationModal';
+import { WEBPUSH_ACTIVE } from '../../../config';
 
 export const SystemNotifications = () => {
     const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
@@ -24,45 +25,55 @@ export const SystemNotifications = () => {
         }
     };
 
+    const channels = useMemo(() => {
+        const defaultChannels = [{ id: 'email', label: t('notification.controlPanel.preference.channel.email') }];
+        if (WEBPUSH_ACTIVE) {
+            defaultChannels.push({
+                id: 'push',
+                label: t('notification.controlPanel.preference.channel.push'),
+            });
+        }
+        return defaultChannels;
+    }, [t]);
+
     return (
         <Box>
-            <Flex direction="row" bg={'primary.100'} mx={2} my={1} py={3} px={4} borderRadius="sm" justifyContent="space-between">
-                <Flex direction="row" alignItems="center" flexWrap="wrap">
-                    <Text fontWeight="bold" color={'primary.900'} fontSize="sm">
-                        {t(`notification.controlPanel.preference.pushNotifications`)}
-                    </Text>
-                    <>
-                        {isMobileOrTable ? (
-                            <IconButton onPress={() => setIsInfoModalOpen(true)} icon={<InformationBadge ml={0} bg="primary.900" />} />
-                        ) : (
-                            <Tooltip
-                                maxW={250}
-                                label={t('notification.controlPanel.preference.pushNotificationTooltip')}
-                                bg={'primary.900'}
-                                _text={{ textAlign: 'center' }}
-                                p={3}
-                                hasArrow
-                                children={
-                                    <Box ml={2}>
-                                        <InformationBadge ml={0} bg="primary.900" />
-                                    </Box>
-                                }
-                            ></Tooltip>
-                        )}
-                    </>
+            {WEBPUSH_ACTIVE && (
+                <Flex direction="row" bg={'primary.100'} mx={2} my={1} py={3} px={4} borderRadius="sm" justifyContent="space-between">
+                    <Flex direction="row" alignItems="center" flexWrap="wrap">
+                        <Text fontWeight="bold" color={'primary.900'} fontSize="sm">
+                            {t(`notification.controlPanel.preference.pushNotifications`)}
+                        </Text>
+                        <>
+                            {isMobileOrTable ? (
+                                <IconButton onPress={() => setIsInfoModalOpen(true)} icon={<InformationBadge ml={0} bg="primary.900" />} />
+                            ) : (
+                                <Tooltip
+                                    maxW={250}
+                                    label={t('notification.controlPanel.preference.pushNotificationTooltip')}
+                                    bg={'primary.900'}
+                                    _text={{ textAlign: 'center' }}
+                                    p={3}
+                                    hasArrow
+                                    children={
+                                        <Box ml={2}>
+                                            <InformationBadge ml={0} bg="primary.900" />
+                                        </Box>
+                                    }
+                                ></Tooltip>
+                            )}
+                        </>
+                    </Flex>
+                    <Flex direction="row" alignItems="center">
+                        {status === 'loading' && <Spinner mr={1} />}
+                        <Switch isChecked={status === 'subscribed'} onValueChange={handleOnChange} />
+                    </Flex>
                 </Flex>
-                <Flex direction="row" alignItems="center">
-                    {status === 'loading' && <Spinner mr={1} />}
-                    <Switch isChecked={status === 'subscribed'} onValueChange={handleOnChange} />
-                </Flex>
-            </Flex>
+            )}
             <Preferences
                 title={t('notification.controlPanel.tabs.system.description')}
                 notificationCategories={systemNotificationCategories}
-                channels={[
-                    { id: 'email', label: t('notification.controlPanel.preference.channel.email') },
-                    { id: 'push', label: t('notification.controlPanel.preference.channel.push') },
-                ]}
+                channels={channels}
             />
             <InformationModal
                 title={t('notification.controlPanel.preference.pushNotifications')}
