a0234f6da2b2d40633fbdd741e44e81e8ab7ed4e
Jonas <jonas@wilms.ninja>
2024-07-13 16:16:15 +0200

    Touchups

--- scripts/bundle-size.sh
+++ scripts/bundle-size.sh
@@ -2,17 +2,21 @@ MAX_BUNDLE_SIZE_KB=500
 
 commands=""
 
-output="##Bundle Size\n"
-output+="| Javascript Bundle | Size (gzip compressed) | Check | \n"
-output+="| ----------------- | ---------------------- | ----- | \n"
+output="## Bundle Size\n"
+
+output+="On a slow mobile connection, 100kB take about 3s to load. As loading JavaScript blocks the application "
+output+=" if the app is opened the first time, bundle sizes should be kept as small as possible (especially main.js)\n"
+
+output+="| Javascript Bundle | Size (bytes, gzip compressed) | Check | \n"
+output+="| ----------------- | ----------------------------- | ----- | \n"
 
 for bundle in build/static/js/*.js; do
   # We do not care about absolute size, but compressed size (= network transfer size)
   compressed_size=$(gzip -c $bundle | wc -c)
 
-  in_bounds="yes"
+  in_bounds=""
   if (( compressed_size > MAX_BUNDLE_SIZE_KB * 1000 )); then
-    in_bounds="no"
+    in_bounds="❌"
     commands+="::error file=./scripts/bundle-size.sh,line=1,col=1,title=Bundle Size Exceeded::$bundle has a size of $compressed_size bytes (gzip compressed)\n"
   fi
 
