2ce4e1d7d50b419171993da9e71371d96d196e82
Florian T. <github@mail.cr.eative.dev>
2024-07-19 14:18:34 +0200

    added translations and hint popup in case no selection is made

--- src/pages/registration/UserState.tsx
+++ src/pages/registration/UserState.tsx
@@ -1,16 +1,27 @@
 import { Box, Button, Column, Heading, Row, useTheme, VStack, Text } from 'native-base';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { RegistrationContext } from '../Registration';
 import IconTagList from '../../widgets/IconTagList';
 import { states } from '../../types/lernfair/State';
 import { useTranslation } from 'react-i18next';
 import { usePageTitle } from '../../hooks/usePageTitle';
+import AlertMessage from '../../widgets/AlertMessage';
 
 const UserState: React.FC = () => {
     const { userState, setUserState, onPrev, onNext } = useContext(RegistrationContext);
     const { space } = useTheme();
     const { t } = useTranslation();
     usePageTitle('Lern-Fair - Registrierung: Schulort');
+
+    const [showStateMissing, setShowStateMissing] = useState<boolean>(false);
+
+    const handleNext = () => {
+        if (!userState) {
+            setShowStateMissing(true);
+        } else {
+            onNext();
+        }
+    };
 
     return (
         <VStack flex="1" marginTop={space['1']}>
@@ -28,6 +39,7 @@ const UserState: React.FC = () => {
                     </Column>
                 ))}
             </Row>
+            {showStateMissing && <AlertMessage content={t('registration.hint.userStateSelectionMissing')} />}
             <Box alignItems="center" marginTop={space['2']}>
                 <Row space={space['1']} justifyContent="center">
                     <Column width="100%">
@@ -36,7 +48,7 @@ const UserState: React.FC = () => {
                         </Button>
                     </Column>
                     <Column width="100%">
-                        <Button width="100%" onPress={onNext}>
+                        <Button width="100%" onPress={handleNext}>
                             {t('next')}
                         </Button>
                     </Column>
