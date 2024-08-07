ec6fd6d02d9b55e157282a68e2106415cd03bd49
Jonas <jonas@wilms.ninja>
2024-08-07 17:48:16 +0200

    fix: Use serviceWorker.ready
    register() does not necessarily wait till the service worker is active and we might run into timing issues. ready waits for the service worker to be active

--- src/service-worker-proxy.ts
+++ src/service-worker-proxy.ts
@@ -1,18 +1,19 @@
 import { SERVICE_WORKER_ACTIVE } from './config';
 import { log } from './log';
 
-let serviceWorker: Promise<ServiceWorkerRegistration>;
-
 // If service workers are enabled and supported by the browser
 // register the service worker here
 // The service worker will then run independently in the background of the browser
 // (thus lifecycle mangement is slightly more complex)
-if (SERVICE_WORKER_ACTIVE && 'serviceWorker' in navigator) {
-    serviceWorker = navigator.serviceWorker.register('/service-worker.js');
+const active = SERVICE_WORKER_ACTIVE && 'serviceWorker' in navigator;
+if (active) {
+    navigator.serviceWorker.register('/service-worker.js');
     log('ServiceWorker', 'Service Worker registered');
 }
 
 // If a service worker is active, one can get it here
-export function getServiceWorker() {
-    return serviceWorker;
+export async function getServiceWorker() {
+    if (!active) return undefined;
+
+    return await navigator.serviceWorker.ready;
 }
