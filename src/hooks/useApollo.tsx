e2cff2d2ae0f6c7785a29061bd7166457c97ff43
Jonas <jonas@wilms.ninja>
2024-09-05 20:45:24 +0200

    fix: Skip cache for appointment queries
    Unfortunately pagination in GraphQL is terribly broken, and we update the query result of an existing query with fetchMore(...) passing in other variables, without changing the original variables of the query. Thus this messes up the cache.
    
    Unfortunately for some reason cachePolicy: "no-cache" does not work and the cache is still taken :/ Thus adding a hack to skip the cache, by adding NO_CACHE in the query name

--- src/hooks/useApollo.tsx
+++ src/hooks/useApollo.tsx
@@ -72,6 +72,10 @@ class FullResultCache extends ApolloCache<NormalizedCacheObject> {
         }
 
         const name = definition.name?.value;
+        if (name?.includes('NO_CACHE')) {
+            return undefined;
+        }
+
         return name;
     }
 
