fd16b49825fe640bab1a7fd045b31db287533b87
John Angel <161815068+JeangelLF@users.noreply.github.com>
2024-09-12 10:07:41 +0200

    Feat: UI Facelift - Course Page SuS (#648)
    ## Ticket
    
    https://github.com/corona-school/project-user/issues/1290
    
    ## What was done?
    
    - Replace native-base with our own components on the SuS course detail
    page / Related components
    - Updated models to use the right theme depending on intent
    "destructive/confirmation"
    - Other minor style adjustments to match the UI-Facelift of the HuH
    version
    
    **Preview**
    <img width="1440" alt="Bildschirmfoto 2024-09-11 um 16 56 04"
    src="https://github.com/user-attachments/assets/bb90c5f6-35b2-4243-a8ad-f4c6ae156816">
    <img width="1440" alt="Bildschirmfoto 2024-09-11 um 17 06 15"
    src="https://github.com/user-attachments/assets/73599ad6-dfb8-4adf-93b2-c10b377bb487">
    <img width="311" alt="Bildschirmfoto 2024-09-11 um 16 56 57"
    src="https://github.com/user-attachments/assets/3921fda7-7732-4f0c-86f5-15a7e80dcc08">

--- src/widgets/PupilJoinedCourseBanner.tsx
+++ src/widgets/PupilJoinedCourseBanner.tsx
@@ -1,31 +1,31 @@
-import { Box, Card, Divider, Stack, Text, useTheme } from 'native-base';
 import { useTranslation } from 'react-i18next';
 import CourseTrafficLamp from './CourseTrafficLamp';
 import CheckIcon from '../assets/icons/lernfair/Icon_Done.svg';
 import { TrafficStatus } from '../types/lernfair/Course';
+import { Separator } from '@/components/Separator';
+import { Typography } from '@/components/Typography';
 
 type BannerProps = {
     courseStatus: TrafficStatus;
     seatsLeft: number;
 };
 
-const PupilJoinedCourseBanner: React.FC<BannerProps> = ({ courseStatus, seatsLeft }) => {
+const PupilJoinedCourseBanner = ({ courseStatus, seatsLeft }: BannerProps) => {
     const { t } = useTranslation();
-    const { sizes } = useTheme();
 
     return (
-        <Card bg="primary.100" maxWidth={sizes['smallWidth']}>
-            <CourseTrafficLamp seatsLeft={seatsLeft} status={courseStatus} paddingY={3} />
-            <Divider mb="3" />
-            <Stack direction="row" alignItems="center">
-                <Box pr="2">
-                    <CheckIcon width={54} />
-                </Box>
-                <Text ml="1" fontSize="md">
-                    {t('single.card.alreadyRegistered')}
-                </Text>
-            </Stack>
-        </Card>
+        <div className="bg-primary-lighter w-full max-w-[460px] p-4 rounded-lg shadow-md">
+            <div className="flex flex-col justify-between items-start gap-1">
+                <CourseTrafficLamp seatsLeft={seatsLeft} status={courseStatus} paddingY={1} />
+                <Separator className="mb-3" />
+                <div className="flex items-center">
+                    <div className="pr-2">
+                        <CheckIcon width={40} />
+                    </div>
+                    <Typography className="ml-1">{t('single.card.alreadyRegistered')}</Typography>
+                </div>
+            </div>
+        </div>
     );
 };
 
