c616d6baf28ea369ae476b0b71186b311d4f3920
John Angel <john.angel@lern-fair.de>
2024-09-12 16:09:27 +0200

    fix: Font-sizes / File input styles

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
         <input
             type={type}
             className={cn(
-                'flex h-10 rounded-md border border-input bg-transparent px-3 py-1 text-base font-normal text-primary transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+                'flex h-10 rounded-md border border-input bg-transparent px-3 py-1 text-form font-normal text-primary transition-colors file:border-0 file:bg-transparent file:text-form file:py-2 placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
                 className
             )}
             ref={ref}
