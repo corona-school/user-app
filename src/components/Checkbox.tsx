d3a0aee3334292bd1294ce4b452ab72f0516a1ca
John Angel <john.angel@lern-fair.de>
2024-09-12 15:30:27 +0200

    feat: Add Checkbox & Radios

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { IconCheck } from '@tabler/icons-react';
+import { cn } from '@/lib/Tailwind';
+
+const Checkbox = React.forwardRef<React.ElementRef<typeof CheckboxPrimitive.Root>, React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>>(
+    ({ className, ...props }, ref) => (
+        <CheckboxPrimitive.Root
+            ref={ref}
+            className={cn(
+                'peer size-6 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+                className
+            )}
+            {...props}
+        >
+            <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
+                <IconCheck className="size-4" />
+            </CheckboxPrimitive.Indicator>
+        </CheckboxPrimitive.Root>
+    )
+);
+
+export { Checkbox };
---
+++ src/components/RadioGroup.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
+import { IconCheck } from '@tabler/icons-react';
+import { cn } from '@/lib/Tailwind';
+
+const RadioGroup = React.forwardRef<React.ElementRef<typeof RadioGroupPrimitive.Root>, React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>>(
+    ({ className, ...props }, ref) => {
+        return <RadioGroupPrimitive.Root className={cn('grid gap-2', className)} {...props} ref={ref} />;
+    }
+);
+
+const RadioGroupItem = React.forwardRef<React.ElementRef<typeof RadioGroupPrimitive.Item>, React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>>(
+    ({ className, ...props }, ref) => {
+        return (
+            <RadioGroupPrimitive.Item
+                ref={ref}
+                className={cn(
+                    'aspect-square size-6 rounded-full border border-primary data-[state=checked]:bg-primary text-primary-foreground shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+                    className
+                )}
+                {...props}
+            >
+                <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
+                    <IconCheck className="size-3" />
+                </RadioGroupPrimitive.Indicator>
+            </RadioGroupPrimitive.Item>
+        );
+    }
+);
+
+export { RadioGroup, RadioGroupItem };
