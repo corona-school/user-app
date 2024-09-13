6cac516cdb458a734153e0c122728353dfe6b69a
John Angel <john.angel@lern-fair.de>
2024-09-13 13:10:54 +0200

    fix: Allow toast to appear on top of modal overlay

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -36,6 +36,12 @@ const ModalContent = React.forwardRef<React.ElementRef<typeof DialogPrimitive.Co
                     className
                 )}
                 {...props}
+                onInteractOutside={(e) => {
+                    const { originalEvent } = e.detail;
+                    if (originalEvent.target instanceof Element && originalEvent.target.closest('.group.toast')) {
+                        e.preventDefault();
+                    }
+                }}
             >
                 {children}
                 <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
