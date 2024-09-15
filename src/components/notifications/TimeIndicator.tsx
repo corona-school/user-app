20977785f67f1f2934781d35cc31fbc5c964946f
John Angel <john.angel@lern-fair.de>
2024-09-15 12:06:20 +0200

    feat: Remove native-base from Notification-related components

--- src/components/notifications/TimeIndicator.tsx
+++ src/components/notifications/TimeIndicator.tsx
@@ -1,31 +1,28 @@
 import { useState } from 'react';
-import { Box, Text } from 'native-base';
 import useInterval from '../../hooks/useInterval';
 import { useTranslation } from 'react-i18next';
 import { getTimeText } from '../../helper/notification-helper';
+import { Typography } from '../Typography';
 
 type TimeIndicatorProps = {
     sentAt: string;
 };
 
-const TimeIndicator: React.FC<TimeIndicatorProps> = ({ sentAt }) => {
+const TimeIndicator = ({ sentAt }: TimeIndicatorProps) => {
     const { t } = useTranslation();
     const [toggleRerender, setToggleRerender] = useState<boolean>(false);
     const time = getTimeText(sentAt);
-
-    const boxPropsAllDevices = {
-        maxW: 80,
-        pr: 3,
-    };
 
     useInterval(() => {
         setToggleRerender(!toggleRerender);
     }, 60_000);
 
     return (
-        <Box maxW={boxPropsAllDevices.maxW} pr={boxPropsAllDevices.pr}>
-            {typeof time === 'string' ? <Text fontSize="xs">{time}</Text> : <Text fontSize="xs">{t(time.text, time?.options)}</Text>}
-        </Box>
+        <div className="max-w-[80px] pr-3">
+            <Typography variant="xs" className="text-end">
+                {typeof time === 'string' ? time : t(time.text, time?.options)}
+            </Typography>
+        </div>
     );
 };
 
