ab880348a5cf731091ead2f31d0f8dd8cdd1dbe4
John Angel <john.angel@lern-fair.de>
2024-09-18 20:59:25 +0200

    update side id

--- src/matomo.ts
+++ src/matomo.ts
@@ -3,7 +3,7 @@ import { DISABLE_MATOMO } from './config';
 
 export default createInstance({
     urlBase: process.env.REACT_APP_MATOMO_URL,
-    siteId: 6,
+    siteId: 7,
     disabled: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test' || DISABLE_MATOMO === 'true',
     configurations: {
         setCookieDomain: ['*.lern-fair.de'],
