661d0aa25df48f18bcf24d78196208e8c306dfcb
John Angel <john.angel@lern-fair.de>
2024-10-15 11:51:16 +0200

    feat: Add Breadcrumb component and routes

---
+++ src/hooks/useBreadcrumbItems.ts
@@ -0,0 +1,21 @@
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const useBreadcrumbItems = () => {
+    const localStorageLanguage = localStorage.getItem('lernfair-language');
+    const { t } = useTranslation();
+    const items = useMemo(() => {
+        return {
+            APPOINTMENTS: { label: t('navigation.label.appointments'), route: '/appointments' },
+            APPOINTMENT: { label: '', route: '/appointment' },
+            CREATE_APPOINTMENT: { label: t('navigation.label.createAppointment'), route: '/create-appointment' },
+            EDIT_APPOINTMENT: { label: t('navigation.label.editAppointment'), route: '/edit-appointment' },
+            COURSES: { label: t('navigation.label.group'), route: '/group' },
+            CREATE_COURSE: { label: t('navigation.label.createCourse'), route: '/create-course' },
+            MATCHING: { label: t('navigation.label.matching'), route: '/matching' },
+            REQUEST_MATCH: { label: t('navigation.label.requestMatch'), route: '/request-match' },
+        } as const;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [localStorageLanguage, t]);
+    return items;
+};
