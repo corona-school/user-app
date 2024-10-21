ed3fc522584c7fa1cb563abfcf5197f0fcef7d11
realmayus <realmayus@gmail.com>
2024-10-21 11:06:14 +0200

    add string for devices without user agents

--- src/components/SessionCard.tsx
+++ src/components/SessionCard.tsx
@@ -16,7 +16,7 @@ interface Props {
 
 const SessionCard: React.FC<Props> = ({ userAgent, deviceType, lastLogin, logOut, isCurrentSession }) => {
     const { t } = useTranslation();
-    let icon = <IconInfoCircle />;
+    let icon = <IconInfoCircle size={32} />;
     if (deviceType === 'mobile') {
         icon = <IconDeviceMobile size={32} />;
     }
@@ -31,8 +31,8 @@ const SessionCard: React.FC<Props> = ({ userAgent, deviceType, lastLogin, logOut
     return (
         <div className="flex justify-between items-center rounded-lg px-4 py-3 bg-primary-lighter text-foreground gap-5 flex-grow flex-shrink-0 max-w-xl min-h-20">
             <div className="pt-1">{icon}</div>
-            <div className="flex flex-col items-start ">
-                <Typography className="font-bold">{userAgent}</Typography>
+            <div className="flex flex-col items-center ">
+                <Typography className="font-bold">{userAgent ?? t('sessionManager.unknownDevice')}</Typography>
                 {lastLogin && (
                     <Typography className="text-form font-normal [&_p]:leading-relaxed">
                         {t('sessionManager.lastUsed')} {toTimerString(DateTime.now(), DateTime.fromJSDate(new Date(lastLogin)))}
