378e52c2b1c353f770f64ecc8acdaae67f60f460
John Angel <161815068+JeangelLF@users.noreply.github.com>
2024-10-23 10:02:55 +0200

    Feat: Add matomo events (#688)
    ## Ticket
    
    Resolves https://github.com/corona-school/project-user/issues/1354
    Resolves https://github.com/corona-school/project-user/issues/1359
    
    ## What was done?
    
    - Added tracking for Recommend section / Blocker Modal

--- src/modals/RecommendModal.tsx
+++ src/modals/RecommendModal.tsx
@@ -5,12 +5,14 @@ import { BaseModalProps, Modal, ModalHeader, ModalTitle } from '@/components/Mod
 import { TextArea } from '@/components/TextArea';
 import { Button } from '@/components/Button';
 import { IconCopy, IconBrandWhatsapp, IconAt } from '@tabler/icons-react';
+import { useMatomo } from '@jonkoops/matomo-tracker-react';
 
 interface RecommendModalProps extends BaseModalProps {}
 
 const RecommendModal = ({ isOpen, onOpenChange }: RecommendModalProps) => {
     const { t } = useTranslation();
     const [copied, setCopied] = useState<boolean>();
+    const { trackEvent } = useMatomo();
 
     const copyTextToClipboard = (text: string) => {
         navigator.clipboard.writeText(text);
@@ -20,6 +22,31 @@ const RecommendModal = ({ isOpen, onOpenChange }: RecommendModalProps) => {
         copyTextToClipboard(t('dashboard.helpers.contents.recommendText'));
         setCopied(true);
         toast.info(t('dashboard.helpers.contents.toast'));
+        trackEvent({
+            category: 'Recommend Section on Start Page',
+            action: 'Click Button “Copy Text” in Recommend Modal',
+            name: 'huh',
+        });
+    };
+
+    const handleOnShareWithWhatsAppClick = () => {
+        window.open(`https://wa.me/?text=${encodeURIComponent(t('dashboard.helpers.contents.recommendText'))}`, '_blank');
+        trackEvent({
+            category: 'Recommend Section on Start Page',
+            action: 'Click Button “WhatsApp” in Recommend Modal',
+            name: 'huh',
+        });
+    };
+
+    const handleOnShareWithEmail = () => {
+        window.location.href = `mailto:?subject=${encodeURIComponent(t('dashboard.helpers.contents.recommendSubject'))}&body=${encodeURIComponent(
+            t('dashboard.helpers.contents.recommendText')
+        )}`;
+        trackEvent({
+            category: 'Recommend Section on Start Page',
+            action: 'Click Button “e-mail” in Recommend Modal',
+            name: 'huh',
+        });
     };
 
     useEffect(() => {
@@ -49,24 +76,10 @@ const RecommendModal = ({ isOpen, onOpenChange }: RecommendModalProps) => {
                     <Button className="w-full" onClick={handleCopyClick} leftIcon={<IconCopy />}>
                         {copied ? t('copied_text') : t('copy_text')}
                     </Button>
-                    <Button
-                        className="w-full"
-                        variant="outline"
-                        onClick={() => window.open(`https://wa.me/?text=${encodeURIComponent(t('dashboard.helpers.contents.recommendText'))}`, '_blank')}
-                        leftIcon={<IconBrandWhatsapp />}
-                    >
+                    <Button className="w-full" variant="outline" onClick={handleOnShareWithWhatsAppClick} leftIcon={<IconBrandWhatsapp />}>
                         {t('dashboard.helpers.channels.whatsApp')}
                     </Button>
-                    <Button
-                        className="w-full"
-                        onClick={() =>
-                            (window.location.href = `mailto:?subject=${encodeURIComponent(
-                                t('dashboard.helpers.contents.recommendSubject')
-                            )}&body=${encodeURIComponent(t('dashboard.helpers.contents.recommendText'))}`)
-                        }
-                        variant="outline"
-                        leftIcon={<IconAt />}
-                    >
+                    <Button className="w-full" onClick={handleOnShareWithEmail} variant="outline" leftIcon={<IconAt />}>
                         {t('dashboard.helpers.channels.email')}
                     </Button>
                 </div>
