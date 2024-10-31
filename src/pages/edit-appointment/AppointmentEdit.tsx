83b30873c80ebcd46ae25f18a06f95529e6ee9d2
John Angel <john.angel@lern-fair.de>
2024-10-31 12:48:39 +0100

    feat: Remove native-base from AppointmentEdit

--- src/pages/edit-appointment/AppointmentEdit.tsx
+++ src/pages/edit-appointment/AppointmentEdit.tsx
@@ -1,17 +1,23 @@
 import { useMutation, useQuery } from '@apollo/client';
-import { Button, Stack, useToast } from 'native-base';
-import { useCallback, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { gql } from '../../gql';
-import { AppointmentUpdateInput } from '../../gql/graphql';
-import { useLayoutHelper } from '../../hooks/useLayoutHelper';
-import AppointmentEditForm from './AppointmentEditForm';
-import { convertStartDate, formatStart } from '../../helper/appointment-helper';
 import { PUPIL_APPOINTMENT, STUDENT_APPOINTMENT } from '../Appointment';
 import { DateTime } from 'luxon';
 import { Breadcrumb } from '@/components/Breadcrumb';
 import { useBreadcrumbRoutes } from '@/hooks/useBreadcrumb';
+import { toast } from 'sonner';
+import { Button } from '@/components/Button';
+import { DatePicker } from '@/components/DatePicker';
+import { Label } from '@/components/Label';
+import { Input } from '@/components/Input';
+import CenterLoadingSpinner from '@/components/CenterLoadingSpinner';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/Select';
+import { TextArea } from '@/components/TextArea';
+import { Typography } from '@/components/Typography';
+import { AppointmentUpdateInput } from '@/gql/graphql';
+import { convertStartDate } from '@/helper/appointment-helper';
 
 type FormErrors = {
     title?: string;
@@ -26,11 +32,11 @@ type EditProps = {
     appointmentId: number;
 };
 
-export type UpdateAppointment = {
+export type FormState = {
     id: number;
     title: string;
     description: string;
-    date: string;
+    date?: Date;
     time: string;
     duration: number;
 };
@@ -49,44 +55,54 @@ query getAppointmentById($appointmentId: Float!) {
 }`);
 
 const AppointmentEdit: React.FC<EditProps> = ({ appointmentId }) => {
-    const { data } = useQuery(getAppointmentById, { variables: { appointmentId } });
+    const { data, loading: isLoading } = useQuery(getAppointmentById, { variables: { appointmentId } });
     const [errors, setErrors] = useState<FormErrors>({});
-    const [updatedAppointment, setUpdatedAppointment] = useState<UpdateAppointment>({
-        id: data?.appointment?.id ?? 0,
-        title: data?.appointment?.title ?? '',
-        description: data?.appointment?.description ?? '',
-        date: formatStart(data?.appointment.start).date,
-        time: DateTime.fromISO(data?.appointment.start).toFormat('HH:mm'),
-        duration: data?.appointment.duration ?? 0,
-    });
-
+    const [isEditing, setIsEditing] = useState(false);
     const { t } = useTranslation();
     const navigate = useNavigate();
-    const toast = useToast();
-    const { isMobile } = useLayoutHelper();
     const breadcrumbRoutes = useBreadcrumbRoutes();
+    const [updatedAppointment, setUpdatedAppointment] = useState<FormState>({
+        id: 0,
+        title: '',
+        description: '',
+        time: '',
+        duration: 0,
+    });
+
+    useEffect(() => {
+        if (data) {
+            setUpdatedAppointment({
+                id: data.appointment.id,
+                title: data.appointment.title ?? '',
+                description: data.appointment.description ?? '',
+                date: data.appointment.start ? DateTime.fromISO(data.appointment.start).toJSDate() : undefined,
+                time: data.appointment.start ? DateTime.fromISO(data.appointment.start).toFormat('HH:mm') : '',
+                duration: data.appointment.duration,
+            });
+        }
+    }, [data]);
 
     const isInputValid = () => {
+        const newErrors: FormErrors = {};
         if (!updatedAppointment.date) {
-            setErrors({ ...errors, date: t('appointment.errors.date') });
-            return false;
-        } else {
-            delete errors.date;
+            newErrors.date = t('appointment.errors.date');
         }
 
-        if (!updatedAppointment.time.length) {
-            setErrors({ ...errors, time: t('appointment.errors.time') });
-            return false;
-        } else {
-            delete errors.time;
+        const newStart = DateTime.fromISO(convertStartDate(updatedAppointment.date?.toISOString()!, updatedAppointment.time));
+        const nowPlus5Minutes = DateTime.now().plus({ minutes: 5 });
+        if (!updatedAppointment.time) {
+            newErrors.time = t('appointment.errors.time');
+        } else if (newStart < nowPlus5Minutes) {
+            newErrors.time = t('appointment.errors.timeNotInFiveMin');
         }
+
         if (updatedAppointment.duration === 0) {
-            setErrors({ ...errors, duration: t('appointment.errors.duration') });
-            return false;
-        } else {
-            delete errors.duration;
+            newErrors.duration = t('appointment.errors.duration');
         }
-        return true;
+
+        setErrors(newErrors);
+        const hasErrors = Object.keys(newErrors).length > 0;
+        return !hasErrors;
     };
 
     const [updateAppointment] = useMutation(
@@ -98,36 +114,29 @@ const AppointmentEdit: React.FC<EditProps> = ({ appointmentId }) => {
         { refetchQueries: [PUPIL_APPOINTMENT, STUDENT_APPOINTMENT] }
     );
 
-    const handleUpdateClick = useCallback(async () => {
+    const handleUpdateClick = async () => {
         if (isInputValid()) {
             const appointmentToBeUpdated: AppointmentUpdateInput = {
                 id: updatedAppointment.id,
                 title: updatedAppointment.title,
                 description: updatedAppointment.description,
-                start: convertStartDate(updatedAppointment.date, updatedAppointment.time),
+                start: convertStartDate(updatedAppointment.date?.toISOString()!, updatedAppointment.time),
                 duration: updatedAppointment.duration,
             };
-
-            await updateAppointment({ variables: { appointmentToBeUpdated } });
-            toast.show({ description: t('appointment.editSuccess'), placement: 'top' });
-            navigate(`/appointment/${updatedAppointment.id}`);
+            try {
+                setIsEditing(true);
+                await updateAppointment({ variables: { appointmentToBeUpdated } });
+                toast.success(t('appointment.editSuccess'));
+                navigate(`/appointment/${updatedAppointment.id}`);
+            } catch (error) {
+                toast.error(t('error'));
+            } finally {
+                setIsEditing(false);
+            }
         }
-    }, [
-        navigate,
-        t,
-        toast,
-        updatedAppointment,
-        updateAppointment,
-        updatedAppointment.date,
-        updatedAppointment.description,
-        updatedAppointment.duration,
-        updatedAppointment.id,
-        updatedAppointment.time,
-        updatedAppointment.title,
-        isInputValid,
-    ]);
-
-    const appointmentTile = data?.appointment?.title || t('appointment.appointmentTile.lecture', { position: data?.appointment?.position });
+    };
+    const lectureNumber = t('appointment.appointmentTile.lecture', { position: data?.appointment?.position });
+    const appointmentTile = data?.appointment?.title || lectureNumber;
 
     return (
         <>
@@ -139,18 +148,99 @@ const AppointmentEdit: React.FC<EditProps> = ({ appointmentId }) => {
                 ]}
                 className="mb-4"
             />
-            <AppointmentEditForm
-                errors={errors}
-                appointmentsCount={data?.appointment.position ?? 0}
-                updatedAppointment={updatedAppointment}
-                setUpdatedAppointment={setUpdatedAppointment}
-            />
-            <Stack space={3} mt={5} direction={isMobile ? 'column' : 'row'}>
-                <Button onPress={() => navigate(-1)} variant="outline">
-                    {t('appointment.goBack')}
-                </Button>
-                <Button onPress={async () => await handleUpdateClick()}>{t('appointment.saveChanges')}</Button>
-            </Stack>
+            <div className="flex flex-1 flex-col justify-between lg:justify-start">
+                {isLoading ? (
+                    <CenterLoadingSpinner />
+                ) : (
+                    <>
+                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                            <div className="flex flex-col gap-y-1">
+                                <Label htmlFor="title">{t('appointment.create.titleLabel')}</Label>
+                                <Input
+                                    className="w-full"
+                                    id="title"
+                                    value={updatedAppointment.title}
+                                    placeholder={lectureNumber}
+                                    onChange={(e) => setUpdatedAppointment({ ...updatedAppointment, title: e.target.value })}
+                                />
+                            </div>
+                            <div className="flex flex-col gap-y-1">
+                                <Label htmlFor="date">{t('appointment.create.dateLabel')}</Label>
+                                <DatePicker
+                                    id="date"
+                                    value={updatedAppointment.date}
+                                    onChange={(date) => setUpdatedAppointment({ ...updatedAppointment, date })}
+                                />
+                                {errors.date && (
+                                    <Typography variant="sm" className="text-red-500">
+                                        {errors.date}
+                                    </Typography>
+                                )}
+                            </div>
+                            <div className="flex flex-col gap-y-1">
+                                <Label htmlFor="time">{t('appointment.create.timeLabel')}</Label>
+                                <Input
+                                    className="w-full"
+                                    id="time"
+                                    type="time"
+                                    value={updatedAppointment.time}
+                                    placeholder={lectureNumber}
+                                    onChange={(e) => setUpdatedAppointment({ ...updatedAppointment, time: e.target.value })}
+                                />
+                                {errors.time && (
+                                    <Typography variant="sm" className="text-red-500">
+                                        {errors.time}
+                                    </Typography>
+                                )}
+                            </div>
+                            <div className="flex flex-col gap-y-1">
+                                <Label htmlFor="duration">{t('appointment.create.durationLabel')}</Label>
+                                <Select
+                                    value={updatedAppointment.duration.toString()}
+                                    onValueChange={(duration) => setUpdatedAppointment({ ...updatedAppointment, duration: Number(duration) })}
+                                >
+                                    <SelectTrigger id="duration" className="h-10 w-full">
+                                        <SelectValue />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="15">{t('course.selectOptions._15minutes')}</SelectItem>
+                                        <SelectItem value="30">{t('course.selectOptions._30minutes')}</SelectItem>
+                                        <SelectItem value="45">{t('course.selectOptions._45minutes')}</SelectItem>
+                                        <SelectItem value="60">{t('course.selectOptions._1hour')}</SelectItem>
+                                        <SelectItem value="90">{t('course.selectOptions._90minutes')}</SelectItem>
+                                        <SelectItem value="120">{t('course.selectOptions._2hour')}</SelectItem>
+                                        <SelectItem value="180">{t('course.selectOptions._3hour')}</SelectItem>
+                                        <SelectItem value="240">{t('course.selectOptions._4hour')}</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                                {errors.duration && (
+                                    <Typography variant="sm" className="text-red-500">
+                                        {errors.duration}
+                                    </Typography>
+                                )}
+                            </div>
+                            <div className="flex flex-col gap-y-1 lg:col-span-2">
+                                <Label htmlFor="description">{t('appointment.create.descriptionLabel')}</Label>
+                                <TextArea
+                                    className="resize-none h-20 lg:h-20 w-full"
+                                    id="description"
+                                    value={updatedAppointment.description}
+                                    onChange={(e) => setUpdatedAppointment({ ...updatedAppointment, description: e.target.value })}
+                                    placeholder={t('appointment.create.descriptionPlaceholder')}
+                                />
+                            </div>
+                        </div>
+                        <div className="flex flex-col lg:flex-row gap-4 mt-10">
+                            <Button disabled={isEditing} onClick={() => navigate(-1)} variant="outline" className="w-full lg:w-fit">
+                                {t('appointment.goBack')}
+                            </Button>
+                            <Button isLoading={isEditing} onClick={handleUpdateClick} className="w-full lg:w-fit">
+                                {t('appointment.saveChanges')}
+                            </Button>
+                        </div>
+                    </>
+                )}
+            </div>
         </>
     );
 };
