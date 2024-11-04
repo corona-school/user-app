0769e422fd34b95fb07282619ee4ef219cd93014
Jonas Wilms <jonaswilms2000@gmail.com>
2024-11-04 10:34:30 +0100

    feat: Track referrers via Matomo (#703)

--- src/matomo.ts
+++ src/matomo.ts
@@ -1,7 +1,7 @@
 import { createInstance } from '@jonkoops/matomo-tracker-react';
 import { DISABLE_MATOMO } from './config';
 
-export default createInstance({
+const matomo = createInstance({
     urlBase: process.env.REACT_APP_MATOMO_URL,
     siteId: 6,
     disabled: process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test' || DISABLE_MATOMO === 'true',
@@ -15,3 +15,16 @@ export default createInstance({
         enableLinkTracking: true,
     },
 });
+
+// If a user arrives at some-page?referrer=some-referrer,
+// track it using Matomo
+const searchParams = new URLSearchParams(window.location.search);
+if (searchParams.has('referrer')) {
+    matomo.trackEvent({
+        category: 'opening',
+        action: 'from-referrer',
+        name: searchParams.get('referrer')!,
+    });
+}
+
+export default matomo;
