19ea2303976dbd5ca1a68a65b5fbab089c146e16
John Angel <161815068+JeangelLF@users.noreply.github.com>
2024-11-06 09:03:06 +0100

    feat: Add CalendlyWidget on the RequireScreeningModal (#704)
    ## Ticket
    
    Resolves https://github.com/corona-school/project-user/issues/1368
    
    ## What was done?
    
    - Updated RequireScreeningModal to include the Calendly widget
    - Added matomo tracking events
    
    ## Preview
    <img width="1440" alt="Bildschirmfoto 2024-11-05 um 13 20 52"
    src="https://github.com/user-attachments/assets/2b5c45fa-6668-4579-a5e3-d48c93db9e72">

--- src/components/CenterLoadingSpinner.tsx
+++ src/components/CenterLoadingSpinner.tsx
@@ -1,11 +1,14 @@
+import { cn } from '@/lib/Tailwind';
 import { IconLoader2 } from '@tabler/icons-react';
 
-type Props = {};
+type Props = {
+    className?: string;
+};
 
-const CenterLoadingSpinner: React.FC<Props> = () => {
+const CenterLoadingSpinner: React.FC<Props> = ({ className }) => {
     return (
         <div className="flex flex-1 justify-center items-center">
-            <IconLoader2 className="size-6 text-primary animate-spin" />
+            <IconLoader2 className={cn('size-6 text-primary animate-spin', className)} />
         </div>
     );
 };
