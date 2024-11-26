44bb4b54614f6127b77722da3228e000a1e2f517
John Angel <john.angel@lern-fair.de>
2024-11-26 10:08:35 +0100

    feat: Make Select generic

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -75,15 +75,15 @@ const SelectItem = React.forwardRef<React.ElementRef<typeof SelectPrimitive.Item
     )
 );
 
-interface SelectInputProps {
-    value: string;
-    onValueChange: (value: string) => void;
+interface SelectInputProps<T> {
+    value: T;
+    onValueChange: (value: T) => void;
     className?: string;
     options: { value: string; label: string }[] | string[];
     placeholder?: string;
 }
 
-export const SelectInput = ({ value, onValueChange, options, placeholder, className }: SelectInputProps) => (
+export const SelectInput = <T extends string>({ value, onValueChange, options, placeholder, className }: SelectInputProps<T>) => (
     <Select value={value} onValueChange={onValueChange}>
         <SelectTrigger className={cn('h-10 w-full', className)} placeholder={placeholder}>
             <SelectValue />
