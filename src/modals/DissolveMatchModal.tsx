e83cbb05ad810540153ed9c0f1a01245a419ea3f
realmayus <realmayus@gmail.com>
2025-01-20 17:08:18 +0100

    add free text field for other dissolve reason

--- src/modals/DissolveMatchModal.tsx
+++ src/modals/DissolveMatchModal.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal, Radio, Row, useTheme, VStack } from 'native-base';
+import { Button, Input, Modal, Radio, Row, useTheme, VStack } from 'native-base';
 import { useState } from 'react';
 import { useUserType } from '../hooks/useApollo';
 import { useTranslation } from 'react-i18next';
@@ -8,7 +8,7 @@ import DisableableButton from '../components/DisablebleButton';
 type DissolveModalProps = {
     showDissolveModal: boolean | undefined;
     alsoShowWarningModal?: boolean | undefined;
-    onPressDissolve: (dissolveReasons: Dissolve_Reason[]) => any;
+    onPressDissolve: (dissolveReasons: Dissolve_Reason[], otherFreeText: string | undefined) => any;
     onPressBack: () => any;
 };
 
@@ -18,6 +18,7 @@ const DissolveMatchModal: React.FC<DissolveModalProps> = ({ showDissolveModal, a
     const { space } = useTheme();
     const userType = useUserType();
     const [reasons, setReasons] = useState<Dissolve_Reason[]>([]);
+    const [otherFreeText, setOtherFreeText] = useState<string>('');
     const availableReasons = [
         Dissolve_Reason.Ghosted,
         Dissolve_Reason.NoMoreHelpNeeded,
@@ -51,22 +52,31 @@ const DissolveMatchModal: React.FC<DissolveModalProps> = ({ showDissolveModal, a
                     <>
                         <Modal.Header>{t('matching.dissolve.modal.title')}</Modal.Header>
                         <Modal.Body>
-                            <Radio.Group name="dissolve-reason" value={reasons[0]} onChange={(key) => setReasons([key as Dissolve_Reason])}>
-                                <VStack space={space['1']}>
-                                    {availableReasons.map((key) => (
-                                        <Radio key={key} value={key}>
-                                            {t(`matching.dissolveReasons.${userType}.${key}` as unknown as TemplateStringsArray)}
-                                        </Radio>
-                                    ))}
-                                </VStack>
-                            </Radio.Group>
+                            <VStack space={space['1']}>
+                                <Radio.Group name="dissolve-reason" value={reasons[0]} onChange={(key) => setReasons([key as Dissolve_Reason])}>
+                                    <VStack space={space['1']}>
+                                        {availableReasons.map((key) => (
+                                            <Radio key={key} value={key}>
+                                                {t(`matching.dissolveReasons.${userType}.${key}` as unknown as TemplateStringsArray)}
+                                            </Radio>
+                                        ))}
+                                    </VStack>
+                                </Radio.Group>
+                                {reasons.includes(Dissolve_Reason.Other) && (
+                                    <Input
+                                        placeholder={t('matching.dissolve.modal.otherFreeText')}
+                                        value={otherFreeText}
+                                        onChangeText={(text) => setOtherFreeText(text)}
+                                    />
+                                )}
+                            </VStack>
                         </Modal.Body>
                         <Modal.Footer>
                             <Row space={space['1']}>
                                 <DisableableButton
-                                    isDisabled={reasons.length === 0}
+                                    isDisabled={reasons.length === 0} // todo: other freetext required?
                                     reasonDisabled={t('matching.dissolve.modal.tooltip')}
-                                    onPress={() => onPressDissolve(reasons ?? [Dissolve_Reason.Unknown])}
+                                    onPress={() => onPressDissolve(reasons, reasons.includes(Dissolve_Reason.Other) ? otherFreeText : undefined)}
                                 >
                                     {t('matching.dissolve.modal.btn')}
                                 </DisableableButton>
