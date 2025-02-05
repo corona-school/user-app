0dfb8927cae1a80394f2ff85bd9e00d37e7d2786
John Angel <john.angel@lern-fair.de>
2025-02-05 14:15:58 +0100

    refactor: Replace Navigate component with navigate function

--- src/pages/LoginWithIDP.tsx
+++ src/pages/LoginWithIDP.tsx
@@ -1,27 +1,35 @@
 import { SsoAuthStatus } from '@/gql/graphql';
 import LinkIDPModal from '@/modals/LinkIDPModal';
 import { useCallback, useEffect, useState } from 'react';
-import { Navigate, useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import CenterLoadingSpinner from '../components/CenterLoadingSpinner';
 import useApollo from '../hooks/useApollo';
 
 const LoginWithIDP = () => {
     const [showReauthenticate, setShowReauthenticate] = useState(false);
     const { roles, loginWithSSO } = useApollo();
-    const [ssoStatus, setSsoStatus] = useState<SsoAuthStatus>();
     const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
 
     const code = searchParams.get('code');
 
     const handleOnLoginWithSSO = useCallback(
         async (idpCode: string) => {
-            const result = await loginWithSSO(idpCode);
-            setSsoStatus(result);
-            if (result === SsoAuthStatus.Link) {
+            const ssoStatus = await loginWithSSO(idpCode);
+
+            if (roles.includes('SSO_REGISTERING_USER') && ssoStatus === SsoAuthStatus.Register) {
+                navigate('/registration');
+            }
+
+            if (ssoStatus === SsoAuthStatus.Link) {
                 setShowReauthenticate(true);
             }
+
+            if (roles.includes('USER')) {
+                navigate('/');
+            }
         },
-        [loginWithSSO]
+        [loginWithSSO, roles, navigate]
     );
 
     useEffect(() => {
@@ -29,14 +37,6 @@ const LoginWithIDP = () => {
             handleOnLoginWithSSO(code);
         }
     }, [code, handleOnLoginWithSSO]);
-
-    if (roles.includes('SSO_REGISTERING_USER') && ssoStatus === SsoAuthStatus.Register) {
-        return <Navigate to="/registration" />;
-    }
-
-    if (roles.includes('USER')) {
-        return <Navigate to="/" />;
-    }
 
     return (
         <>
