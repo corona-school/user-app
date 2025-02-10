fdbff4ca2729e5c4d36b4e50efdc48439ca8ea4c
John Angel <john.angel@lern-fair.de>
2025-02-10 10:26:00 +0100

    fix: Add timeout before redirecting to google

--- src/hooks/useLoginWithIDP.ts
+++ src/hooks/useLoginWithIDP.ts
@@ -4,15 +4,17 @@ const GOOGLE_AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
 
 const useLoginWithIDP = () => {
     const loginWithGoogle = () => {
-        const AUTH_URL = `${GOOGLE_AUTH_URL}?${new URLSearchParams({
-            prompt: 'consent',
-            response_type: 'code',
-            client_id: GOOGLE_CLIENT_ID,
-            scope: 'openid profile email',
-            enable_granular_consent: 'true',
-            redirect_uri: `${window.location.origin}/login-with`,
-        }).toString()}`;
-        window.location.href = AUTH_URL;
+        setTimeout(() => {
+            const AUTH_URL = `${GOOGLE_AUTH_URL}?${new URLSearchParams({
+                prompt: 'consent',
+                response_type: 'code',
+                client_id: GOOGLE_CLIENT_ID,
+                scope: 'openid profile email',
+                enable_granular_consent: 'true',
+                redirect_uri: `${window.location.origin}/login-with`,
+            }).toString()}`;
+            window.location.href = AUTH_URL;
+        }, 0);
     };
     return { loginWithGoogle };
 };
