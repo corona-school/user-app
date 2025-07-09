5f98c19bad4275ccdcd2210b30eca1924ee44e56
John Angel <john.angel@lern-fair.de>
2025-07-09 16:07:12 +0200

    feat: Update Layout of UpdateData components

--- src/pages/student/matching/UpdateData.tsx
+++ src/pages/student/matching/UpdateData.tsx
@@ -5,12 +5,15 @@ import { useTranslation } from 'react-i18next';
 import { CalendarPreferences, Language, StudentLanguage } from '@/gql/graphql';
 import { gql } from '@/gql';
 import { Label } from '@/components/Label';
-import { LanguageSelector } from '@/components/LanguageSelector';
+import { LanguageIcon, LanguageSelector } from '@/components/LanguageSelector';
 import { Typography } from '@/components/Typography';
 import { WeeklyAvailabilitySelector } from '@/components/availability/WeeklyAvailabilitySelector';
 import { NextPrevButtons } from '@/widgets/NextPrevButtons';
 import { toast } from 'sonner';
 import { logError } from '@/log';
+import { Modal, ModalFooter, ModalHeader, ModalTitle } from '@/components/Modal';
+import { Button } from '@/components/Button';
+import { asTranslationKey } from '@/helper/string-helper';
 
 const ME_UPDATE_MUTATION = gql(`
     mutation changeStudentStateData($languages: [StudentLanguage!], $calendarPreferences: CalendarPreferences) {
@@ -29,11 +32,15 @@ interface UpdateDataProps {
     onBack: () => void;
 }
 
+type ModalType = 'languages';
+
 const UpdateData = ({ refetchQuery, profile, onNext, onBack }: UpdateDataProps) => {
     const { t } = useTranslation();
     const isLoading = !profile;
     const [selectedLanguages, setSelectedLanguages] = useState<Language[]>(profile?.languages ?? []);
     const [calendarPreferences, setCalendarPreferences] = useState<CalendarPreferences | undefined>(profile?.calendarPreferences);
+    const [modalType, setModalType] = useState<ModalType>();
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const [meUpdate, { loading: isUpdating }] = useMutation(ME_UPDATE_MUTATION, { refetchQueries: [refetchQuery] });
 
@@ -66,15 +73,30 @@ const UpdateData = ({ refetchQuery, profile, onNext, onBack }: UpdateDataProps) 
         }
     };
 
+    const handleOnOpenModal = (type: ModalType) => {
+        setModalType(type);
+        setIsModalOpen(true);
+    };
+
     return (
         <div className="flex flex-col gap-y-6">
             <div>
                 <Typography variant="h4">{t('matching.wizard.student.profile.title')}</Typography>
                 <Typography className="mb-2">{t('matching.wizard.student.profile.subtitle')}</Typography>
             </div>
-            <div className="flex flex-col gap-y-1">
+            <div className="flex flex-col gap-y-1 max-w-[500px] overflow-hidden w-full">
                 <Label>{t('profile.Languages.labelStudent')}</Label>
-                <LanguageSelector multiple value={selectedLanguages} setValue={setSelectedLanguages} />
+                <Button className="w-full h-auto py-2 min-h-10" variant="input" size="input" onClick={() => handleOnOpenModal('languages')}>
+                    <div className="w-full flex items-center gap-x-4 min-w-[200px] flex-wrap gap-y-4">
+                        {!selectedLanguages && t('edit')}
+                        {selectedLanguages.map((e) => (
+                            <span className="flex items-center gap-x-1">
+                                <LanguageIcon className="size-4" languageName={e} />
+                                {t(asTranslationKey(`lernfair.languages.${e.toLowerCase()}`))}
+                            </span>
+                        ))}
+                    </div>
+                </Button>
             </div>
             <div className="flex flex-col gap-y-2">
                 <Label>{t('profile.availability')}</Label>
@@ -85,6 +107,30 @@ const UpdateData = ({ refetchQuery, profile, onNext, onBack }: UpdateDataProps) 
                 />
             </div>
             <NextPrevButtons isLoading={isUpdating} onlyNext disablingNext={getIsNextDisabled()} onPressPrev={onBack} onPressNext={handleOnNext} />
+            <Modal isOpen={isModalOpen} onOpenChange={setIsModalOpen}>
+                <ModalHeader>
+                    <ModalTitle>{t('change')}</ModalTitle>
+                </ModalHeader>
+                <div>
+                    {modalType === 'languages' && (
+                        <div className="flex flex-col gap-y-2">
+                            <Label>{t('profile.Languages.labelStudent')}</Label>
+                            <LanguageSelector multiple value={selectedLanguages} setValue={setSelectedLanguages} />
+                        </div>
+                    )}
+                </div>
+                <ModalFooter>
+                    <Button
+                        className="w-full lg:w-fit"
+                        variant="outline"
+                        onClick={() => {
+                            setIsModalOpen(false);
+                        }}
+                    >
+                        {t('back')}
+                    </Button>
+                </ModalFooter>
+            </Modal>
         </div>
     );
 };
