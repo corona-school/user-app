94eed7ad0909148fc9cacfb694c5d7867a75a60c
John Angel <john.angel@lern-fair.de>
2025-07-09 15:31:01 +0200

    feat: Expose grade/language icons

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,16 +1,19 @@
 import { Language } from '@/gql/graphql';
 import { languageIcons, languageList } from '@/I18n';
+import { cn } from '@/lib/Tailwind';
 import { IconQuestionMark } from '@tabler/icons-react';
 import { EnumSelector } from './EnumSelector';
 
 export const LanguageSelector = EnumSelector(
     Language,
     (k) => `lernfair.languages.${k.toLowerCase()}` as any,
-    (k) => {
-        const language = languageList.find((e) => e.long === k.toLowerCase());
-        if (!language) return <IconQuestionMark className="size-6" />;
-        const short = language.short as keyof typeof languageIcons;
-        const Icon = languageIcons[short];
-        return <Icon className="rounded-full size-6" />;
-    }
+    (k) => <LanguageIcon languageName={k} />
 );
+
+export const LanguageIcon = ({ languageName, className }: { languageName: string; className?: string }) => {
+    const item = languageList.find((e) => e.long === languageName.toLowerCase());
+    if (!item) return <IconQuestionMark className="size-6" />;
+    const short = item.short as keyof typeof languageIcons;
+    const Icon = languageIcons[short];
+    return <Icon className={cn('rounded-full size-6', className)} />;
+};
