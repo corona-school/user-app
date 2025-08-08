8358ee4bc75125bb47d110abd325fa87917c3a58
John Angel <john.angel@lern-fair.de>
2025-08-08 10:44:57 +0200

    style: Make invisible registration step back/next buttons when no handler is passed

--- src/pages/registration/RegistrationStep.tsx
+++ src/pages/registration/RegistrationStep.tsx
@@ -28,26 +28,26 @@ export const RegistrationStep = ({ onBack, onNext, isBackDisabled, isNextDisable
         <div className={cn('animate-in fade-in-5 duration-300 relative flex flex-col flex-1 max-w-[800px] w-full mx-auto md:pb-24', className)}>
             <div className="z-10 flex flex-1 flex-col items-center justify-center max-w-[536px] mx-auto px-[23px] md:px-0">{children}</div>
             <div className="md:absolute bottom-0 px-10 flex justify-between w-full pb-8 md:bottom-1/2 md:px-0">
-                {onBack && (
-                    <Button
-                        disabled={isBackDisabled}
-                        onClick={onBack}
-                        variant="ghost"
-                        className="size-[70px] rounded-full border border-transparent hover:border-primary transition-colors duration-300 ease-in-out px-0"
-                    >
-                        <IconArrowLeft size={48} className="!stroke-[2px]" />
-                    </Button>
-                )}
-                {onNext && (
-                    <Button
-                        disabled={isNextDisabled}
-                        onClick={onNext}
-                        variant="ghost"
-                        className="size-[70px] rounded-full border border-transparent hover:border-primary transition-colors duration-300 ease-in-out px-0"
-                    >
-                        <IconArrowRight size={48} className="!stroke-[2px]" />
-                    </Button>
-                )}
+                <Button
+                    disabled={isBackDisabled}
+                    onClick={onBack}
+                    variant="ghost"
+                    className={cn('size-[70px] rounded-full border border-transparent hover:border-primary transition-colors duration-300 ease-in-out px-0', {
+                        invisible: !onBack,
+                    })}
+                >
+                    <IconArrowLeft size={48} className="!stroke-[2px]" />
+                </Button>
+                <Button
+                    disabled={isNextDisabled}
+                    onClick={onNext}
+                    variant="ghost"
+                    className={cn('size-[70px] rounded-full border border-transparent hover:border-primary transition-colors duration-300 ease-in-out px-0', {
+                        invisible: !onNext,
+                    })}
+                >
+                    <IconArrowRight size={48} className="!stroke-[2px]" />
+                </Button>
             </div>
         </div>
     );
