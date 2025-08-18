c542b0646f5c0fe17642c0fddf58ee320793946c
John Angel <john.angel@lern-fair.de>
2025-08-18 12:16:07 +0200

    feat: Update Languages

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -4,6 +4,33 @@ import { cn } from '@/lib/Tailwind';
 import { IconQuestionMark } from '@tabler/icons-react';
 import { EnumSelector } from './EnumSelector';
 
+const priorityLanguages: Language[] = [
+    Language.Deutsch,
+    Language.TRkisch,
+    Language.Arabisch,
+    Language.Ukrainisch,
+    Language.Russisch,
+    Language.Polnisch,
+    Language.Englisch,
+];
+
+const getSortedLanguages = () => {
+    const entries = Object.entries(Language);
+
+    const priorityEntries = priorityLanguages.map((val) => entries.find(([_, v]) => v === val)!);
+
+    const remainingEntries = entries
+        .filter(([_, v]) => !priorityLanguages.includes(v))
+        .sort((a, b) => {
+            if (a[1] === Language.Andere) return 1;
+            if (b[1] === Language.Andere) return -1;
+            return a[1].localeCompare(b[1]);
+        });
+    const sortedEntries = [...priorityEntries, ...remainingEntries];
+
+    return Object.fromEntries(sortedEntries);
+};
+
 export const LanguageSelector = EnumSelector(
     Language,
     (k) => `lernfair.languages.${k.toLowerCase()}` as any,
