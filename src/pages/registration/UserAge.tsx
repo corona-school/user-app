d5dc92f860630dc2e068eb714e4643e514573fc1
John Angel <161815068+JeangelLF@users.noreply.github.com>
2025-08-18 18:10:59 +0200

    Fix: Registration funnel minor feedback (#862)
    ## Ticket
    
    Part of https://github.com/corona-school/project-user/issues/1516
    
    ## What was done?
    
    Mostly changed some texts / added some descriptions, hide some others
    and added a minimum age validation

--- src/pages/registration/UserAge.tsx
+++ src/pages/registration/UserAge.tsx
@@ -3,26 +3,45 @@ import { RegistrationStep, RegistrationStepProps, RegistrationStepTitle } from '
 import { useRegistrationForm } from './useRegistrationForm';
 import { Input } from '@/components/Input';
 import { Typography } from '@/components/Typography';
+import { useState } from 'react';
 
 interface UserAgeProps extends RegistrationStepProps {}
+
+const MIN_AGE = 7;
 
 export const UserAge = ({ onBack, onNext }: UserAgeProps) => {
     const { form, onFormChange } = useRegistrationForm();
     const { t } = useTranslation();
+    const [error, setError] = useState(!!form.age && form.age < MIN_AGE ? t('registration.steps.userAge.tooYoungError', { minAge: MIN_AGE }) : '');
 
     const onChange = (value: string) => {
-        onFormChange({ age: Number(value.replace(/\D/g, '').substring(0, 2)) });
+        const age = Number(value.replace(/\D/g, '').substring(0, 2));
+        if (age >= MIN_AGE) {
+            setError('');
+        } else {
+            setError(t('registration.steps.userAge.tooYoungError', { minAge: MIN_AGE }));
+        }
+        onFormChange({ age });
     };
 
     return (
-        <RegistrationStep onBack={onBack} onNext={onNext} isNextDisabled={!form.age}>
+        <RegistrationStep onBack={onBack} onNext={onNext} isNextDisabled={!form.age || !!error}>
             <RegistrationStepTitle className="md:mb-10">{t('registration.steps.userAge.title')}</RegistrationStepTitle>
             <Typography variant="body-lg" className="text-center mb-10 md:whitespace-pre-line text-balance">
                 {t('registration.steps.userAge.description')}
             </Typography>
-            <div className="flex flex-col gap-y-3 w-full max-w-20 pb-32 md:pb-0">
-                <div className="w-full flex flex-col justify-center gap-y-1">
-                    <Input autoFocus variant="white" className="w-full h-12" value={form.age || ''} onChangeText={onChange} />
+            <div className="flex flex-col items-center gap-y-3 w-full pb-32 md:pb-0">
+                <div className="w-full flex flex-col items-center justify-center gap-y-1 max-w-[300px] text-center">
+                    <Input
+                        autoFocus
+                        variant="white"
+                        className="w-full h-12 max-w-20"
+                        value={form.age || ''}
+                        onChangeText={onChange}
+                        errorMessage={error}
+                        errorMessageClassName="min-h-10"
+                        min={MIN_AGE}
+                    />
                 </div>
             </div>
         </RegistrationStep>
