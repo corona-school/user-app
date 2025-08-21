7afc03dd02ebd2c1c8174d48b30043b8258250a6
John Angel <john.angel@lern-fair.de>
2025-08-21 11:23:05 +0200

    feat: Update modals with new components

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -47,6 +47,10 @@ const ModalContent = React.forwardRef<React.ElementRef<typeof DialogPrimitive.Co
                         e.preventDefault();
                     }
                 }}
+                onCloseAutoFocus={(event) => {
+                    event.preventDefault();
+                    document.body.style.pointerEvents = '';
+                }}
             >
                 {children}
                 <DialogPrimitive.Close
