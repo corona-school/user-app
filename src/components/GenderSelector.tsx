22a4756ae0aed57dcb56a3da43af2b6fffac6526
John Angel <161815068+JeangelLF@users.noreply.github.com>
2025-11-27 08:05:30 +0100

    Feat: Add user gender step in funnel (#913)
    ## Ticket
    
    Resolves https://github.com/corona-school/project-user/issues/1562
    
    ## What was done?
    
    - Added gender step in registration funnel

---
+++ src/components/GenderSelector.tsx
@@ -0,0 +1,23 @@
+import { Gender } from '@/gql/graphql';
+import { cn } from '@/lib/Tailwind';
+import { EnumSelector } from './EnumSelector';
+import { IconGenderGenderqueer, IconGenderFemale, IconGenderMale } from '@tabler/icons-react';
+
+export const GenderSelector = EnumSelector(
+    Gender,
+    (k) => `lernfair.genders.${k.toLowerCase()}` as any,
+    (k) => <GenderIcon genderName={k} />
+);
+
+export const GenderIcon = ({ genderName, className }: { genderName: Gender; className?: string }) => {
+    switch (genderName) {
+        case Gender.Male:
+            return <IconGenderMale className={cn('rounded-full size-6 flex-shrink-0', className)} />;
+        case Gender.Female:
+            return <IconGenderFemale className={cn('rounded-full size-6 flex-shrink-0', className)} />;
+        case Gender.Diverse:
+            return <IconGenderGenderqueer className={cn('rounded-full size-6 flex-shrink-0', className)} />;
+        default:
+            return null;
+    }
+};
