57d81236210f02817d702056db33f4b7eb0f9004
John Angel <161815068+JeangelLF@users.noreply.github.com>
2026-01-07 12:54:56 +0100

    feat: Add missing course option / Add missing tooltips (#926)
    ## What was done?
    
    - Added missing option: `Teilnehmer:innen erlauben in den Chat zu
    schreiben`
    - Added missing tooltips in course create page

--- src/pages/course-creation/CourseSettings.tsx
+++ src/pages/course-creation/CourseSettings.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { Typography } from '@/components/Typography';
 import { LFSubCourse } from '@/types/lernfair/Course';
 import { Dispatch, SetStateAction } from 'react';
+import { InfoTooltipButton } from '@/components/Tooltip';
 
 interface Props {
     subcourse: LFSubCourse;
@@ -16,9 +17,12 @@ const CourseSettings: React.FC<Props> = ({ subcourse, setSubcourse }) => {
         <div className="flex flex-col gap-2.5 w-full">
             <Typography variant="h4">{t('course.CourseDate.form.otherHeadline')}</Typography>
             <div className="flex justify-between">
-                <Label htmlFor="1" className="inline flex-grow text-base font-normal">
-                    {t('course.CourseDate.form.otherOptionStart')}
-                </Label>
+                <div className="inline-flex align-baseline gap-1.5">
+                    <Label htmlFor="1" className="inline flex-grow text-base font-medium">
+                        {t('course.CourseDate.form.otherOptionStart')}
+                    </Label>
+                    <InfoTooltipButton tooltipContent={t('course.CourseDate.form.otherOptionStartToolTip')} />
+                </div>
                 <Switch
                     id="1"
                     className="flex-shrink-0"
@@ -27,14 +31,17 @@ const CourseSettings: React.FC<Props> = ({ subcourse, setSubcourse }) => {
                 />
             </div>
             <div className="flex justify-between">
-                <Label htmlFor="2" className="inline flex-grow text-base font-normal">
+                <Label htmlFor="2" className="inline flex-grow text-base font-medium">
                     {t('course.CourseDate.form.otherOptionContact')}
                 </Label>
             </div>
             <div className="flex justify-between ml-5">
-                <Label htmlFor="3" className="inline flex-grow text-base font-normal">
-                    {t('course.CourseDate.form.otherProspects')}
-                </Label>
+                <div className="inline-flex flex-grow align-baseline gap-1.5 text-end mr-20">
+                    <Label htmlFor="3" className="inline flex-grow text-base font-medium">
+                        {t('course.CourseDate.form.otherProspects')}
+                    </Label>
+                    <InfoTooltipButton tooltipContent={t('course.CourseDate.form.prospectContactTooltip')} />
+                </div>
                 <Switch
                     id="3"
                     className="flex-shrink-0"
@@ -43,14 +50,28 @@ const CourseSettings: React.FC<Props> = ({ subcourse, setSubcourse }) => {
                 />
             </div>
             <div className="flex justify-between ml-5">
-                <Label htmlFor="4" className="inline flex-grow text-base font-normal">
-                    {t('course.CourseDate.form.otherParticipants')}
-                </Label>
+                <div className="inline-flex flex-grow align-baseline gap-1.5 text-end mr-20">
+                    <Label htmlFor="4" className="inline flex-grow text-base font-medium">
+                        {t('course.CourseDate.form.otherParticipants')}
+                    </Label>
+                    <InfoTooltipButton tooltipContent={t('course.CourseDate.form.participantsContactTooltip')} />
+                </div>
                 <Switch
                     id="4"
                     className="flex-shrink-0"
                     checked={!!subcourse.allowChatContactParticipants}
                     onCheckedChange={(c) => setSubcourse((s) => ({ ...s, allowChatContactParticipants: c }))}
+                />
+            </div>
+            <div className="flex justify-between">
+                <Label htmlFor="4" className="inline flex-grow text-base font-medium">
+                    {t('course.CourseDate.form.allowChatContact')}
+                </Label>
+                <Switch
+                    id="4"
+                    className="flex-shrink-0"
+                    checked={!!subcourse.course.allowContact}
+                    onCheckedChange={(c) => setSubcourse((s) => ({ ...s, course: { ...s.course, allowContact: c } }))}
                 />
             </div>
         </div>
