58cbf8a1e41590761cac602135ac27f91c54219b
John Angel <john.angel@lern-fair.de>
2026-02-26 12:18:33 +0100

    feat: Update SpecialTeachingExperienceSelector

--- src/components/SpecialTeachingExperienceSelector.tsx
+++ src/components/SpecialTeachingExperienceSelector.tsx
@@ -1,4 +1,5 @@
 import { EnumSelector } from './EnumSelector';
+import { TeachingExperienceLevelEnum } from './TeachingExperienceLevelSelector';
 
 export enum SpecialTeachingExperienceEnum {
     daz = 'daz',
@@ -13,5 +14,52 @@ export enum SpecialTeachingExperienceEnum {
     traumaSensitivePedagogy = 'traumaSensitivePedagogy',
     other = 'other',
 }
+
+interface SplitSpecialExperienceResult {
+    specialTeachingExperience: SpecialTeachingExperienceEnum[];
+    freeText?: string;
+    teachingExperienceLevel: {
+        '1:1'?: TeachingExperienceLevelEnum;
+        group?: TeachingExperienceLevelEnum;
+    };
+}
+
+export const splitSpecialExperience = (mixedValues: (SpecialTeachingExperienceEnum | string)[]): SplitSpecialExperienceResult => {
+    const result: SplitSpecialExperienceResult = {
+        specialTeachingExperience: [],
+        teachingExperienceLevel: {},
+    };
+    mixedValues.forEach((value) => {
+        if (Object.values(SpecialTeachingExperienceEnum).includes(value as SpecialTeachingExperienceEnum)) {
+            result.specialTeachingExperience.push(value as SpecialTeachingExperienceEnum);
+        }
+        if (value.endsWith(' 1:1')) {
+            result.teachingExperienceLevel['1:1'] = value.replace(' 1:1', '') as TeachingExperienceLevelEnum;
+        } else if (value.endsWith(' group')) {
+            result.teachingExperienceLevel.group = value.replace(' group', '') as TeachingExperienceLevelEnum;
+        } else if (value.startsWith(`${SpecialTeachingExperienceEnum.other}:`)) {
+            result.freeText = value.replace(`${SpecialTeachingExperienceEnum.other}:`, '');
+        }
+    });
+    return result;
+};
+
+export const combineSpecialExperience = (
+    specialTeachingExperience: SpecialTeachingExperienceEnum[],
+    freeText: string | undefined,
+    teachingExperienceLevel: { '1:1'?: TeachingExperienceLevelEnum; group?: TeachingExperienceLevelEnum }
+): string[] => {
+    const combinedValues: string[] = [...specialTeachingExperience];
+    if (teachingExperienceLevel['1:1']) {
+        combinedValues.push(`${teachingExperienceLevel['1:1']} 1:1`);
+    }
+    if (teachingExperienceLevel.group) {
+        combinedValues.push(`${teachingExperienceLevel.group} group`);
+    }
+    if (freeText) {
+        combinedValues.push(freeText);
+    }
+    return combinedValues;
+};
 
 export const SpecialTeachingExperienceSelector = EnumSelector(SpecialTeachingExperienceEnum, (k) => `specialTeachingExperience.${k}`);
