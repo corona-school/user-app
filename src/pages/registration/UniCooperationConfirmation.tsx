d6790065e38dee31a7031c67ca971a95b6cf5da1
John Angel <john.angel@lern-fair.de>
2026-03-23 16:15:44 +0100

    feat: Update UniCooperationConfirmation

--- src/pages/registration/UniCooperationConfirmation.tsx
+++ src/pages/registration/UniCooperationConfirmation.tsx
@@ -2,6 +2,8 @@ import { Trans, useTranslation } from 'react-i18next';
 import { usePageTitle } from '../../hooks/usePageTitle';
 import { RegistrationStep, RegistrationStepTitle } from './RegistrationStep';
 import { Typography } from '@/components/Typography';
+import { useRegistrationForm } from './useRegistrationForm';
+import { RegistrationStep as RegistrationStepEnum } from './util';
 
 interface UniCooperationConfirmationProps {
     onBack?: () => void;
@@ -10,14 +12,31 @@ interface UniCooperationConfirmationProps {
 export const UniCooperationConfirmation = ({ onBack }: UniCooperationConfirmationProps) => {
     usePageTitle(`Registrierung: Uni-Kooperation - (Helfer:in)`);
     const { t } = useTranslation();
+    const { onFormChange } = useRegistrationForm();
 
     return (
         <RegistrationStep onBack={onBack}>
             <RegistrationStepTitle>{t('registration.steps.uniCooperationConfirmation.title')}</RegistrationStepTitle>
             <div className="text-3xl my-4">🥳</div>
+            <Typography className="text-center md:whitespace-pre-line">{t('registration.steps.uniCooperationConfirmation.description')}</Typography>
+            <Typography className="text-center md:whitespace-pre-line my-4">
+                <Trans
+                    i18nKey="registration.steps.uniCooperationConfirmation.notUniCooperation"
+                    components={{
+                        navigate: (
+                            <span
+                                className="inline underline text-primary cursor-pointer"
+                                onClick={() => onFormChange({ isFromUniCooperation: false, currentStep: RegistrationStepEnum.uniCooperation })}
+                            >
+                                hier
+                            </span>
+                        ),
+                    }}
+                ></Trans>
+            </Typography>
             <Typography className="text-center md:whitespace-pre-line">
                 <Trans
-                    i18nKey="registration.steps.uniCooperationConfirmation.description"
+                    i18nKey="registration.steps.uniCooperationConfirmation.support"
                     components={{
                         supportEmail: (
                             <a className="inline underline text-primary" href="mailto:support@lern-fair.de">
