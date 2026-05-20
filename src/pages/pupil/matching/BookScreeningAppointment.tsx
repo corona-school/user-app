df9e263f819e397da6c604452e4929a79e3e1e5c
John Angel <john.angel@lern-fair.de>
2026-05-20 16:38:35 +0200

    style: Fix mobile styles

--- src/pages/pupil/matching/BookScreeningAppointment.tsx
+++ src/pages/pupil/matching/BookScreeningAppointment.tsx
@@ -79,7 +79,9 @@ export const BookScreeningAppointment = () => {
     return (
         <MatchRequestStep className="pb-0">
             <div className="relative h-full">
-                <Typography variant="h4">{t('matching.wizard.pupil.bookScreeningAppointment.title')}</Typography>
+                <Typography variant="h4" className="text-lg leading-5 md:text-xl md:leading-8">
+                    {t('matching.wizard.pupil.bookScreeningAppointment.title')}
+                </Typography>
                 <div className="flex flex-col gap-y-7 mb-4">
                     <div className="flex gap-x-4 items-center mt-7 w-full">
                         <Typography className="max-w-[680px]">{t('matching.wizard.pupil.bookScreeningAppointment.description')}</Typography>
@@ -88,8 +90,8 @@ export const BookScreeningAppointment = () => {
                         {t('matching.wizard.pupil.bookScreeningAppointment.alert', { minutes: 10 })}
                     </Alert>
                 </div>
-                <div className="flex flex-col justify-center items-center">
-                    <Accordion type="single" collapsible className="w-full my-4 max-w-[845px] min-h-[350px]">
+                <div className="flex flex-col justify-center items-center mb-10 md:mb-0">
+                    <Accordion type="single" collapsible className="w-full my-4 max-w-[845px] md:min-h-[310px]">
                         <AccordionItem className="border-none py-0" value={'about'}>
                             <AccordionTrigger IconComponent={IconCircleChevronDown} iconClasses="size-10 !stroke-[0.5px]" className="py-0 items-center">
                                 <Typography variant="body-lg" className="font-medium">
@@ -97,7 +99,7 @@ export const BookScreeningAppointment = () => {
                                 </Typography>
                             </AccordionTrigger>
                             <AccordionContent className="flex flex-col pt-2">
-                                <ul className="list-disc list-inside text-pretty pl-3">
+                                <ul className="list-disc list-outside md:list-inside text-pretty pl-3">
                                     {aboutTheAppointmentPoints.map((point, index) => (
                                         <li key={index}>
                                             <Typography variant="subtle" as="span">
@@ -116,7 +118,7 @@ export const BookScreeningAppointment = () => {
                                 </Typography>
                             </AccordionTrigger>
                             <AccordionContent className="flex flex-col pt-2">
-                                <ul className="list-disc list-inside text-pretty pl-3">
+                                <ul className="list-disc list-outside md:list-inside text-pretty pl-3">
                                     {importantPoints.map((point, index) => (
                                         <li key={index}>
                                             <Typography variant="subtle" as="span">
@@ -140,26 +142,28 @@ export const BookScreeningAppointment = () => {
                         </AccordionItem>
                     </Accordion>
                 </div>
-                <div className="flex gap-x-4">
-                    {!form.isAppointmentStepForced && (
-                        <Button
-                            leftIcon={<IconArrowLeft size={14} className="!stroke-[2px]" />}
-                            className="min-w-[177px]"
-                            variant="outline"
-                            onClick={handleOnBack}
-                            disabled={isLoading}
-                        >
-                            {t('back')}
+                <div className="flex flex-col md:flex-row gap-x-4 gap-y-2">
+                    <div className="flex flex-row gap-x-4">
+                        {!form.isAppointmentStepForced && (
+                            <Button
+                                leftIcon={<IconArrowLeft size={14} className="!stroke-[2px]" />}
+                                className="md:min-w-[177px] w-full md:w-auto"
+                                variant="outline"
+                                onClick={handleOnBack}
+                                disabled={isLoading}
+                            >
+                                {t('back')}
+                            </Button>
+                        )}
+                        <Button className="md:min-w-[177px] w-full md:w-auto" variant="outline" onClick={() => navigate(-1)} disabled={isLoading}>
+                            {t('cancel')}
                         </Button>
-                    )}
-                    <Button className="min-w-[177px]" variant="outline" onClick={() => navigate(-1)} disabled={isLoading}>
-                        {t('cancel')}
-                    </Button>
+                    </div>
                     <Button
                         isLoading={isLoading}
                         leftIcon={<IconCalendar size={20} />}
                         onClick={() => setIsCalendarOpen(true)}
-                        className="min-w-[177px] px-[43px]"
+                        className="min-w-[177px] px-[43px] w-full md:w-auto"
                     >
                         {t('matching.wizard.pupil.bookScreeningAppointment.bookAppointment')}
                     </Button>
