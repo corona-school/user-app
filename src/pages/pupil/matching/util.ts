fcd26816ac0f7527179b3f93a2e5fd355dbca6e5
John Angel <john.angel@lern-fair.de>
2026-06-25 15:48:03 +0200

    feat: Remove unused match request step

--- src/pages/pupil/matching/util.ts
+++ src/pages/pupil/matching/util.ts
@@ -1,15 +1,13 @@
 export enum MatchRequestStep {
     updateData = 'updateData',
-    german = 'german',
     subjects = 'subjects',
     priority = 'priority',
     bookScreeningAppointment = 'bookScreeningAppointment',
 }
 
 export const pupilMatchRequestFlow: MatchRequestStep[] = [
-    MatchRequestStep.updateData,
-    MatchRequestStep.german,
     MatchRequestStep.subjects,
     MatchRequestStep.priority,
+    MatchRequestStep.updateData,
     MatchRequestStep.bookScreeningAppointment,
 ];
