54f5c9c762fc208d560f613ca9a1f38f91cecdde
John Angel <john.angel@lern-fair.de>
2026-07-02 15:17:24 +0200

    style: Ring for focus states

--- src/components/Panels.tsx
+++ src/components/Panels.tsx
@@ -35,7 +35,7 @@ const TabsTrigger = React.forwardRef<React.ElementRef<typeof TabsPrimitive.Trigg
     <TabsPrimitive.Trigger
         ref={ref}
         className={cn(
-            'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-3 text-form font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow',
+            'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-3 text-form font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow',
             className
         )}
         {...props}
@@ -61,7 +61,7 @@ const TabsContent = React.forwardRef<React.ElementRef<typeof TabsPrimitive.Conte
             forceMount={forceMount || inactiveMode === 'hide' ? true : undefined}
             className={cn(
                 inactiveMode === 'hide' ? 'data-[state=inactive]:hidden' : '',
-                'mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+                'mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2',
                 className
             )}
             {...props}
