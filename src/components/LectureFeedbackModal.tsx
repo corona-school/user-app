15fe887a74934021ae118b85f551c5a241f4cae4
John Angel <john.angel@lern-fair.de>
2026-09-03 09:53:58 +0200

    feat: Add dummy success message / layout adjustments

--- src/components/LectureFeedbackModal.tsx
+++ src/components/LectureFeedbackModal.tsx
@@ -7,6 +7,7 @@ import { useMutation } from '@apollo/client';
 import { IconSend } from '@tabler/icons-react';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { toast } from 'sonner';
 import { Input } from './Input';
 import { Typography } from './Typography';
 
@@ -52,6 +53,12 @@ export const LectureFeedbackModal = ({ feedbackId, onOpenChange, isOpen, learnin
         }
     };
 
+    const handleOnSubmit = async () => {
+        await submitFeedback({ variables: { feedbackId, rating: rating!, tags } });
+        toast.success('Dankeschön!', { description: 'Dein Feedback wurde gesendet' });
+        onOpenChange(false);
+    };
+
     useEffect(() => {
         setTags([]);
     }, [rating]);
@@ -61,8 +68,10 @@ export const LectureFeedbackModal = ({ feedbackId, onOpenChange, isOpen, learnin
             <ModalHeader>
                 <ModalTitle>Wie war euer Meeting?</ModalTitle>
             </ModalHeader>
-            <div className="mb-4">
-                <Typography className="mb-10">Das Feedback geht nur an Lern-Fair. Dein*e Lernpartner*in {learningPartnerName} kann es nicht sehen.</Typography>
+            <div>
+                <Typography className="mb-4 md:mb-8">
+                    Das Feedback geht nur an Lern-Fair. Dein*e Lernpartner*in {learningPartnerName} kann es nicht sehen.
+                </Typography>
                 <div className="flex gap-x-1">
                     <Toggle
                         className="h-[52px] w-[58.8px] md:h-[52px] md:w-[99.2px] hover:bg-yellow-100 text-3xl"
@@ -101,10 +110,10 @@ export const LectureFeedbackModal = ({ feedbackId, onOpenChange, isOpen, learnin
                     </Toggle>
                 </div>
             </div>
-            <div>
-                <div className="flex flex-wrap gap-1">
-                    {rating &&
-                        RATING_TAGS[rating].map((tag) => (
+            {rating && (
+                <div>
+                    <div className="flex flex-wrap gap-1">
+                        {RATING_TAGS[rating].map((tag) => (
                             <Toggle
                                 className="rounded-full py-[13px] px-[16px]"
                                 variant="outline-accent"
@@ -115,7 +124,6 @@ export const LectureFeedbackModal = ({ feedbackId, onOpenChange, isOpen, learnin
                                 {tag}
                             </Toggle>
                         ))}
-                    {rating && (
                         <Toggle
                             className="rounded-full py-[13px] px-[16px]"
                             variant="outline-accent"
@@ -124,20 +132,17 @@ export const LectureFeedbackModal = ({ feedbackId, onOpenChange, isOpen, learnin
                         >
                             Sonstiges
                         </Toggle>
+                    </div>
+                    {tags.includes('Sonstiges') && (
+                        <Input className="w-full mt-4" placeholder="Dein Kommentar" errorMessageClassName="hidden" value={comment} onChangeText={setComment} />
                     )}
                 </div>
-                {tags.includes('Sonstiges') && <Input placeholder="Dein Kommentar" errorMessageClassName="hidden" value={comment} onChangeText={setComment} />}
-            </div>
+            )}
             <ModalFooter mobileLayout="column">
                 <Button className="w-full lg:w-[130px]" variant="outline" onClick={() => onOpenChange(false)}>
                     Überspringen
                 </Button>
-                <Button
-                    className="w-full lg:w-[180px]"
-                    rightIcon={<IconSend size={16} />}
-                    disabled={!rating}
-                    onClick={() => submitFeedback({ variables: { feedbackId, rating: rating!, tags } })}
-                >
+                <Button className="w-full lg:w-[180px]" rightIcon={<IconSend size={16} />} disabled={!rating} onClick={handleOnSubmit}>
                     Feedback senden
                 </Button>
             </ModalFooter>
